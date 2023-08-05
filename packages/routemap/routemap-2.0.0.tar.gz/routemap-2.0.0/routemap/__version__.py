1cf734c65871e52a586cb618fd5e02580c4bb2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:51 +0000

    Add package routemap/2.0.0/routemap-2.0.0.tar.gz

---
+++ packages/routemap/routemap-2.0.0.tar.gz/routemap-2.0.0/routemap/__version__.py
@@ -0,0 +1,12 @@
+"""
+Version information
+"""
+
+__title__ = 'routemap'
+__description__ = 'Generate route maps from various sources.'
+__url__ = 'https://gitlab.com/vascowhite/routemap'
+__version__ = '2.0.0'
+__author__ = 'Paul White'
+__author_email__ = 'routemap@vascowhite.co.uk'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Paul White'
