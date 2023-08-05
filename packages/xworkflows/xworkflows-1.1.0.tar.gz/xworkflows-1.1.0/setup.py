eebc00539b897a9ec11c4e0451793f225f777b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:24 +0000

    Add package xworkflows/1.1.0/xworkflows-1.1.0.tar.gz

---
+++ packages/xworkflows/xworkflows-1.1.0.tar.gz/xworkflows-1.1.0/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) The xworkflows project
+# This code is distributed under the two-clause BSD License.
+
+from setuptools import find_packages, setup
+
+setup()
