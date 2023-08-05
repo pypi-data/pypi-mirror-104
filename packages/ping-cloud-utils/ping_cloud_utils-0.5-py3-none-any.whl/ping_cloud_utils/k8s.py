8933c8b15c0d08d305a110ba8a919cfd73d07015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package ping-cloud-utils/0.5/ping_cloud_utils-0.5-py3-none-any.whl

---
+++ packages/ping-cloud-utils/ping_cloud_utils-0.5-py3-none-any.whl/ping_cloud_utils/k8s.py
@@ -0,0 +1,34 @@
+from kubernetes import client, config
+
+
+class K8s:
+    def __init__(self):
+        """
+        Initialize connection to k8s API
+        """
+        try:
+            config.load_incluster_config()
+        except config.config_exception.ConfigException:
+            config.load_kube_config()
+
+        self.client = client.CoreV1Api()
+        self.batch_api = client.BatchV1Api()
+        self.batch_v1beta1_api = client.BatchV1beta1Api()
+        self.extension_api = client.ExtensionsV1beta1Api()
+        self.version_api = client.VersionApi()
+
+    def get_namespace_pods(self, namespace="default", label_selector=""):
+        """
+        This will return a list of objects req namespace
+        """
+        return self.client.list_namespaced_pod(
+            namespace=namespace, label_selector=label_selector
+        )
+
+    def get_version(self):
+        """
+        Method to get cluster version
+        :return:
+        JSON with cluster versions
+        """
+        return self.version_api.get_code()
