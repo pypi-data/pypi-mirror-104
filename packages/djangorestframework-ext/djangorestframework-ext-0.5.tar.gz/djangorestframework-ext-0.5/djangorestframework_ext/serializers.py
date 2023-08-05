8d77506bb292180e8a8ba10ae11c00d541651f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package djangorestframework-ext/0.5/djangorestframework-ext-0.5.tar.gz

---
+++ packages/djangorestframework-ext/djangorestframework-ext-0.5.tar.gz/djangorestframework-ext-0.5/djangorestframework_ext/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.utils import dateformat
+from django.utils.dateparse import parse_datetime
+
+
+class RecursiveSerializer(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
+class ExportModelSerializer(serializers.ModelSerializer):
+    def to_representation(self, obj):
+        data = super().to_representation(obj)
+        result = dict()
+        for k, v in data.items():
+            field = self.get_fields()[k]
+            if isinstance(field, serializers.DateTimeField) and v:
+                value = dateformat.format(parse_datetime(v), 'Y-m-d H:i:s')
+            else:
+                value = v
+            result[getattr(field, 'label') or k] = value
+        return result
