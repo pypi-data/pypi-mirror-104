b8faa8f383c47d4d43d0eb1fe94f98de868334ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package thebrukhellomodule/3.0/thebrukhellomodule-3.0.tar.gz

---
+++ packages/thebrukhellomodule/thebrukhellomodule-3.0.tar.gz/thebrukhellomodule-3.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='thebrukhellomodule',
+      version='3.0',
+      description='Hello!',
+      packages=['thebrukhellomodule'],
+      author_email='thebrukprojects@gmail.com',
+      zip_safe=False)
---
+++ packages/thebrukhellomodule/thebrukhellomodule-3.0.tar.gz/thebrukhellomodule-3.0/thebrukhellomodule/emojier.py
@@ -0,0 +1,9 @@
+def hello():
+    print()
+    print("|￣￣￣￣￣￣|")
+    print("| Привет!  |")
+    print("| От Егора |")
+    print("|＿＿＿＿＿__|")
+    print("(\__/) ||    ")
+    print("(•ㅅ•) ||    ")
+    print("/ 　 づ      ")
