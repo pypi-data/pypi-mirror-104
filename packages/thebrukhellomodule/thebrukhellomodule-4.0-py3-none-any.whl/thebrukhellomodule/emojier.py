971714abf2b04917876397d9802d5b769e5a6d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package thebrukhellomodule/4.0/thebrukhellomodule-4.0-py3-none-any.whl

---
+++ packages/thebrukhellomodule/thebrukhellomodule-4.0-py3-none-any.whl/thebrukhellomodule/emojier.py
@@ -0,0 +1,16 @@
+'''
+def hello():
+    print()
+    print("|￣￣￣￣￣￣|")
+    print("| Привет!  |")
+    print("| От Егора |")
+    print("|＿＿＿＿＿__|")
+    print("(\__/) ||    ")
+    print("(•ㅅ•) ||    ")
+    print("/ 　 づ      ")
+'''
+
+def hello():
+    print("HELLO")
+
+hello()
