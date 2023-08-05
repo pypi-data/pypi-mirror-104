8ccd594b46f14559a3db3ca37b511f60292c144e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:24 +0000

    Add package TurkishPreprocessor/0.0.4/TurkishPreprocessor-0.0.4.tar.gz

---
+++ packages/TurkishPreprocessor/TurkishPreprocessor-0.0.4.tar.gz/TurkishPreprocessor-0.0.4/README.md
@@ -0,0 +1,13 @@
+# TurkishPreprocesser
+ 
+I will add detail information about this lib when I have time.
+
+But you can install library with : 
+```
+pip install TurkishPreprocessor
+```
+
+Usage : 
+```
+    from TurkishPreprocessor.Preprocessor import Preprocessor
+```
---
+++ packages/TurkishPreprocessor/TurkishPreprocessor-0.0.4.tar.gz/TurkishPreprocessor-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TurkishPreprocessor",
+    version="0.0.4",
+    author="Yusuf ANI",
+    author_email="yusufani8@gmail.com",
+    description="A Turkish Preprocessor for NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yusufani/TurkishPreprocessor",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+          data_files=[
+                  ('config', ['TurkishPreprocessor\\Turkish_Lemmatizer\\revisedDict.pkl'])],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
