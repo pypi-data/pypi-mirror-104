1e16463b95c20b7cfbf4ae59502ffe6095170f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package mypy-boto3-support/1.17.60.1/mypy-boto3-support-1.17.60.1.tar.gz

---
+++ packages/mypy-boto3-support/mypy-boto3-support-1.17.60.1.tar.gz/mypy-boto3-support-1.17.60.1/mypy_boto3_support/literals.py
@@ -0,0 +1,24 @@
+"""
+Main interface for support service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_support.literals import DescribeCasesPaginatorName
+
+    data: DescribeCasesPaginatorName = "describe_cases"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("DescribeCasesPaginatorName", "DescribeCommunicationsPaginatorName")
+
+
+DescribeCasesPaginatorName = Literal["describe_cases"]
+DescribeCommunicationsPaginatorName = Literal["describe_communications"]
