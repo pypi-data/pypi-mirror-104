7fcf15bb6df9c93d54c3fb70c8873d62b3b981cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:06 +0000

    Add package edge-impulse-sdk/1.2.0/edge-impulse-sdk-1.2.0.tar.gz

---
+++ packages/edge-impulse-sdk/edge-impulse-sdk-1.2.0.tar.gz/edge-impulse-sdk-1.2.0/edge_impulse_sdk/api/__init__.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from edge_impulse_sdk.api.admin_api import AdminApi
+from edge_impulse_sdk.api.allows_read_only_api import AllowsReadOnlyApi
+from edge_impulse_sdk.api.auth_api import AuthApi
+from edge_impulse_sdk.api.cdn_api import CDNApi
+from edge_impulse_sdk.api.classify_api import ClassifyApi
+from edge_impulse_sdk.api.dsp_api import DSPApi
+from edge_impulse_sdk.api.deployment_api import DeploymentApi
+from edge_impulse_sdk.api.devices_api import DevicesApi
+from edge_impulse_sdk.api.export_api import ExportApi
+from edge_impulse_sdk.api.health_api import HealthApi
+from edge_impulse_sdk.api.impulse_api import ImpulseApi
+from edge_impulse_sdk.api.jobs_api import JobsApi
+from edge_impulse_sdk.api.learn_api import LearnApi
+from edge_impulse_sdk.api.login_api import LoginApi
+from edge_impulse_sdk.api.optimization_api import OptimizationApi
+from edge_impulse_sdk.api.organization_blocks_api import OrganizationBlocksApi
+from edge_impulse_sdk.api.organization_create_project_api import OrganizationCreateProjectApi
+from edge_impulse_sdk.api.organization_data_api import OrganizationDataApi
+from edge_impulse_sdk.api.organization_jobs_api import OrganizationJobsApi
+from edge_impulse_sdk.api.organization_portals_api import OrganizationPortalsApi
+from edge_impulse_sdk.api.organization_requires_admin_api import OrganizationRequiresAdminApi
+from edge_impulse_sdk.api.organizations_api import OrganizationsApi
+from edge_impulse_sdk.api.projects_api import ProjectsApi
+from edge_impulse_sdk.api.raw_data_api import RawDataApi
+from edge_impulse_sdk.api.requires_sudo_api import RequiresSudoApi
+from edge_impulse_sdk.api.requires_third_party_auth_api_key_api import RequiresThirdPartyAuthApiKeyApi
+from edge_impulse_sdk.api.supports_range_api import SupportsRangeApi
+from edge_impulse_sdk.api.third_party_auth_api import ThirdPartyAuthApi
+from edge_impulse_sdk.api.upload_portal_api import UploadPortalApi
+from edge_impulse_sdk.api.user_api import UserApi
