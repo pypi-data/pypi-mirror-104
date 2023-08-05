0d8da1ed20162ab96447ee1c3a29a7b0a759f85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package dotenv-flow/0.2.2/dotenv-flow-0.2.2.tar.gz

---
+++ packages/dotenv-flow/dotenv-flow-0.2.2.tar.gz/dotenv-flow-0.2.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['dotenv_flow']
+install_requires = \
+['python-dotenv>=0.14.0,<0.15.0']
+
+setup_kwargs = {
+    'name': 'dotenv-flow',
+    'version': '0.2.2',
+    'description': 'Like the dotenv-flow NodeJS library, for Python',
+    'long_description': None,
+    'author': 'Carlos Gonzalez',
+    'author_email': 'gonsa.carlos@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
