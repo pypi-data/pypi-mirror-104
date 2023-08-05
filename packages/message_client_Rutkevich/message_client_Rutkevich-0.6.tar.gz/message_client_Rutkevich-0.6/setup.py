48382c57848d5daab04943f10df7a5e9dc693ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:11 +0000

    Add package message_client_Rutkevich/0.6/message_client_Rutkevich-0.6.tar.gz

---
+++ packages/message_client_Rutkevich/message_client_Rutkevich-0.6.tar.gz/message_client_Rutkevich-0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="message_client_Rutkevich",
+      version="0.6",
+      description="It's alive, ALIVE!",
+      author="Yaroslav Rutkevich",
+      author_email="Fenix12121995@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
