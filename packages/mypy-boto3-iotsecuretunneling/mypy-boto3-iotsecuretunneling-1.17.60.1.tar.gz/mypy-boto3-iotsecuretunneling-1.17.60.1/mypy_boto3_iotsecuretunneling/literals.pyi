b801021a856c890bf72d8d61f0f04c9eacff059f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package mypy-boto3-iotsecuretunneling/1.17.60.1/mypy-boto3-iotsecuretunneling-1.17.60.1.tar.gz

---
+++ packages/mypy-boto3-iotsecuretunneling/mypy-boto3-iotsecuretunneling-1.17.60.1.tar.gz/mypy-boto3-iotsecuretunneling-1.17.60.1/mypy_boto3_iotsecuretunneling/literals.pyi
@@ -0,0 +1,22 @@
+"""
+Main interface for iotsecuretunneling service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_iotsecuretunneling.literals import ConnectionStatus
+
+    data: ConnectionStatus = "CONNECTED"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("ConnectionStatus", "TunnelStatus")
+
+ConnectionStatus = Literal["CONNECTED", "DISCONNECTED"]
+TunnelStatus = Literal["CLOSED", "OPEN"]
