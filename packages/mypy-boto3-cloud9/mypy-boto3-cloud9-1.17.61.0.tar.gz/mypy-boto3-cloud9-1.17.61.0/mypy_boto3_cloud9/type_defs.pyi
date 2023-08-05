c26318679c9ff95337eadd394801df27024ae4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:26 +0000

    Add package mypy-boto3-cloud9/1.17.61.0/mypy-boto3-cloud9-1.17.61.0.tar.gz

---
+++ packages/mypy-boto3-cloud9/mypy-boto3-cloud9-1.17.61.0.tar.gz/mypy-boto3-cloud9-1.17.61.0/mypy_boto3_cloud9/type_defs.pyi
@@ -0,0 +1,124 @@
+"""
+Main interface for cloud9 service type definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_cloud9.type_defs import EnvironmentLifecycleTypeDef
+
+    data: EnvironmentLifecycleTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import List
+
+from mypy_boto3_cloud9.literals import (
+    ConnectionType,
+    EnvironmentLifecycleStatus,
+    EnvironmentStatus,
+    EnvironmentType,
+    ManagedCredentialsStatus,
+    Permissions,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "EnvironmentLifecycleTypeDef",
+    "EnvironmentMemberTypeDef",
+    "EnvironmentTypeDef",
+    "TagTypeDef",
+    "CreateEnvironmentEC2ResultTypeDef",
+    "CreateEnvironmentMembershipResultTypeDef",
+    "DescribeEnvironmentMembershipsResultTypeDef",
+    "DescribeEnvironmentStatusResultTypeDef",
+    "DescribeEnvironmentsResultTypeDef",
+    "ListEnvironmentsResultTypeDef",
+    "ListTagsForResourceResponseTypeDef",
+    "PaginatorConfigTypeDef",
+    "UpdateEnvironmentMembershipResultTypeDef",
+)
+
+EnvironmentLifecycleTypeDef = TypedDict(
+    "EnvironmentLifecycleTypeDef",
+    {"status": EnvironmentLifecycleStatus, "reason": str, "failureResource": str},
+    total=False,
+)
+
+_RequiredEnvironmentMemberTypeDef = TypedDict(
+    "_RequiredEnvironmentMemberTypeDef",
+    {"permissions": Permissions, "userId": str, "userArn": str, "environmentId": str},
+)
+_OptionalEnvironmentMemberTypeDef = TypedDict(
+    "_OptionalEnvironmentMemberTypeDef", {"lastAccess": datetime}, total=False
+)
+
+class EnvironmentMemberTypeDef(
+    _RequiredEnvironmentMemberTypeDef, _OptionalEnvironmentMemberTypeDef
+):
+    pass
+
+_RequiredEnvironmentTypeDef = TypedDict(
+    "_RequiredEnvironmentTypeDef", {"type": EnvironmentType, "arn": str, "ownerArn": str}
+)
+_OptionalEnvironmentTypeDef = TypedDict(
+    "_OptionalEnvironmentTypeDef",
+    {
+        "id": str,
+        "name": str,
+        "description": str,
+        "connectionType": ConnectionType,
+        "lifecycle": "EnvironmentLifecycleTypeDef",
+        "managedCredentialsStatus": ManagedCredentialsStatus,
+    },
+    total=False,
+)
+
+class EnvironmentTypeDef(_RequiredEnvironmentTypeDef, _OptionalEnvironmentTypeDef):
+    pass
+
+TagTypeDef = TypedDict("TagTypeDef", {"Key": str, "Value": str})
+
+CreateEnvironmentEC2ResultTypeDef = TypedDict(
+    "CreateEnvironmentEC2ResultTypeDef", {"environmentId": str}, total=False
+)
+
+CreateEnvironmentMembershipResultTypeDef = TypedDict(
+    "CreateEnvironmentMembershipResultTypeDef", {"membership": "EnvironmentMemberTypeDef"}
+)
+
+DescribeEnvironmentMembershipsResultTypeDef = TypedDict(
+    "DescribeEnvironmentMembershipsResultTypeDef",
+    {"memberships": List["EnvironmentMemberTypeDef"], "nextToken": str},
+    total=False,
+)
+
+DescribeEnvironmentStatusResultTypeDef = TypedDict(
+    "DescribeEnvironmentStatusResultTypeDef", {"status": EnvironmentStatus, "message": str}
+)
+
+DescribeEnvironmentsResultTypeDef = TypedDict(
+    "DescribeEnvironmentsResultTypeDef", {"environments": List["EnvironmentTypeDef"]}, total=False
+)
+
+ListEnvironmentsResultTypeDef = TypedDict(
+    "ListEnvironmentsResultTypeDef", {"nextToken": str, "environmentIds": List[str]}, total=False
+)
+
+ListTagsForResourceResponseTypeDef = TypedDict(
+    "ListTagsForResourceResponseTypeDef", {"Tags": List["TagTypeDef"]}, total=False
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef", {"MaxItems": int, "PageSize": int, "StartingToken": str}, total=False
+)
+
+UpdateEnvironmentMembershipResultTypeDef = TypedDict(
+    "UpdateEnvironmentMembershipResultTypeDef",
+    {"membership": "EnvironmentMemberTypeDef"},
+    total=False,
+)
