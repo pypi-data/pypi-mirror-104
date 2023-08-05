8311ca17ad9a41d7e99a1e861aff8ca34546f843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:54 +0000

    Add package pharmpy-core/0.23.3/pharmpy_core-0.23.3-py3-none-any.whl

---
+++ packages/pharmpy-core/pharmpy_core-0.23.3-py3-none-any.whl/pharmpy/modeling/evaluation.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sympy
+
+
+def evaluate_expression(model, expression):
+    """Evaluate expression using model
+
+    Calculate the value of expression for each data record.
+    The expression can contain dataset columns, variables in model and
+    population parameters. If the model has parameter estimates these
+    will be used. Initial estimates will be used for non-estimated parameters.
+
+    Parameters
+    ----------
+    expression : str or sympy expression
+        Expression to evaluate
+
+    Returns
+    -------
+    pd.Series
+        A series of one evaluated value for each data record
+    """
+    expression = sympy.sympify(expression)
+    full_expr = model.statements.full_expression_from_odes(expression)
+    pe = model.modelfit_results.parameter_estimates
+    inits = model.parameters.inits
+    expr = full_expr.subs(dict(pe)).subs(inits)
+    data = model.dataset
+
+    def func(row):
+        return np.float64(expr.evalf(subs=dict(row)))
+
+    df = data.apply(func, axis=1)
+    return df
