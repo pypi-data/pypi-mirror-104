79328790d4444ab7998ea684477d6efe1276e21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package py-easy-rest/0.2.1/py_easy_rest-0.2.1-py3-none-any.whl

---
+++ packages/py-easy-rest/py_easy_rest-0.2.1-py3-none-any.whl/tests/py_easy_rest/__init__.py
@@ -0,0 +1,44 @@
+from unittest.mock import Mock
+from aiounittest import AsyncTestCase
+from py_easy_rest.server import App
+from py_easy_rest.repos.memory import MemoryRepo
+from py_easy_rest.caches.memory import MemoryCache
+
+
+api_config_mock = {
+    "name": "Project Name",
+    "schemas": [{
+        "name": "Mock",
+        "slug": "mock",
+        "properties": {
+            "name": {"type": "string"},
+            "age": {"type": "integer"},
+        },
+        "required": ["name"],
+    }, {
+        "name": "Second api",
+        "slug": "second",
+        "properties": {"name": {"type": "string"}},
+    }]
+}
+
+
+class BaseSanicTestCase(AsyncTestCase):
+
+    def setUp(self):
+        self._mock_repo = Mock(MemoryRepo)
+        self._cache = MemoryCache()
+        self._py_easy_rest = App(
+            api_config_mock,
+            repo=self._mock_repo,
+            cache=self._cache,
+        )
+
+    async def request_api(self, path, method="GET", json=None):
+        client = self._py_easy_rest.app.asgi_client
+
+        request, response = await client.request(method, path, json=json)
+
+        await client.aclose()
+
+        return request, response
