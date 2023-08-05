f2595fb062156d0ec71f6b0f9605a757d7a90130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:51 +0000

    Add package charlypkg/0.2/charlypkg-0.2.tar.gz

---
+++ packages/charlypkg/charlypkg-0.2.tar.gz/charlypkg-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='charlypkg',
+      version='0.2',
+      description='First python package ever created by me.',
+      url='http://github.com/antunescarles/charlypkg',
+      author='Carlos Antunes',
+      author_email='antunes.p.carlos@hotmail.com',
+      license='MIT',
+      py_modules = ['charlypkg'],
+      packages=['charlypkg'],
+      zip_safe=False)
