30f9d0f8e72ce6e4de7f860eea1b072d8c02ccbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:00 +0000

    Add package lino-noi/21.4.0/lino-noi-21.4.0.tar.gz

---
+++ packages/lino-noi/lino-noi-21.4.0.tar.gz/lino-noi-21.4.0/lino_noi/lib/users/models.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015-2017 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+"""Database models for this plugin.
+
+"""
+
+from lino_xl.lib.online.users.models import *
+
