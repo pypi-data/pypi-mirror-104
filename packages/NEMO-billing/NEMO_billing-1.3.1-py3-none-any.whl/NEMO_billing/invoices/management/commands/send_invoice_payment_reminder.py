47f22f70ac0c27273e64c2431f9da3b975807b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:48 +0000

    Add package NEMO-billing/1.3.1/NEMO_billing-1.3.1-py3-none-any.whl

---
+++ packages/NEMO-billing/NEMO_billing-1.3.1-py3-none-any.whl/NEMO_billing/invoices/management/commands/send_invoice_payment_reminder.py
@@ -0,0 +1,12 @@
+from django.core.management import BaseCommand
+
+from NEMO_billing.invoices.views.invoices import do_send_invoice_payment_reminder
+
+
+class Command(BaseCommand):
+	help = (
+		"Run every day to send payment reminders for unpaid invoices. "
+	)
+
+	def handle(self, *args, **options):
+		do_send_invoice_payment_reminder()
