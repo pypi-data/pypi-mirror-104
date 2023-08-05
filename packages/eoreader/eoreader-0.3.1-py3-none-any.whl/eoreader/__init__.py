58476a57fe1d9c00f6c3ea6f2e501a6b69604d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:06 +0000

    Add package eoreader/0.3.1/eoreader-0.3.1-py3-none-any.whl

---
+++ packages/eoreader/eoreader-0.3.1-py3-none-any.whl/eoreader/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021, SERTIT-ICube - France, https://sertit.unistra.fr/
+# This file is part of eoreader project
+#     https://github.com/sertit/eoreader
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+**Source Code**: https://github.com/sertit/eoreader
+
+.. include:: ../documentation/index.md
+
+_____
+
+.. include:: ../documentation/main_features.md
+
+_____
+
+.. include:: ../documentation/optical.md
+
+_____
+
+.. include:: ../documentation/sar.md
+
+"""
+
+__pdoc__ = {
+    "eoreader.data": False,
+}
+
+__version__ = "0.3.1"
