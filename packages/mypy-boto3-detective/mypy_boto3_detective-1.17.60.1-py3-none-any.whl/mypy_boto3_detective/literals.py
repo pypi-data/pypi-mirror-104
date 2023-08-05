b7228726fff53e1b73ab26cc719bc2f6470058bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package mypy-boto3-detective/1.17.60.1/mypy_boto3_detective-1.17.60.1-py3-none-any.whl

---
+++ packages/mypy-boto3-detective/mypy_boto3_detective-1.17.60.1-py3-none-any.whl/mypy_boto3_detective/literals.py
@@ -0,0 +1,26 @@
+"""
+Main interface for detective service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_detective.literals import MemberDisabledReason
+
+    data: MemberDisabledReason = "VOLUME_TOO_HIGH"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("MemberDisabledReason", "MemberStatus")
+
+
+MemberDisabledReason = Literal["VOLUME_TOO_HIGH", "VOLUME_UNKNOWN"]
+MemberStatus = Literal[
+    "ACCEPTED_BUT_DISABLED", "ENABLED", "INVITED", "VERIFICATION_FAILED", "VERIFICATION_IN_PROGRESS"
+]
