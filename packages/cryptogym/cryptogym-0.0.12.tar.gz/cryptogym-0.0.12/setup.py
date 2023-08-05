3108550d8fcd9805a7ed8610131af44de713b416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package cryptogym/0.0.12/cryptogym-0.0.12.tar.gz

---
+++ packages/cryptogym/cryptogym-0.0.12.tar.gz/cryptogym-0.0.12/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cryptogym",
+    version='0.0.12',
+    license="MIT",
+    description="CryptoCurrency Trading environment",
+    author="Alan Tessier",
+    author_email="alantessier.cr@gmail.com",
+    url="https://github.com/alantess/cryptogym",
+    keywords=[
+        "cryptocurrency", "reinforcement learning", "deep learning",
+        "artifical intelligence"
+    ],
+    install_requires=["matplotlib", "gym", "pandas", "numpy", "scikit_learn"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages())
