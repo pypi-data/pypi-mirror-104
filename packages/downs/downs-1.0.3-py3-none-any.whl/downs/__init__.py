59503603ac4e5c8207966d0fe88c2fd934fd055a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:07 +0000

    Add package downs/1.0.3/downs-1.0.3-py3-none-any.whl

---
+++ packages/downs/downs-1.0.3-py3-none-any.whl/downs/__init__.py
@@ -0,0 +1,34 @@
+# Multithreaded download tool.
+# @author userElaina
+'''
+
+`throws` `nThread` `nDown`
+
+	>>> throws(f:function,args:tuple)->None
+	>>> nThread(
+		self,
+		n:int=0,
+		waits:int=0,
+		f:function=None,
+		args:list=None,
+		fast:bool=False
+		)
+	>>> nDown(
+		url:list,
+		name:list,
+		pth:str,
+		h1:list=HEADERS,
+		h2:list=HEADERS,
+		f:function=None,
+		proxies:dict=None,
+		stream_size:int=-1,
+		chunk_size:int=1<<20,
+		n:int=30,
+		waits:int=0,
+		print_log:bool=True,
+		test7z:bool=False,
+		)
+'''
+
+from downs._thread import throws,nThread
+from downs._down import nDown
