fe449ac7d24233ded8faa3380d88062cd406ea16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package salt-viewer/0.1.2/salt-viewer-0.1.2.tar.gz

---
+++ packages/salt-viewer/salt-viewer-0.1.2.tar.gz/salt-viewer-0.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setuptools.setup(
+    name="salt-viewer",
+    version="0.1.2",
+    description="Simple image viewer",
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/GuiltyCat/SaltViewer",
+    author="GuiltyCat",
+    keywords="image, viewer, archive, animation",
+    install_requires=[
+        "natsort",
+        "pillow",
+        "send2trash",
+        "rarfile",
+        "py7zr",
+        "cairosvg",
+        "pdf2image",
+    ],
+    py_modules=['salt_viewer'],
+    entry_points={"console_scripts": ["salt-viewer=salt_viewer:main"]},
+)
