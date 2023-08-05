2b422ccc5fd3fabfe10d6b8e357e5ad65c8ec32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package mypy-boto3-mediaconnect/1.17.60.1/mypy_boto3_mediaconnect-1.17.60.1-py3-none-any.whl

---
+++ packages/mypy-boto3-mediaconnect/mypy_boto3_mediaconnect-1.17.60.1-py3-none-any.whl/mypy_boto3_mediaconnect/waiter.pyi
@@ -0,0 +1,57 @@
+"""
+Main interface for mediaconnect service client waiters.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_mediaconnect import MediaConnectClient
+    from mypy_boto3_mediaconnect.waiter import (
+        FlowActiveWaiter,
+        FlowDeletedWaiter,
+        FlowStandbyWaiter,
+    )
+
+    client: MediaConnectClient = boto3.client("mediaconnect")
+
+    flow_active_waiter: FlowActiveWaiter = client.get_waiter("flow_active")
+    flow_deleted_waiter: FlowDeletedWaiter = client.get_waiter("flow_deleted")
+    flow_standby_waiter: FlowStandbyWaiter = client.get_waiter("flow_standby")
+    ```
+"""
+from botocore.waiter import Waiter as Boto3Waiter
+
+from mypy_boto3_mediaconnect.type_defs import WaiterConfigTypeDef
+
+__all__ = ("FlowActiveWaiter", "FlowDeletedWaiter", "FlowStandbyWaiter")
+
+class FlowActiveWaiter(Boto3Waiter):
+    """
+    [Waiter.FlowActive documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/mediaconnect.html#MediaConnect.Waiter.FlowActive)
+    """
+
+    def wait(self, FlowArn: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [FlowActive.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/mediaconnect.html#MediaConnect.Waiter.FlowActive.wait)
+        """
+
+class FlowDeletedWaiter(Boto3Waiter):
+    """
+    [Waiter.FlowDeleted documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/mediaconnect.html#MediaConnect.Waiter.FlowDeleted)
+    """
+
+    def wait(self, FlowArn: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [FlowDeleted.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/mediaconnect.html#MediaConnect.Waiter.FlowDeleted.wait)
+        """
+
+class FlowStandbyWaiter(Boto3Waiter):
+    """
+    [Waiter.FlowStandby documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/mediaconnect.html#MediaConnect.Waiter.FlowStandby)
+    """
+
+    def wait(self, FlowArn: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [FlowStandby.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/mediaconnect.html#MediaConnect.Waiter.FlowStandby.wait)
+        """
