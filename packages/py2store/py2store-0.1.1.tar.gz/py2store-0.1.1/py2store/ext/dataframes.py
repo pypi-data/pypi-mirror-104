d6df328e541202ed180b6809c7ba4c080d8ac977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:03 +0000

    Add package py2store/0.1.1/py2store-0.1.1.tar.gz

---
+++ packages/py2store/py2store-0.1.1.tar.gz/py2store-0.1.1/py2store/ext/dataframes.py
@@ -0,0 +1,8 @@
+"""Data as `pandas.DataFrame` from various sources"""
+
+from warnings import warn
+
+warn(
+    'Moved to independent package: tabled (pip install tabled)',
+    DeprecationWarning,
+)
