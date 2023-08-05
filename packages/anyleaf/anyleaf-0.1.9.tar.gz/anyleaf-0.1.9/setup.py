4138d8da92bd53e026091c099e5f7ec261579982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:39 +0000

    Add package anyleaf/0.1.9/anyleaf-0.1.9.tar.gz

---
+++ packages/anyleaf/anyleaf-0.1.9.tar.gz/anyleaf-0.1.9/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anyleaf",
+    version="0.1.9",
+    author="Anyleaf",
+    author_email="anyleaf@anyleaf.org",
+    description="Driver for the Anyleaf pH sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://anyleaf.org",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Hardware :: Hardware Drivers ",
+    ],
+    python_requires=">=3.7",
+    license="MIT",
+    install_requires=[
+        'adafruit-circuitpython-ads1x15>=2.2.2',
+        'adafruit-circuitpython-max31865>=2.2.3',
+        'filterpy>=1.4.5',
+        'pyserial>=3.4',
+    ]
+)
