d46e0a07abc1bf598babee3971000884cbbaeb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:15 +0000

    Add package mehrshad/0.1-beta/mehrshad-0.1-beta.tar.gz

---
+++ packages/mehrshad/mehrshad-0.1-beta.tar.gz/mehrshad-0.1-beta/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='mehrshad',
+    packages=['mehrshad'],
+    version='0.1-beta',
+    description='a python package that make your code easier!',
+    author='Mehrshad Dadashzadeh',
+    author_email='mehrdadashzadeh1379@gmail.com',
+    url='https://github.com/mehrshaad/mehrshad-pypi',
+    download_url='https://github.com/mehrshaad/mehrshad-pypi/archive/refs/tags/v0.1-beta.tar.gz',
+    keywords=['json', 'excel', 'pygame', 'mehrshad'],
+    classifiers=[],
+)
