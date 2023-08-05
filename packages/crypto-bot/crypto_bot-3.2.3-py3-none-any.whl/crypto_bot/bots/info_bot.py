e95e620ef0229059021171acdbc65e078064148d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:16 +0000

    Add package crypto-bot/3.2.3/crypto_bot-3.2.3-py3-none-any.whl

---
+++ packages/crypto-bot/crypto_bot-3.2.3-py3-none-any.whl/crypto_bot/bots/info_bot.py
@@ -0,0 +1,273 @@
+import asyncio
+import logging
+import math
+from datetime import datetime, timedelta
+from random import random
+
+import discord
+import pytz
+from dateutil.parser import parse, ParserError
+from discord.ext.commands import Bot, CommandNotFound
+
+from crypto_bot.bots import bot_globals
+
+config_loader = None
+
+
+class CoinAssociation:
+
+    def __init__(self, coin, membership):
+        self.coin = coin
+        self.update(coin)
+        self.membership = membership
+
+    def update(self, coin):
+        self.coin = coin.upper()
+        self.image = None
+
+
+class Countdown():
+
+    def __init__(self, alert_time, name, schedule=None, callback=None, channels=None, alert_date=None, message=None):
+
+        self.alert_date = self.parse_date(alert_date).date() if alert_date else None
+        self.alert_time = self.parse_date(alert_time)
+        if self.alert_date is not None:
+            self.alert_time = datetime.combine(self.alert_date, self.alert_time.time())
+        else:
+            self.alert_time = datetime.combine(datetime.utcnow(), self.alert_time.time())
+        self.alert_time = pytz.timezone("UTC").localize(self.alert_time)
+        self.message = message
+        self.name = name
+        self.logger = logging.getLogger(name)
+        self.channels = channels
+        self.callback = callback
+        if not schedule:
+            self.notifications = {}
+        else:
+            if isinstance(schedule, list):
+                self.schedule = {a: None for a in schedule}
+            else:
+                self.schedule = dict(schedule)
+            self.notifications = self.create_notifications()
+        self.notifications[self.alert_time] = self.message
+
+    def create_notifications(self):
+        return {self.alert_time - timedelta(minutes=t): m for t, m in self.schedule.items()}
+
+    def parse_date(self, date_obj) -> datetime:
+        try:
+            date_obj = parse(date_obj, ignoretz=True)
+            return pytz.timezone("UTC").localize(date_obj)
+        except ParserError:
+            raise AssertionError(
+                "Invalid time format {} - please specific in 24 hour %H:%M:%S".format(date_obj))
+
+    def now(self) -> datetime:
+        return pytz.timezone("UTC").localize(datetime.utcnow())
+
+    def delta_to_event(self):
+        return round((self.alert_time - self.now()).total_seconds() / 60)
+
+    def get_alert_message(self, custom=None):
+        if custom:
+            return custom.replace('%%time%%', str(self.delta_to_event()))
+        return "**Alert!** {} minutes to {}!".format(self.delta_to_event(), self.name)
+
+    async def send_message(self, msg):
+        await self.callback(msg, self.channels)
+
+    async def run_loop(self):
+        n = self.notifications
+        while True:
+            now = self.now()
+            for t in list(n):
+                try:
+                    m = math.floor((now - t).total_seconds() / 60)
+                    if m < 0:
+                        continue
+                    if m == 0:
+                        msg = self.get_alert_message(n[t])
+                        self.logger.debug(msg)
+                        await self.send_message(msg)
+                    if not self.alert_date:
+                        new = t + timedelta(days=1)
+                        n[new] = n[t]
+                        if t == self.alert_time:
+                            self.alert_time = new
+                    else:
+                        self.logger.debug("Discarding old alert: {} {}".format(t, n[t]))
+                    n.pop(t)
+                except Exception as e:
+                    self.logger.error("Error sending notification: {}".format(e))
+            if not n:
+                break
+            await asyncio.sleep(5)
+
+
+class CryptoBot(Bot):
+
+    def __init__(self, token, name, avatar, countdowns, command_roles, indexer, *args, **kwargs):
+        super(CryptoBot, self).__init__(*args, **kwargs)
+        self.token = token
+        self.name = name
+        self.avatar = avatar
+        self.countdowns = [Countdown(**c, callback=self.message_channels) for c in countdowns]
+        self.command_roles = {c.lower() for c in command_roles}
+        self.logger = logging.getLogger("{} bot".format(self.name))
+        self.logger.info("Starting {} bot...".format(self.name))
+        self.indexer = indexer
+
+    async def ready(self):
+        await self.update_nick()
+        for c in self.countdowns:
+            asyncio.ensure_future(c.run_loop())
+
+    async def message_channels(self, msg, channels=None):
+        for c in channels:
+            try:
+                await self.get_channel(c).send(msg)
+            except Exception as e:
+                self.logger.error(e)
+
+    async def update_nick(self):
+        for g in self.guilds:
+            m = [u for u in g.members if u.id == self.user.id]
+            if not m:
+                self.logger.warning("No matching member id found for {}".format(g.id))
+                continue
+            elif len(m) > 1:
+                self.logger.warning("Multiple matches found for id for {} - using first match".format(g.id))
+
+            act = discord.Activity(type=discord.ActivityType.watching, name="you")
+            await m[0].edit(nick="{}".format(self.name))
+            await self.change_presence(status=discord.Status.online, activity=act)
+            if self.avatar:
+                self.logger.info("Updating avatar to: {}".format(self.avatar))
+                with open(self.avatar, 'rb') as image:
+                    await self.user.edit(avatar=image.read())
+                    self.logger.info("Updated avatar")
+
+    async def start(self):
+        await super().start(self.token)
+
+
+def create_bot(token, name, avatar, countdowns, command_roles, indexer):
+    bot = CryptoBot(command_prefix="!",
+                    token=token,
+                    name=name,
+                    avatar=avatar,
+                    countdowns=countdowns,
+                    command_roles=command_roles,
+                    indexer=indexer,
+                    case_insensitive=True)
+
+    @bot.event
+    async def on_ready():
+        bot.logger.info("{} is ready!".format(bot.name))
+        await bot.ready()
+
+    @bot.command(name='feed', help='Get some soup - !feed')
+    async def feed(ctx):
+        if random() >= 0.7:
+            await ctx.send("You may eat today {}, Qapla'!".format(ctx.author.name))
+        else:
+            await ctx.send("No soup for you!")
+
+    @bot.command(name='info', help='Get coin info. Usage: !info DOGE')
+    async def get_info(ctx, symbol):
+        try:
+            c = bot.indexer.get_coin(symbol, wait=True, info=True)
+            message = \
+                """
+**Ticker**: {ticker}
+**Name**: {name}
+**Price**: ${price} / {change} %
+**ATH**: ${ath} on {ath_date}
+**Total Supply**: {supply}
+**Circulating Supply**: {circulating}
+**Percent Mined**: {mined}
+**Market Cap**: {cap}
+**Current Index**: {exchange}
+**Homepage**: <{homepage}>
+**Coingecko**: <{coingecko}>
+**Development**: {repos}"""
+
+            repos = c.info['repos']
+            if repos:
+                repos = ["<{}>".format(z) for z in c.info['repos']]
+                if len(repos) > 1:
+                    repos = "\n" + "\n".join(repos)
+                else:
+                    repos = repos[0]
+
+            d = c.info['ath_date']
+            if isinstance(d, datetime):
+                d = d.strftime('%m/%d/%Y')
+
+            mined = "N/A"
+            supply = "Uncapped"
+            circulating = "Unknown"
+            market_cap = "Unknown"
+            if c.info['circulating_coins']:
+                circulating = "{:,}".format(round(c.info['circulating_coins']))
+            if c.info['market_cap']:
+                market_cap = "$" + "{:,}".format(c.info['market_cap'])
+            if c.info['total_coins']:
+                supply = "{:,}".format(round(c.info['total_coins']))
+                if circulating != "Unknown":
+                    mined = round(c.info['circulating_coins'] / c.info['total_coins'] * 100, 2)
+                    mined = str(mined) + "%"
+
+            msg = message.format(
+                image=c.info['image'],
+                ticker=symbol.upper(),
+                name=c.name,
+                price=c.price,
+                change=c.perc,
+                ath=c.info['ath'],
+                ath_date=d,
+                supply=supply,
+                mined=mined,
+                circulating=circulating,
+                cap=market_cap,
+                exchange=c.last_exchange,
+                homepage=c.info['homepage'],
+                coingecko=c.info['coingecko'],
+                repos=repos or "None provided"
+            )
+
+            if c.info['algorithm']:
+                message += "\n**Hashing Algorithm**: {}".format(c.info['algorithm'])
+            if c.info['block_time']:
+                message += "\n**Block Time**: {} minutes".format(c.info['block_time'])
+
+            await ctx.send(c.info['image'])
+            await ctx.send(msg)
+            desc = c.info['description']
+            message = "**Description**: \n{description}"
+            if not desc:
+                await ctx.send("**Description**: None provided")
+                return
+            if len(desc) > 500:
+                message += "... \nTruncated - for a longer description, see <{}> ".format(c.info['coingecko'])
+            await ctx.send(message.format(description=desc[0:500]))
+
+        except Exception as e:
+            await ctx.send("Error: {}".format(e))
+
+    @bot.command(name='price', help='Get a price. Usage: !price doge')
+    async def get_price(ctx, symbol):
+        await bot_globals.get_symbol_price(ctx, symbol)
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, CommandNotFound):
+            try:
+                int(ctx.invoked_with)
+                return
+            except Exception:
+                await ctx.send(error.args[0])
+        raise error
+
+    return bot
---
+++ packages/crypto-bot/crypto_bot-3.2.3-py3-none-any.whl/crypto_bot/exchanges.py
@@ -0,0 +1,226 @@
+import collections
+import logging
+import threading
+import time
+from datetime import datetime
+from html.parser import HTMLParser
+from io import StringIO
+
+import requests
+
+from crypto_bot.error import CoinNotFoundException
+from crypto_bot.price_indexer import Coin
+
+
+class Exchange:
+    hard_coins = {}
+
+    def __init__(self, config):
+        self.priority = int(config['priority'])
+        self.base_url = config['api_url']
+        self.name = None
+        self.coins = {}
+        self.logger = logging.getLogger("connector")
+        self.ready = False
+        self.coins_path = None
+        self.ticker_path = None
+
+    def call(self, url, method="GET", headers=None, data=None, json=True):
+        r = requests.request(method=method, url=url, data=data or {}, headers=headers or {})
+        if r.status_code is not 200:
+            raise requests.RequestException("{}: {}".format(r.status_code, r.content))
+        return r.json() if json else r.content
+
+    def get_tickers(self, symbols):
+
+        coins = {}
+        if not symbols:
+            return coins
+        symbols = {symbols} if isinstance(symbols, str) else set(symbols)
+
+        for c in symbols:
+            try:
+                coin = self.get_coin_def(c)
+                coins[coin.coin_id] = coin
+            except CoinNotFoundException:
+                continue
+        return self.get_ticker_range(coins)
+
+    def get_coin_def(self, symbol, raises=True):
+        c = self.coins.get(symbol.lower())
+        if not c and raises:
+            raise CoinNotFoundException(symbol, self.name)
+        return c
+
+    def get_ticker_range(self, coins):
+        pass
+
+    def get_ticker(self, symbol):
+        return self.get_tickers(symbol).get(symbol)
+
+    def parse_ticker(self, d):
+        pass
+
+    def parse_coins_response(self, resp) -> list:
+        pass
+
+    @classmethod
+    def create(self, name, config):
+        name = name.lower()
+        if name == "coingecko":
+            return CoinGeckoExchange(config)
+        if name == "kucoin":
+            return KucoinExchange(config)
+        else:
+            raise ValueError("Unknown exchange: {}".format(name))
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+
+class CoinGeckoExchange(Exchange):
+    hard_coins = {
+        'one': 'harmony'
+    }
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.coins_path = "/coins/list"
+        self.coins_info_path = "/coins"
+        self.ticker_path = "/simple/price?ids={}&vs_currencies=usd&include_24hr_change=true"
+        self.name = "coingecko"
+        threading.Thread(target=self.get_coins).start()
+
+    def get_coins(self):
+        while True:
+            try:
+                response = self.call(self.base_url + self.coins_path)
+                for coin in self.parse_coins_response(response):
+                    if coin.symbol in self.hard_coins and coin.coin_id != self.hard_coins[coin.symbol]:
+                        continue
+                    if coin.symbol not in self.coins:
+                        self.coins[coin.symbol] = coin
+                    else:
+                        self.coins[coin.symbol].coin_id = coin.coin_id
+                        self.coins[coin.symbol].name = coin.name
+            except Exception as e:
+                self.logger.error(e)
+            self.ready = True
+            time.sleep(650)
+
+    def get_coin_info(self, symbol):
+        symbol = symbol.lower()
+        if symbol.lower() not in self.coins:
+            raise CoinNotFoundException(symbol)
+
+        cid = self.coins[symbol].coin_id
+        info = self.call(self.base_url + "/{}/{}".format(self.coins_info_path, cid))
+        info_dict = {
+            'name': info.get('name'),
+            'homepage': self.get_nested_key(info, ['links', 'homepage'])[0],
+            'reddit': self.get_nested_key(info, ['links', 'subreddit_url']),
+            'coingecko': 'https://www.coingecko.com/en/coins/{}'.format(cid),
+            'total_coins': self.get_nested_key(info, ['market_data', 'total_supply']),
+            'circulating_coins': self.get_nested_key(info, ['market_data', 'circulating_supply']),
+            'market_cap': self.get_nested_key(info, ['market_data', 'market_cap', 'usd']),
+            'ath': self.get_nested_key(info, ['market_data', 'ath', 'usd']),
+            'algorithm': info.get('hashing_algorithm'),
+            'block_time': info.get('block_time_in_minutes'),
+            'image': self.get_nested_key(info, ['image', 'small']),
+        }
+
+        entries = []
+        repos = self.get_nested_key(info, ['links', 'repos_url'])
+        if repos is not None:
+            for r in repos.values():
+                entries.extend(r)
+        info_dict['repos'] = entries
+
+        desc = self.get_nested_key(info, ['description', 'en'])
+        if desc is not None:
+            desc = self.strip_tags(desc)
+        info_dict['description'] = desc
+
+        ath_date = self.get_nested_key(info, ['market_data', 'ath_date', 'usd'])
+        if ath_date is not None:
+            ath_date = datetime.strptime(ath_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+        info_dict['ath_date'] = ath_date
+        return info_dict
+
+    def strip_tags(self, html):
+        s = MLStripper()
+        s.feed(html)
+        return s.get_data()
+
+    def get_nested_key(self, d, keys):
+        for k in keys:
+            if d is None or not isinstance(d, collections.Mapping):
+                break
+            d = d.get(k)
+        return d
+
+    def get_ticker_range(self, coins):
+        l = ",".join(coins.keys())
+        tickers = self.call(self.base_url + self.ticker_path.format(l))
+        return {coins[t].symbol: self.parse_ticker(d) for t, d in tickers.items()}
+
+    def parse_ticker(self, d):
+        price = d['usd']
+        perc = d['usd_24h_change']
+        return price, perc
+
+    def parse_coins_response(self, resp):
+        if not isinstance(resp, list):
+            raise AssertionError("Response is not a list of coins")
+
+        parsed_coins = []
+        for c in resp:
+            try:
+                parsed_coins.append(Coin(c['id'], c['symbol'], c['name'], exchange=self.name))
+            except Exception as e:
+                self.logger.error(e)
+        return parsed_coins
+
+
+class KucoinExchange(Exchange):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.coins_path = "/api/v1/market/allTickers"
+        self.update_rate = config['update_rate']
+        self.name = "kucoin"
+        threading.Thread(target=self.get_coins).start()
+
+    def get_coins(self):
+        while True:
+            try:
+                response = self.call(self.base_url + self.coins_path)
+                for coin in response['data']['ticker']:
+                    s = coin['symbol'].lower()
+                    if 'usdt' not in s:
+                        continue
+                    s = s.split('-')[0]
+                    if s not in self.coins:
+                        self.coins[s] = Coin(coin['symbol'], symbol=s, exchange=self.name)
+                    else:
+                        self.coins[s].coin_id = coin['symbol']
+                    self.coins[s].update(float(coin['last']), float(coin['changeRate']))
+            except Exception as e:
+                self.logger.error(e)
+            self.ready = True
+            time.sleep(self.update_rate)
+
+    def get_ticker_range(self, symbols):
+        return {v.symbol: (v.price, v.perc) for v in symbols.values()}
