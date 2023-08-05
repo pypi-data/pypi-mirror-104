64d9d119639240aa4199b7317b8c2334735400e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:06 +0000

    Add package straitjacket/202104.1018/straitjacket-202104.1018.tar.gz

---
+++ packages/straitjacket/straitjacket-202104.1018.tar.gz/straitjacket-202104.1018/src/straitjacket/sjfmtd.py
@@ -0,0 +1,21 @@
+# This file is part of the straitjacket project
+# https://github.com/mbarkhau/straitjacket
+#
+# Copyright (c) 2018-2021 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+import click
+
+from sjfmt_vendor import black
+from sjfmt_vendor import blackd
+from straitjacket import sjfmt
+
+
+def main(*args, **kwargs) -> None:
+    black.patch_click()
+    blackd.main = click.version_option(version=sjfmt.__version__)(black.main)
+    blackd.main(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    main()
