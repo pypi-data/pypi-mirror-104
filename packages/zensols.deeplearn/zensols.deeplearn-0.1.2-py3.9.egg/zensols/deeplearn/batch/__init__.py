3512ca7375775d98c2959bcb052e592c88bc56f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:56 +0000

    Add package zensols.deeplearn/0.1.2/zensols.deeplearn-0.1.2-py3.9.egg

---
+++ packages/zensols.deeplearn/zensols.deeplearn-0.1.2-py3.9.egg/zensols/deeplearn/batch/__init__.py
@@ -0,0 +1,8 @@
+"""Contains classes that batch vectorized data in forked subprocesses.
+
+"""
+from .multi import *
+from .mapping import *
+from .stash import *
+from .domain import *
+from .meta import *
