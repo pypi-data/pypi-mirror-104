039a79a79c8d3fbeca7f67a76312fd479f93f7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:07 +0000

    Add package cmc-csci046-emmagodfrey/1.0.0/cmc_csci046_emmagodfrey-1.0.0.tar.gz

---
+++ packages/cmc-csci046-emmagodfrey/cmc_csci046_emmagodfrey-1.0.0.tar.gz/cmc_csci046_emmagodfrey-1.0.0/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cmc_csci046_emmagodfrey",
+    version="1.0.0",
+    description="Implementations of a binary tree, binary search tree, avl tree, and a heap. Additional small projects included.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/emmacgodfrey/containers2",
+    author="Emma Godfrey",
+    author_email="emma.godfrey@pomona.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests")),
+    include_package_data=True,
+    install_requires=["unicodedata","copy"],
+    entry_points={
+        "console_scripts": [
+            "cmc_csci046_emmagodfrey = containers2.__main__:main",
+        ]
+    },
+)
