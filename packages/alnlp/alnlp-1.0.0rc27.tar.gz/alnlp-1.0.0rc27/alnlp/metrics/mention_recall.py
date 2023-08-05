729b96cb9a5fd5345394c3c0ac69e8ae1c5dcccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:51 +0000

    Add package alnlp/1.0.0rc27/alnlp-1.0.0rc27.tar.gz

---
+++ packages/alnlp/alnlp-1.0.0rc27.tar.gz/alnlp-1.0.0rc27/alnlp/metrics/mention_recall.py
@@ -0,0 +1,39 @@
+from typing import Any, Dict, List, Set, Tuple
+
+from alnlp.metrics import Metric
+
+
+import torch
+
+
+class MentionRecall(Metric):
+    def __init__(self) -> None:
+        self._num_gold_mentions = 0
+        self._num_recalled_mentions = 0
+
+
+    def __call__(
+            self,  # type: ignore
+            batched_top_spans: torch.Tensor,
+            batch: Dict[str, List],
+    ):
+        for top_spans, clusters in zip(batched_top_spans.tolist(), batch["clusters"]):
+            gold_mentions: Set[Tuple[int, int]] = set(mention for cluster in clusters for mention in cluster)
+            predicted_spans: Set[Tuple[int, int]] = {(span[0], span[1]) for span in top_spans}
+            self._num_gold_mentions += len(gold_mentions)
+            self._num_recalled_mentions += len(gold_mentions & predicted_spans)
+
+
+    def get_metric(self, reset: bool = False) -> float:
+        if self._num_gold_mentions == 0:
+            recall = 0.0
+        else:
+            recall = self._num_recalled_mentions / self._num_gold_mentions
+        if reset:
+            self.reset()
+        return recall
+
+
+    def reset(self):
+        self._num_gold_mentions = 0
+        self._num_recalled_mentions = 0
