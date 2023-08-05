bfb119ff6d913d737f01a14654a39269654e0bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:13 +0000

    Add package probability-distributions-drew/0.2/probability_distributions_drew-0.2.tar.gz

---
+++ packages/probability-distributions-drew/probability_distributions_drew-0.2.tar.gz/probability_distributions_drew-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distributions_drew',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions_drew'],
+      author='Andrew Sithole',
+      author_email='andrewsithole7@gmail.com',
+      zip_safe=False)
