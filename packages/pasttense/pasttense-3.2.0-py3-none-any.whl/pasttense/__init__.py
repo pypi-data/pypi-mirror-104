404785d17c6a6d06106f4aff8946050a95f939ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package pasttense/3.2.0/pasttense-3.2.0-py3-none-any.whl

---
+++ packages/pasttense/pasttense-3.2.0-py3-none-any.whl/pasttense/__init__.py
@@ -0,0 +1,18 @@
+class Past:
+    def __init__(self, past):
+        """
+        self.past = past
+        past.hello = str(input("Enter a word:"))
+        """
+        self.past = past
+
+        def f(v):
+            T,x,m='aeiou',"ed",v[-1];return[[[v+x,v+m+x][v[-2]in T and m and v[-3]not in T],[v+x,v[:-1]+"ied"][v[-2]not in T]][m=='y'],v+"d"][m=='e']
+        if past == "go":
+            print("went")
+        elif past == "get":
+            print("got")
+        elif past == "eat":
+            print("ate")
+        else:
+            print(f(past))
