afe9dbf6c0123359df77ced25fbb776db2eab883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:06 +0000

    Add package columntracer/1.0.2/columntracer-1.0.2.tar.gz

---
+++ packages/columntracer/columntracer-1.0.2.tar.gz/columntracer-1.0.2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parents
+
+README = (HERE[0]/"README.md").read_text()
+
+setup(
+      name='columntracer',
+      version='1.0.2',
+      description='first version',
+ 	  long_description=README,
+ 	  long_description_content_type="text/markdown",
+      packages=['columntracer'],
+      install_requires=['numpy', 'matplotlib', 'scipy'])
