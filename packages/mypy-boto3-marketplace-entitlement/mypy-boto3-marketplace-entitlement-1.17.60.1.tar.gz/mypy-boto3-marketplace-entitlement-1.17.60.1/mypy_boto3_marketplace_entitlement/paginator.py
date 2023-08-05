9935b1704e822d0f04ee21953a088843b427b6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package mypy-boto3-marketplace-entitlement/1.17.60.1/mypy-boto3-marketplace-entitlement-1.17.60.1.tar.gz

---
+++ packages/mypy-boto3-marketplace-entitlement/mypy-boto3-marketplace-entitlement-1.17.60.1.tar.gz/mypy-boto3-marketplace-entitlement-1.17.60.1/mypy_boto3_marketplace_entitlement/paginator.py
@@ -0,0 +1,45 @@
+"""
+Main interface for marketplace-entitlement service client paginators.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_marketplace_entitlement import MarketplaceEntitlementServiceClient
+    from mypy_boto3_marketplace_entitlement.paginator import (
+        GetEntitlementsPaginator,
+    )
+
+    client: MarketplaceEntitlementServiceClient = boto3.client("marketplace-entitlement")
+
+    get_entitlements_paginator: GetEntitlementsPaginator = client.get_paginator("get_entitlements")
+    ```
+"""
+from typing import Dict, Iterator, List
+
+from botocore.paginate import Paginator as Boto3Paginator
+
+from mypy_boto3_marketplace_entitlement.literals import GetEntitlementFilterName
+from mypy_boto3_marketplace_entitlement.type_defs import (
+    GetEntitlementsResultTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = ("GetEntitlementsPaginator",)
+
+
+class GetEntitlementsPaginator(Boto3Paginator):
+    """
+    [Paginator.GetEntitlements documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/marketplace-entitlement.html#MarketplaceEntitlementService.Paginator.GetEntitlements)
+    """
+
+    def paginate(
+        self,
+        ProductCode: str,
+        Filter: Dict[GetEntitlementFilterName, List[str]] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Iterator[GetEntitlementsResultTypeDef]:
+        """
+        [GetEntitlements.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/marketplace-entitlement.html#MarketplaceEntitlementService.Paginator.GetEntitlements.paginate)
+        """
