d8d1516a3a26d146a79bc27116ce90e2d9061bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:19 +0000

    Add package mypy-boto3-chime/1.17.60.1/mypy_boto3_chime-1.17.60.1-py3-none-any.whl

---
+++ packages/mypy-boto3-chime/mypy_boto3_chime-1.17.60.1-py3-none-any.whl/mypy_boto3_chime/paginator.pyi
@@ -0,0 +1,63 @@
+"""
+Main interface for chime service client paginators.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_chime import ChimeClient
+    from mypy_boto3_chime.paginator import (
+        ListAccountsPaginator,
+        ListUsersPaginator,
+    )
+
+    client: ChimeClient = boto3.client("chime")
+
+    list_accounts_paginator: ListAccountsPaginator = client.get_paginator("list_accounts")
+    list_users_paginator: ListUsersPaginator = client.get_paginator("list_users")
+    ```
+"""
+from typing import Iterator
+
+from botocore.paginate import Paginator as Boto3Paginator
+
+from mypy_boto3_chime.literals import UserType
+from mypy_boto3_chime.type_defs import (
+    ListAccountsResponseTypeDef,
+    ListUsersResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+__all__ = ("ListAccountsPaginator", "ListUsersPaginator")
+
+class ListAccountsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListAccounts documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/chime.html#Chime.Paginator.ListAccounts)
+    """
+
+    def paginate(
+        self,
+        Name: str = None,
+        UserEmail: str = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Iterator[ListAccountsResponseTypeDef]:
+        """
+        [ListAccounts.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/chime.html#Chime.Paginator.ListAccounts.paginate)
+        """
+
+class ListUsersPaginator(Boto3Paginator):
+    """
+    [Paginator.ListUsers documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/chime.html#Chime.Paginator.ListUsers)
+    """
+
+    def paginate(
+        self,
+        AccountId: str,
+        UserEmail: str = None,
+        UserType: UserType = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Iterator[ListUsersResponseTypeDef]:
+        """
+        [ListUsers.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.60/reference/services/chime.html#Chime.Paginator.ListUsers.paginate)
+        """
