3e41bf3f125153af5dca87d78bb9a52b688c31d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:01 +0000

    Add package dencam/0.0.1/dencam-0.0.1.tar.gz

---
+++ packages/dencam/dencam-0.0.1.tar.gz/dencam-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(PATH, "README.md")) as f:
+    README_TEXT = f.read()
+
+AUTHOR = "Conservation Technology Lab at the San Diego Zoo Wildlife Alliance"
+DESC = "Code for MiniDencam: Polar bear maternal den observation system."
+
+setup(
+    name="dencam",
+    version="0.0.1",
+    author=AUTHOR,
+    description=DESC,
+    long_description=README_TEXT,
+    long_description_content_type="text/markdown",
+    url="https://github.com/icr-ctl/dencam",
+    license="MIT",
+    packages=['dencam'],
+    include_package_data=True,
+    install_requires=[
+        'pyyaml',
+        'rpi.gpio',
+        'picamera',
+        'netifaces',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={"console_scripts": ["dencam=dencam.__main__:main"]},
+)
