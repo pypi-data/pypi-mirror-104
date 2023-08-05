d6e1697565776d48a939943dd138f1735ba3fd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:48 +0000

    Add package cortx-jupyter/0.1.131/cortx_jupyter-0.1.131.tar.gz

---
+++ packages/cortx-jupyter/cortx_jupyter-0.1.131.tar.gz/cortx_jupyter-0.1.131/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cortx_jupyter',
+    version='0.1.131',
+    author='Sumanth Reddy Muni & Priyadarshini Murugan',
+    description='Jupyter Notebook Manager for Cortx',
+    long_description=long_description,
+    long_description_content_type= "text/markdown",
+    url='https://github.com/sumanthreddym/cortx_jupyter',
+    packages=setuptools.find_packages()
+)
