298ad2c747b6c7b49dcc65b18a12d8f37ddf1c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:30 +0000

    Add package python-script-manager/1.3.3/python-script-manager-1.3.3.tar.gz

---
+++ packages/python-script-manager/python-script-manager-1.3.3.tar.gz/python-script-manager-1.3.3/README.md
@@ -0,0 +1,11 @@
+[![Documentation Status](https://readthedocs.org/projects/python-script-manager/badge/?version=latest)](https://python-script-manager.readthedocs.io/en/latest/?badge=latest)
+
+# <img src="https://raw.githubusercontent.com/YunisDEV/python-script-manager/master/assets/psm.svg"/> Welcome to PSM
+
+## What is PSM?
+**PSM** is a tool that helps you to manage and develop your Python apps easily.
+
+## Learn more about PSM
+
+Full documentation is available at readthedocs:
+[python-script-manager.readthedocs.io](https://python-script-manager.readthedocs.io)
---
+++ packages/python-script-manager/python-script-manager-1.3.3.tar.gz/python-script-manager-1.3.3/python_script_manager/templates/flaskmng.py
@@ -0,0 +1,30 @@
+flaskmng = {
+    "body": {
+        "scripts":{
+            "newapp":{
+                "command":"flaskmng startapp",
+                "description":"Creating new app for project."
+            },
+            "rmapp":{
+                "command":"flaskmng removeapp",
+                "description":"Removing app from project."
+            },
+            "mkmig":{
+                "command":"flask db migrate -m \"{m}\"",
+                "description":"Make migrations for project."
+            },
+            "mig":{
+                "command":"flask db upgrade",
+                "description":"Upgrade db for project."
+            },
+            "dbinit":{
+                "command":"flask db init",
+                "description":"Initialize db for project."
+            },
+            "start":{
+                "command":"flask run",
+                "description":"For starting project."
+            }
+        }
+    }
+}
