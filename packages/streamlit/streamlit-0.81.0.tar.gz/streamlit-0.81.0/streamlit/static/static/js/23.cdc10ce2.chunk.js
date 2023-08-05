c2f1a7d728878b365078187c1b9aa90ef1d2e1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:58 +0000

    Add package streamlit/0.81.0/streamlit-0.81.0.tar.gz

---
+++ packages/streamlit/streamlit-0.81.0.tar.gz/streamlit-0.81.0/streamlit/static/static/js/23.cdc10ce2.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 23.cdc10ce2.chunk.js.LICENSE.txt */
+(this["webpackJsonpstreamlit-browser"]=this["webpackJsonpstreamlit-browser"]||[]).push([[23],{4035:function(t,e,r){"use strict";r.r(e),r.d(e,"default",(function(){return i}));var s=r(0),u=r(68),c=r(5);function i(t){var e=t.element,r=t.width,i=Object(s.useRef)(null);Object(s.useEffect)((function(){i.current&&(i.current.currentTime=e.startTime)}),[e.startTime]);var n=Object(u.b)(e.url);return Object(c.jsx)("audio",{id:"audio",ref:i,controls:!0,src:n,className:"stAudio",style:{width:r}})}}}]);
+//# sourceMappingURL=23.cdc10ce2.chunk.js.map
