f99515dfff77fe552b0a609b0390fefa2371d594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:53 +0000

    Add package mypy-boto3-meteringmarketplace/1.17.61.0/mypy-boto3-meteringmarketplace-1.17.61.0.tar.gz

---
+++ packages/mypy-boto3-meteringmarketplace/mypy-boto3-meteringmarketplace-1.17.61.0.tar.gz/mypy-boto3-meteringmarketplace-1.17.61.0/mypy_boto3_meteringmarketplace/literals.pyi
@@ -0,0 +1,21 @@
+"""
+Main interface for meteringmarketplace service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_meteringmarketplace.literals import UsageRecordResultStatus
+
+    data: UsageRecordResultStatus = "CustomerNotSubscribed"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("UsageRecordResultStatus",)
+
+UsageRecordResultStatus = Literal["CustomerNotSubscribed", "DuplicateRecord", "Success"]
