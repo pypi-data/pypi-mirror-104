48cae4cdd66dadaf968f6a5f126b1494382a34a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package mypy-boto3-iotevents-data/1.17.61.0/mypy_boto3_iotevents_data-1.17.61.0-py3-none-any.whl

---
+++ packages/mypy-boto3-iotevents-data/mypy_boto3_iotevents_data-1.17.61.0-py3-none-any.whl/mypy_boto3_iotevents_data/literals.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for iotevents-data service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_iotevents_data.literals import ErrorCode
+
+    data: ErrorCode = "InternalFailureException"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("ErrorCode",)
+
+ErrorCode = Literal[
+    "InternalFailureException",
+    "InvalidRequestException",
+    "ResourceNotFoundException",
+    "ServiceUnavailableException",
+    "ThrottlingException",
+]
