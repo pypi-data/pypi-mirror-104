7c5f3ce4006cb50a0911bafa20b6873243da4ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package mehrshad/0.2-beta/mehrshad-0.2-beta.tar.gz

---
+++ packages/mehrshad/mehrshad-0.2-beta.tar.gz/mehrshad-0.2-beta/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='mehrshad',
+    packages=['mehrshad'],
+    version='0.2-beta',
+    description='A Python 3 module that contains all of my collections to let you code easier!',
+    author='Mehrshad Dadashzadeh',
+    author_email='mehrdadashzadeh1379@gmail.com',
+    url='https://github.com/mehrshaad/mehrshad-pypi',
+    license='MIT',
+    download_url='https://github.com/mehrshaad/mehrshad-pypi/archive/refs/tags/v0.2-beta.tar.gz',
+    keywords=['mehrshad', 'json', 'excel', 'pygame'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
