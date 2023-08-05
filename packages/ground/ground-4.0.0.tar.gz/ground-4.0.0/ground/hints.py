a98f8d4c5f949fe9eca5137fd1f05fac3f7de3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:21 +0000

    Add package ground/4.0.0/ground-4.0.0.tar.gz

---
+++ packages/ground/ground-4.0.0.tar.gz/ground-4.0.0/ground/hints.py
@@ -0,0 +1,25 @@
+from .core.hints import (Box,
+                         Contour,
+                         Empty,
+                         Maybe,
+                         Mix,
+                         Multipoint,
+                         Multipolygon,
+                         Multisegment,
+                         Point,
+                         Polygon,
+                         Scalar,
+                         Segment)
+
+Box = Box
+Contour = Contour
+Empty = Empty
+Maybe = Maybe
+Mix = Mix
+Multipoint = Multipoint
+Multipolygon = Multipolygon
+Multisegment = Multisegment
+Point = Point
+Polygon = Polygon
+Scalar = Scalar
+Segment = Segment
