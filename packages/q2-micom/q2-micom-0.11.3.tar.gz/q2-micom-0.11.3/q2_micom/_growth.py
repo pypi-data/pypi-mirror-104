9cd9d2abcabcee13826b235c02a62952f79a809f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:19 +0000

    Add package q2-micom/0.11.3/q2-micom-0.11.3.tar.gz

---
+++ packages/q2-micom/q2-micom-0.11.3.tar.gz/q2-micom-0.11.3/q2_micom/_growth.py
@@ -0,0 +1,21 @@
+"""Performs a growth simulation."""
+
+import micom.workflows as mw
+import pandas as pd
+from q2_micom._formats_and_types import CommunityModelDirectory
+
+
+def grow(
+    models: CommunityModelDirectory,
+    medium: pd.DataFrame,
+    tradeoff: float = 0.5,
+    threads: int = 1,
+) -> mw.core.GrowthResults:
+    """Simulate growth for a set of community models."""
+    model_folder = str(models.model_files.path_maker(model_id="blub")).replace(
+        "blub.pickle", ""
+    )
+    manifest = models.manifest.view(pd.DataFrame)
+    results = mw.grow(manifest, model_folder, medium, tradeoff, threads)
+
+    return results
