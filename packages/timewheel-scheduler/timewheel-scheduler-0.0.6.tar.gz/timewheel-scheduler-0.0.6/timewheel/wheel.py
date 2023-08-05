de71ef4666949501e528477dc816ba3483f2f5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:45 +0000

    Add package timewheel-scheduler/0.0.6/timewheel-scheduler-0.0.6.tar.gz

---
+++ packages/timewheel-scheduler/timewheel-scheduler-0.0.6.tar.gz/timewheel-scheduler-0.0.6/timewheel/wheel.py
@@ -0,0 +1,48 @@
+import logging
+import asyncio
+from typing import List
+from datetime import datetime
+
+from timewheel.schedule import Schedule
+
+SLEEP_TIME = 1
+
+
+class TimeWheel:
+
+    def __init__(self, schedules: List[Schedule]):
+        self.logger = logging.getLogger("timewheel")
+        self.schedules = schedules
+        self.running = True
+
+    async def run(self):
+        """
+        Starts the scheduler loop
+
+        :return:
+        """
+        timer = 0
+        while self.running:
+            # Only check the schedule every 60 seconds
+            if timer % 60 == 0:
+                timer = 0
+                now = datetime.now()
+                for schedule in self.schedules:
+                    if not schedule.running:
+                        asyncio.create_task(schedule.run(now))
+            await asyncio.sleep(SLEEP_TIME)
+            timer += SLEEP_TIME
+        self.logger.warning("Finished the timewheel loop!")
+
+    async def kill_jobs(self):
+        """
+            When the application receives a system signal to terminate
+            invokes the finish methods for all schedules in order to
+            wait them finish the jobs if there are any running.
+        """
+
+        self.logger.warning("Received system signal to finish!")
+        for schedule in self.schedules:
+            self.logger.debug(f"Waiting for the schedule {schedule.name} to finish...")
+            await schedule.finish()
+        self.running = False
