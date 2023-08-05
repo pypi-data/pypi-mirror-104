7800f5daba7491af9f6fdcd8c7c22921ea8c0696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:05 +0000

    Add package hippolyzer/0.0.0/hippolyzer-0.0.0.tar.gz

---
+++ packages/hippolyzer/hippolyzer-0.0.0.tar.gz/hippolyzer-0.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+# This call to setup() does all the work
+setup(
+    name="hippolyzer",
+    version="0.0.0",
+    description="Stub for pending packaging of hippolyzer",
+    long_description="Stub for pending packaging of hippolyzer",
+    license="LGPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["hippolyzer"],
+    include_package_data=False,
+    install_requires=[],
+    entry_points={
+    },
+)
