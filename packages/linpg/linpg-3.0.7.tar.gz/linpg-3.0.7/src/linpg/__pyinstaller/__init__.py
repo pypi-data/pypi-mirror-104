22c59b0ec6aae1c503b62bd02d0666f41723d621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:37 +0000

    Add package linpg/3.0.7/linpg-3.0.7.tar.gz

---
+++ packages/linpg/linpg-3.0.7.tar.gz/linpg-3.0.7/src/linpg/__pyinstaller/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+def get_hook_dirs() -> list: return [os.path.dirname(__file__)]
---
+++ packages/linpg/linpg-3.0.7.tar.gz/linpg-3.0.7/src/linpg/__pyinstaller/hook-linpg.py
@@ -0,0 +1,17 @@
+import os
+import linpg
+
+LINPG_PATH:str = linpg.__path__[0]
+
+datas = [
+    (os.path.join(LINPG_PATH,'api/*'),"linpg/api"),
+    (os.path.join(LINPG_PATH,'battle/*'),"linpg/battle"),
+    (os.path.join(LINPG_PATH,'config/*'),"linpg/config"),
+    (os.path.join(LINPG_PATH,'core/*'),"linpg/core"),
+    (os.path.join(LINPG_PATH,'dialog/*'),"linpg/dialog"),
+    (os.path.join(LINPG_PATH,'lang/*'),"linpg/lang"),
+    (os.path.join(LINPG_PATH,'__init__.py'),'linpg'),
+    (os.path.join(LINPG_PATH,'LICENSE'),'linpg'),
+    (os.path.join(LINPG_PATH,'README.md'),'linpg'),
+    (os.path.join(LINPG_PATH,'CODE_OF_CONDUCT.md'),'linpg'),
+    ]
