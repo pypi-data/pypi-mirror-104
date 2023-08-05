824ebe43d3fa83ce4f8bf387f389e6c419acec24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:53 +0000

    Add package mypy-boto3-migrationhub-config/1.17.61.0/mypy-boto3-migrationhub-config-1.17.61.0.tar.gz

---
+++ packages/mypy-boto3-migrationhub-config/mypy-boto3-migrationhub-config-1.17.61.0.tar.gz/mypy-boto3-migrationhub-config-1.17.61.0/mypy_boto3_migrationhub_config/literals.pyi
@@ -0,0 +1,21 @@
+"""
+Main interface for migrationhub-config service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_migrationhub_config.literals import TargetType
+
+    data: TargetType = "ACCOUNT"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("TargetType",)
+
+TargetType = Literal["ACCOUNT"]
