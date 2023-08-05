91e8918c8c7b151085f1fcb696d7d81fa4ed9b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:57 +0000

    Add package thebrukhellomodule/2.0/thebrukhellomodule-2.0.tar.gz

---
+++ packages/thebrukhellomodule/thebrukhellomodule-2.0.tar.gz/thebrukhellomodule-2.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='thebrukhellomodule',
+      version='2.0',
+      description='Hello!',
+      packages=['thebrukhellomodule'],
+      author_email='thebrukprojects@gmail.com',
+      zip_safe=False)
