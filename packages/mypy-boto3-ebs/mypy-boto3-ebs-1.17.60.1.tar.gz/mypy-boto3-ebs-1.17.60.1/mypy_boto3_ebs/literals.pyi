121cb0ca8d7e667b98424ab06a84fcc7abaa9892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package mypy-boto3-ebs/1.17.60.1/mypy-boto3-ebs-1.17.60.1.tar.gz

---
+++ packages/mypy-boto3-ebs/mypy-boto3-ebs-1.17.60.1.tar.gz/mypy-boto3-ebs-1.17.60.1/mypy_boto3_ebs/literals.pyi
@@ -0,0 +1,23 @@
+"""
+Main interface for ebs service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_ebs.literals import ChecksumAggregationMethod
+
+    data: ChecksumAggregationMethod = "LINEAR"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("ChecksumAggregationMethod", "ChecksumAlgorithm", "Status")
+
+ChecksumAggregationMethod = Literal["LINEAR"]
+ChecksumAlgorithm = Literal["SHA256"]
+Status = Literal["completed", "error", "pending"]
