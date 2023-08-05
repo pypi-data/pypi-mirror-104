cf961cff9aa1df233ac51b9ef90eb6eabe679ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:55 +0000

    Add package custom-loggers/1.1.0/custom_loggers-1.1.0.tar.gz

---
+++ packages/custom-loggers/custom_loggers-1.1.0.tar.gz/custom_loggers-1.1.0/custom_loggers/__init__.py
@@ -0,0 +1,4 @@
+from custom_loggers.CustomLogger import CustomLogger
+from custom_loggers.ColoredFormatter import ColoredFormatter
+from custom_loggers.Colors import ForeGroundColors, BackGroundColors, Foreground255, Background255, SequenceName, \
+    print_16_colors, print_255_colors, print_styles, FontStyles
