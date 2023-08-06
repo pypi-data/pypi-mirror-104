3971aaa13d14e3fcb6ca82e69247d54f5f98ba4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:42 +0000

    Add package pytest-flyte/0.0.1/pytest-flyte-0.0.1.tar.gz

---
+++ packages/pytest-flyte/pytest-flyte-0.0.1.tar.gz/pytest-flyte-0.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+
+__version__ = "0.0.1"
+
+
+setup(
+    name="pytest-flyte",
+    version=__version__,
+    packages=find_packages("src"),
+    entry_points={"pytest11": ["flyte = pytest_flyte.plugin"]},
+    package_dir={"": "src"},
+    description="Pytest fixtures for simplifying Flyte integration testing",
+    include_package_data=True,
+    install_requires=[
+        "docker-compose",
+        "flytekit",
+        "jinja2",
+        "pytest",
+        "pytest-docker",
+    ],
+)
---
+++ packages/pytest-flyte/pytest-flyte-0.0.1.tar.gz/pytest-flyte-0.0.1/src/pytest_flyte/__init__.py
@@ -0,0 +1,15 @@
+from pytest_flyte.plugin import (
+    capsys_suspender,
+    docker_cleanup,
+    docker_compose,
+    docker_compose_file,
+    docker_compose_project_name,
+    flyte_workflows_register,
+    flyte_workflows_source_dir,
+    flyteclient,
+    kustomization_file,
+    template_cache,
+)
+
+
+__version__ = "0.0.1"
