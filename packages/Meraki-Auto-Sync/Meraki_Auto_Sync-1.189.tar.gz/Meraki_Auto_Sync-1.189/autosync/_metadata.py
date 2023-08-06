5f54ed8c43b812c9989f3527c5f1744a1190a6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:38 +0000

    Add package Meraki-Auto-Sync/1.189/Meraki_Auto_Sync-1.189.tar.gz

---
+++ packages/Meraki-Auto-Sync/Meraki_Auto_Sync-1.189.tar.gz/Meraki_Auto_Sync-1.189/autosync/_metadata.py
@@ -0,0 +1,8 @@
+__title__ = 'Meraki_Auto_Sync'
+__description__ = 'Auto Sync Meraki Network Dashboard cross Orginization based on network tags for master and target'
+__url__ = 'http://devnet.cisco.com'
+__download_url__ = 'https://devnet.cisco.com'
+__author__ = 'Josh Lipton'
+__author_email__ = 'jolipton@cisco.com'
+__copyright__ = "Copyright (c) 2021 Cisco Systems, Inc."
+__license__ = "MIT"
---
+++ packages/Meraki-Auto-Sync/Meraki_Auto_Sync-1.189.tar.gz/Meraki_Auto_Sync-1.189/autosync/model/__init__.py
@@ -0,0 +1,4 @@
+from .appconfig import APPCONFIG
+from .mnet import MNET
+from .orgs import ORGDB
+from .meraki import meraki_nets, golden_nets
