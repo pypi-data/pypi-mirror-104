0738daa0bcfaf49685acd22bb6991e0c565a8f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:32 +0000

    Add package pystoned/0.4.7/pystoned-0.4.7.tar.gz

---
+++ packages/pystoned/pystoned-0.4.7.tar.gz/pystoned-0.4.7/pystoned/constant.py
@@ -0,0 +1,45 @@
+# Composite error term
+CET_ADDI = "addi"
+CET_MULT = "mult"
+CET_Categories = {
+    CET_ADDI: "Additive composite error term",
+    CET_MULT: "Multiplicative composite error term"
+}
+
+# Frontier
+FUN_PROD = "prod"
+FUN_COST = "cost"
+FUN_Categories = {
+    FUN_PROD: "Production frontier",
+    FUN_COST: "Cost frontier"
+}
+
+# Return to scale
+RTS_VRS = "vrs"
+RTS_CRS = "crs"
+RTS_Categories = {
+    RTS_VRS: "Variable returns to scale",
+    RTS_CRS: "Constant returns to scale"
+}
+
+# Orientation
+ORIENT_IO = "io"
+ORIENT_OO = "oo"
+ORIENT_Categories = {
+    ORIENT_IO: "Input orientation",
+    ORIENT_OO: "Output orientation"
+}
+
+# Residual decomposition
+RED_MOM = "MOM"
+RED_QLE = "QLE"
+RED_KDE = "KDE"
+RED_Categories = {
+    RED_MOM: "Method of moments",
+    RED_QLE: "Quassi-likelihood estimation",
+    RED_KDE: "Kernel deconvolution estimation"
+}
+
+# Optimization
+OPT_LOCAL = "local"
+OPT_DEFAULT = None
