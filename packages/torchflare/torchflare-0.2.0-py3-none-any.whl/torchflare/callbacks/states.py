86b64673bf93dcb0ba7efac2d3cd9b87d149fcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:09 +0000

    Add package torchflare/0.2.0/torchflare-0.2.0-py3-none-any.whl

---
+++ packages/torchflare/torchflare-0.2.0-py3-none-any.whl/torchflare/callbacks/states.py
@@ -0,0 +1,28 @@
+"""Definitions of experiment states and Callback order."""
+from enum import Enum, IntEnum
+
+
+class ExperimentStates(Enum):
+    """Class Define various stages of Training."""
+
+    EPOCH_START = "epoch_start"
+    EPOCH_END = "epoch_end"
+    EXP_START = "experiment_start"
+    EXP_END = "experiment_end"
+    BATCH_START = "batch_start"
+    BATCH_END = "batch_end"
+    LOADER_START = "loader_start"
+    LOADER_END = "loader_end"
+    STOP = "stop"
+
+
+class CallbackOrder(IntEnum):
+    """Callback orders."""
+
+    INTERNAL = 0
+    LOGGING = 1
+    STOPPING = 2
+    EXTERNAL = 3
+
+
+__all__ = ["ExperimentStates", "CallbackOrder"]
