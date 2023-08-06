3dbd59151d5a6cca6119454653c8bb9ba03b37ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:17 +0000

    Add package mypy-boto3-ec2-instance-connect/1.17.62.0/mypy-boto3-ec2-instance-connect-1.17.62.0.tar.gz

---
+++ packages/mypy-boto3-ec2-instance-connect/mypy-boto3-ec2-instance-connect-1.17.62.0.tar.gz/mypy-boto3-ec2-instance-connect-1.17.62.0/mypy_boto3_ec2_instance_connect/type_defs.py
@@ -0,0 +1,28 @@
+"""
+Main interface for ec2-instance-connect service type definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_ec2_instance_connect.type_defs import SendSSHPublicKeyResponseTypeDef
+
+    data: SendSSHPublicKeyResponseTypeDef = {...}
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = ("SendSSHPublicKeyResponseTypeDef", "SendSerialConsoleSSHPublicKeyResponseTypeDef")
+
+SendSSHPublicKeyResponseTypeDef = TypedDict(
+    "SendSSHPublicKeyResponseTypeDef", {"RequestId": str, "Success": bool}, total=False
+)
+
+SendSerialConsoleSSHPublicKeyResponseTypeDef = TypedDict(
+    "SendSerialConsoleSSHPublicKeyResponseTypeDef", {"RequestId": str, "Success": bool}, total=False
+)
