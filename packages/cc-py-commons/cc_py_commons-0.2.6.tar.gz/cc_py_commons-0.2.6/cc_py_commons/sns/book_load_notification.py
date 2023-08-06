970f68ae132c422c01a866ed67bba82de549d2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:21 +0000

    Add package cc-py-commons/0.2.6/cc_py_commons-0.2.6.tar.gz

---
+++ packages/cc-py-commons/cc_py_commons-0.2.6.tar.gz/cc_py_commons-0.2.6/cc_py_commons/sns/book_load_notification.py
@@ -0,0 +1,18 @@
+import json
+
+from cc_py_commons.sns.sns_service import SnsService
+
+class BookLoadNotification:
+
+  def __init__(self, app_config):
+    self._app_config = app_config
+
+  def send(self, load_id, user_id):
+    message = '{' + f'"userId" : {user_id}, ' \
+                    f'"loadId": "{load_id}", ' \
+                    f'"subject" : "{self._app_config.BOOK_LOAD_SNS_SUBJECT}", ' \
+                    f'"className":  "{self._app_config.BOOK_LOAD_SNS_CLASS_NAME}"' + '}'
+    print(f"sending messsage {message} to {self._app_config.BOOK_LOAD_SNS_TOPIC_ARN}")
+    sns_service = SnsService()
+    sns_service.send(self._app_config.BOOK_LOAD_SNS_TOPIC_ARN,
+      self._app_config.BOOK_LOAD_SNS_SUBJECT, message)
