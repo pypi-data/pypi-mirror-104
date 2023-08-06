32b44522cef53da26e62992d760bf2c7d044ffec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:46 +0000

    Add package python-liquid/0.7.5/python_liquid-0.7.5-py3-none-any.whl

---
+++ packages/python-liquid/python_liquid-0.7.5-py3-none-any.whl/liquid/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.7.5"
+
+try:
+    from markupsafe import escape
+    from markupsafe import Markup
+except ImportError:
+    from liquid.exceptions import escape
+    from liquid.exceptions import Markup
+
+from .mode import Mode
+from .filter import Filter
+from .token import Token
+from .expression import Expression
+from .loaders import FileSystemLoader
+
+from .context import Context
+from .context import Undefined
+from .context import DebugUndefined
+from .context import StrictUndefined
+from .context import is_undefined
+
+from .environment import Environment
+from .environment import Template
