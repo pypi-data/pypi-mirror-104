dfdc10e19e52d11e3c49eea24e307ef2e7ea21f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:16 +0000

    Add package mod9/0.0.0/mod9-0.0.0.tar.gz

---
+++ packages/mod9/mod9-0.0.0.tar.gz/mod9-0.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(
+    name='mod9',
+    version='0.0.0',
+    description='See mod9-asr for the Mod9 ASR Python SDK.',
+    long_description='See [mod9-asr](https://pypi.org/project/mod9-asr/) for the Mod9 ASR Python SDK.',
+    long_description_content_type='text/markdown',
+    author='Mod9 Technologies',
+    author_email='support@mod9.com',
+    url='https://pypi.org/project/mod9-asr/',
+)
