0624f791b1b0cc1c04d40a4679371b2421b81f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:10 +0000

    Add package skyway/0.0.3/skyway-0.0.3.tar.gz

---
+++ packages/skyway/skyway-0.0.3.tar.gz/skyway-0.0.3/skyway/__init__.py
@@ -0,0 +1,29 @@
+class skyway:
+    def __init__(self):
+        global f, content, project_name, project_version
+        f = open('./skyway/skyway/config.sky', "r")
+        project_namefinder = f.readline()
+        project_versionfinder = f.readline()
+        f.close()
+        project_namefinder.find('PROJECT_NAME =')
+        project_versionfinder.find('PROJECT_VERSION =')
+        project_name = project_namefinder.split(' ')[2]
+        project_versionfinder.find('PROJECT_VERSION =')
+        project_version = project_versionfinder.split(' ')[2]
+
+    def read(self, targetdir, file):
+        self.dir = targetdir
+        self.file = file
+        file = open(targetdir + file, "r")
+        echofinder = file.readlines()
+        file.close()
+        i = 0
+        for i in echofinder:
+            echofinder.remove("\n")
+            echofinder.remove("\n")
+
+        for i2 in echofinder:
+            print(echofinder[echofinder.index(i2)].split('"')[1])
+
+yigit = skyway()
+yigit.read("./skyway/skyway/", "main.sky")
