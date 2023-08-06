ce9c21cb08bf90e83af43aa875f050f8c89df249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:32 +0000

    Add package mess-client123client/0.0.2/mess_client123client-0.0.2.tar.gz

---
+++ packages/mess-client123client/mess_client123client-0.0.2.tar.gz/mess_client123client-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_client123client",
+      version="0.0.2",
+      description="mess_client123client",
+      author="aaaa123",
+      author_email="wepori@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
