96221fd9a77788570a2c1922079d7561ec14590f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:53 +0000

    Add package vika/0.1.2/vika-0.1.2-py3-none-any.whl

---
+++ packages/vika/vika-0.1.2-py3-none-any.whl/vika/exceptions.py
@@ -0,0 +1,43 @@
+class DatasheetDoesNotExist(Exception):
+    """"""
+
+    pass
+
+
+class RecordDoesNotExist(Exception):
+    """
+    查询的即记录不存在
+    """
+
+    pass
+
+
+class FieldDoesNotExist(Exception):
+    """
+    字段不存在
+    """
+
+    pass
+
+
+class RecordWasDeleted(Exception):
+    """
+    主动删除记录后，再访问此记录的属性会报错
+    """
+
+    pass
+
+
+class ErrorFieldKey(Exception):
+    """
+    错误的 fieldKey
+    """
+
+    pass
+
+
+class ErrorSortParams(Exception):
+    """
+    错误的排序规则
+    """
+    pass
