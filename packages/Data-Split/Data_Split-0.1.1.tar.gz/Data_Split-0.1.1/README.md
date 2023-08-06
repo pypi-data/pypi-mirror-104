66e0bf88a17de44e910cae8d15af52aa04318f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package Data-Split/0.1.1/Data_Split-0.1.1.tar.gz

---
+++ packages/Data-Split/Data_Split-0.1.1.tar.gz/Data_Split-0.1.1/README.md
@@ -0,0 +1,36 @@
+# Test_train_validation_split
+
+Test_train_validation_split is a Python library which help you to split directory or folder into training, testing and validation directories.
+
+Submitted By: **BHAWIKA ARORA**
+
+***
+pypi: <https://pypi.org/project/Data_Split>
+
+
+
+git: <https://github.com/Bhawika16/Data_Split/>
+***
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install Data-Split
+```
+
+## Command Line Usage
+
+```python
+train_test_split "directory_name" "split_size"
+train_test_split "C:\Users\BHAWIKA ARORA\plants" 0.80
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/Data-Split/Data_Split-0.1.1.tar.gz/Data_Split-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README  
+setup(
+  name = 'Data_Split',         
+  packages = ['Data_Split_by_Bhawika'],
+  version = '0.1.1',      
+  license='MIT',        
+  description = 'A python package to split Directory into Training, Testing and Validation Directory',
+  long_description=readme(),
+  long_description_content_type="text/markdown",
+  author = 'BHAWIKA ARORA',                   
+  author_email = 'bhawikavk2@gmail.com',  
+  url = 'https://github.com/Bhawika16/Data_Split',   # Provide either the link to your github or to your website
+  include_package_data=True,    
+  classifiers=[
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+  entry_points={
+        "console_scripts": [
+            "train_test_split=Data_Split_by_Bhawika.split_train_validation:main",
+        ]
+    },
+)
