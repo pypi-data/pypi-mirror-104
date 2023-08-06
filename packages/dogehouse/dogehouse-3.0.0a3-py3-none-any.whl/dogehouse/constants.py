18113c7c3499c9df67d07d9a318f01c23a1ad52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:00 +0000

    Add package dogehouse/3.0.0a3/dogehouse-3.0.0a3-py3-none-any.whl

---
+++ packages/dogehouse/dogehouse-3.0.0a3-py3-none-any.whl/dogehouse/constants.py
@@ -0,0 +1,16 @@
+READY = '_ready'
+
+NEW_TOKENS = 'new-tokens'
+
+CREATE_ROOM = 'room:create'
+ROOM_CREATED = 'room:create:reply'
+JOIN_ROOM = 'room:join'
+ROOM_JOINED = 'room:join:reply'
+GET_TOP_ROOMS = 'room:get_top'
+ROOMS_FETCHED = 'room:get_top:reply'
+
+USER_JOINED = 'new_user_join_room'
+USER_LEFT = 'user_left_room'
+MESSAGE = 'chat:send'
+
+SEND_MESSAGE = 'send_room_chat_msg'
