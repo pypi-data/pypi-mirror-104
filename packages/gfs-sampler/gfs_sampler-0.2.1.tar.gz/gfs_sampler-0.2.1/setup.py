31bfb131973725c61c4afce083300588e4ff7a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:42 +0000

    Add package gfs-sampler/0.2.1/gfs_sampler-0.2.1.tar.gz

---
+++ packages/gfs-sampler/gfs_sampler-0.2.1.tar.gz/gfs_sampler-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name = 'gfs_sampler',
+    version = '0.2.1',
+    author = 'Edwin Farley',
+    author_email = 'edwin_farley@alumni.brown.edu',
+    description = "Sample permutations of one data set with respect to another data set within defined blocks for the purposes of computing statistics over a merged data set.",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/edwinfarley/GFSpkg',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
+    ],
+    packages = find_packages(),
+    install_requires = ['theano==1.0.4', 'pymc3==3.6'],
+    python_requires = '>=3.6'
+)
