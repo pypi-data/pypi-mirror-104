a81b234c92875f0c483f831bd40a7e02f58684d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:25 +0000

    Add package mypy-boto3-sdb/1.17.62.0/mypy-boto3-sdb-1.17.62.0.tar.gz

---
+++ packages/mypy-boto3-sdb/mypy-boto3-sdb-1.17.62.0.tar.gz/mypy-boto3-sdb-1.17.62.0/mypy_boto3_sdb/literals.py
@@ -0,0 +1,24 @@
+"""
+Main interface for sdb service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_sdb.literals import ListDomainsPaginatorName
+
+    data: ListDomainsPaginatorName = "list_domains"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("ListDomainsPaginatorName", "SelectPaginatorName")
+
+
+ListDomainsPaginatorName = Literal["list_domains"]
+SelectPaginatorName = Literal["select"]
