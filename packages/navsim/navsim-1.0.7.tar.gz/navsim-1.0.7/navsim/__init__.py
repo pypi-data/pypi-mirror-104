1107cbf892efc8e0c291f11fa53b844a2b263788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:23 +0000

    Add package navsim/1.0.7/navsim-1.0.7.tar.gz

---
+++ packages/navsim/navsim-1.0.7.tar.gz/navsim-1.0.7/navsim/__init__.py
@@ -0,0 +1,8 @@
+import os
+#Path(__file__).parent.joinpath('version.txt')
+with open(os.path.join(os.path.dirname(__file__),'version.txt'), 'r') as vf:
+    __version__ = vf.read().strip()
+from .agents import DDPGAgent
+from .memory import Memory
+from .env import NavSimGymEnv
+from .executor import Executor
