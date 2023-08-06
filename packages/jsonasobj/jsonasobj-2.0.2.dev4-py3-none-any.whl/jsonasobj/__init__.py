98aa25640b083467c30ffcde70a0cd647733021f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:00 +0000

    Add package jsonasobj/2.0.2.dev4/jsonasobj-2.0.2.dev4-py3-none-any.whl

---
+++ packages/jsonasobj/jsonasobj-2.0.2.dev4-py3-none-any.whl/jsonasobj/__init__.py
@@ -0,0 +1,6 @@
+from jsonasobj._jsonobj import JsonObj, as_dict, as_list, as_json, as_json_obj, get, items, loads, load, setdefault, \
+    keys, items, JsonTypes, JsonObjTypes
+from jsonasobj.extendednamespace import ExtendedNamespace
+
+__all__ = ['JsonObj', 'ExtendedNamespace', 'as_dict', 'as_list', 'as_json', 'as_json_obj', 'get', 'items',
+           'load', 'loads', 'setdefault', 'keys', 'JsonTypes', 'JsonObjTypes']
