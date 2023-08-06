b2bfb30facfd3d070dade0cfa53f82b9750b88c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:08 +0000

    Add package bbx-ninja/0.0.2/bbx_ninja-0.0.2.tar.gz

---
+++ packages/bbx-ninja/bbx_ninja-0.0.2.tar.gz/bbx_ninja-0.0.2/bbx_ninja/__init__.py
@@ -0,0 +1,4 @@
+from bbx_ninja.dataset_related.check_squad_integrity import *
+from bbx_ninja.dataset_related.sample_large_ds import *
+from bbx_ninja.knowledge_graph.generate_qa_from_wikidata import *
+from bbx_ninja.web_crawler.cooler import *
