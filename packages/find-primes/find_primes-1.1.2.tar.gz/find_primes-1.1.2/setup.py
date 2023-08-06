e7a62de6ce8bc1b402592f601e2f5533c559845c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:38 +0000

    Add package find-primes/1.1.2/find_primes-1.1.2.tar.gz

---
+++ packages/find-primes/find_primes-1.1.2.tar.gz/find_primes-1.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding = 'utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'find_primes',
+    version = '1.1.2',
+    author = 'JamesJ',
+    author_email = 'GGJamesQQ@yeah.net',
+    description = 'A module for finding primes.',
+    classifiers = [
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta'
+    ],
+    packages = ['find_primes'],
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    python_requires = '>=3.6'
+)
