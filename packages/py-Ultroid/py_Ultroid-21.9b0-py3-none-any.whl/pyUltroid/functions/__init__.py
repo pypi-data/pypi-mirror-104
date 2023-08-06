988e92f8cf536f2b92e390bd55d15d97419f2f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:14 +0000

    Add package py-Ultroid/21.9b0/py_Ultroid-21.9b0-py3-none-any.whl

---
+++ packages/py-Ultroid/py_Ultroid-21.9b0-py3-none-any.whl/pyUltroid/functions/__init__.py
@@ -0,0 +1,20 @@
+from pyUltroid import *
+
+from ..dB.database import Var
+
+DANGER = [
+    "APP_ID",
+    "API_HASH",
+    "SESSION",
+    "HEROKU_API",
+    "base64",
+    "base32",
+    "get_me",
+    "phone",
+    "REDIS_PASSWORD",
+    "load_addons",
+    "load_plugins",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+]
