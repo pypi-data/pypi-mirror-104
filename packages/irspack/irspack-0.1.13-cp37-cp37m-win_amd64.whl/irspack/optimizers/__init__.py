2a8a95126408daa393ee9dd9bf5c17d16ff7bf70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:14 +0000

    Add package irspack/0.1.13/irspack-0.1.13-cp37-cp37m-win_amd64.whl

---
+++ packages/irspack/irspack-0.1.13-cp37-cp37m-win_amd64.whl/irspack/optimizers/__init__.py
@@ -0,0 +1,53 @@
+from ._optimizers import (
+    AsymmetricCosineKNNOptimizer,
+    AsymmetricCosineUserKNNOptimizer,
+    BaseOptimizer,
+    BaseOptimizerWithEarlyStopping,
+    CosineKNNOptimizer,
+    CosineUserKNNOptimizer,
+    DenseSLIMOptimizer,
+    IALSOptimizer,
+    JaccardKNNOptimizer,
+    NMFOptimizer,
+    P3alphaOptimizer,
+    RP3betaOptimizer,
+    SLIMOptimizer,
+    TopPopOptimizer,
+    TruncatedSVDOptimizer,
+    TverskyIndexKNNOptimizer,
+)
+from .autopilot import autopilot
+
+__all__ = [
+    "BaseOptimizer",
+    "BaseOptimizerWithEarlyStopping",
+    "TopPopOptimizer",
+    "IALSOptimizer",
+    "DenseSLIMOptimizer",
+    "SLIMOptimizer",
+    "P3alphaOptimizer",
+    "RP3betaOptimizer",
+    "CosineKNNOptimizer",
+    "AsymmetricCosineKNNOptimizer",
+    "CosineUserKNNOptimizer",
+    "AsymmetricCosineUserKNNOptimizer",
+    "JaccardKNNOptimizer",
+    "TverskyIndexKNNOptimizer",
+    "NMFOptimizer",
+    "TruncatedSVDOptimizer",
+    "autopilot",
+]
+
+try:
+    from ._optimizers import BPRFMOptimizer
+
+    __all__.append("BPRFMOptimizer")
+except ImportError:
+    pass
+
+try:
+    from ._optimizers import MultVAEOptimizer
+
+    __all__.append("MultVAEOptimizer")
+except ImportError:
+    pass
