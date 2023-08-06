470305e65c9ae1b6b3d3543a1af084b4b50d79de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:31 +0000

    Add package error-param-cal/0.0.3/error_param_cal-0.0.3.tar.gz

---
+++ packages/error-param-cal/error_param_cal-0.0.3.tar.gz/error_param_cal-0.0.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 1 19:45:08 2021
+
+@author: Suhas V S
+"""
+
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Calculate the parameters of the power of a test in Hypothesis Testing iteratively'
+
+
+setup(
+    name="error_param_cal",
+    version=VERSION,
+    author="Suhas V S",
+    author_email="<suhas95vs@gmail.com>",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SuhasVS95/Hypothesis_Testing_Error_parameters_Calculator",
+    license="MIT",
+    keywords=['python', 'sample_size', 'Type I Error', 'Type II Error',"False Positive","False Negative","Power of Test"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    py_modules = ['error_param']
+)
