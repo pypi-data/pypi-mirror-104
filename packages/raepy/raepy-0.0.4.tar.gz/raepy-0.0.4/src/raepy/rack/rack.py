6279cdfcfe51b5d1908e1dc2b1c168199a64b51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:40 +0000

    Add package raepy/0.0.4/raepy-0.0.4.tar.gz

---
+++ packages/raepy/raepy-0.0.4.tar.gz/raepy-0.0.4/src/raepy/rack/rack.py
@@ -0,0 +1,33 @@
+
+import time
+
+class Rack(object):
+    def __init__(self,Servo):
+        self._servo = Servo
+
+    def to(self,position):
+        """
+        position in meter. Constant 82644 is calculated by (43,56[mm/360°] => 0.121 [mm/°] => 8.2644 [°/mm] => 8264,46 [°/m]  * 10 => 82644 * (1/10°)/m
+        """
+        self._servo.move_absolute_angle(int(position*82644))
+    
+    def position(self):
+        return self._servo.actual_angle()/82644
+
+    def out(self):
+        self.to(0.08)
+
+    def home(self, feedback_cb=None):
+        self._servo.jog(-50)
+        cnt = 0
+        while self._servo.actual_current() < 700:
+            time.sleep(0.02)
+            cnt+=1
+            if cnt > 1000:
+                return "No Rack inserted"
+
+        self._servo.set_zero_here()
+        self._servo.move_relative_angle(1200)
+        time.sleep(1)
+    
+
