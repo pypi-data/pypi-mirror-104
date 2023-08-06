2f40d13b40ddd111abfc8e557b1d630be07a8056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:31 +0000

    Add package hepo-probability/0.1/hepo_probability-0.1.tar.gz

---
+++ packages/hepo-probability/hepo_probability-0.1.tar.gz/hepo_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'hepo_probability',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['hepo_probability_gaussian_binomial'],
+      author = 'Eddie Palomino',
+      author_email = 'palomino.ochoah@gmail.com',
+    zip_safe=False)
