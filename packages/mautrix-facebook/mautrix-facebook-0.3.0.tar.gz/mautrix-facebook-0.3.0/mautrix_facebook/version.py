db4a9e19023db512c0bf27cfdd13a65e617ab54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package mautrix-facebook/0.3.0/mautrix-facebook-0.3.0.tar.gz

---
+++ packages/mautrix-facebook/mautrix-facebook-0.3.0.tar.gz/mautrix-facebook-0.3.0/mautrix_facebook/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.3.0'
+git_revision = '22989127'
+version = '0.3.0'
+linkified_version = '[0.3.0](https://github.com/tulir/mautrix-facebook/releases/tag/v0.3.0)'
