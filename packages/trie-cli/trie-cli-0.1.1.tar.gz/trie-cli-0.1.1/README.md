d50962fde04ab105f8ac331d8ed8ee776382ea64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:50 +0000

    Add package trie-cli/0.1.1/trie-cli-0.1.1.tar.gz

---
+++ packages/trie-cli/trie-cli-0.1.1.tar.gz/trie-cli-0.1.1/README.md
@@ -0,0 +1,7 @@
+# trie-cli
+
+## Sources Consulted
+Resources used for CLI:
+https://trstringer.com/easy-and-nice-python-cli/
+https://www.youtube.com/watch?v=Jr4QDJwwj60
+https://click.palletsprojects.com/en/7.x/
---
+++ packages/trie-cli/trie-cli-0.1.1.tar.gz/trie-cli-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open("cli_requirements.txt") as req:
+        content = req.read()
+        requirements = content.split("\n")
+    return requirements
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_desc = file.read()
+
+setup(
+    name='trie-cli',
+    version='0.1.1',
+    author = 'Pranav Mathur',
+    author_email = 'pranavmathur001@gmail.com',
+    description = 'A CLI to interact with a global trie data structure',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pmathur007/trie-cli',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    entry_points = {
+        'console_scripts': [
+            'trie-cli=triecli.cli:main'
+        ]
+    }
+)
