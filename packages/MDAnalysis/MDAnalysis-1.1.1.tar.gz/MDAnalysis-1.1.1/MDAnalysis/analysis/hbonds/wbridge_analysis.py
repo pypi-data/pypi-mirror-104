b936a0f248c58d5f8661b5d0317cd68fea8013dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:05 +0000

    Add package MDAnalysis/1.1.1/MDAnalysis-1.1.1.tar.gz

---
+++ packages/MDAnalysis/MDAnalysis-1.1.1.tar.gz/MDAnalysis-1.1.1/MDAnalysis/analysis/hbonds/wbridge_analysis.py
@@ -0,0 +1,35 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# MDAnalysis --- https://www.mdanalysis.org
+# Copyright (c) 2006-2017 The MDAnalysis Development Team and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# Released under the GNU Public Licence, v2 or any higher version
+#
+# Please cite your use of MDAnalysis in published work:
+#
+# R. J. Gowers, M. Linke, J. Barnoud, T. J. E. Reddy, M. N. Melo, S. L. Seyler,
+# D. L. Dotson, J. Domanski, S. Buchoux, I. M. Kenney, and O. Beckstein.
+# MDAnalysis: A Python package for the rapid analysis of molecular dynamics
+# simulations. In S. Benthall and S. Rostrup editors, Proceedings of the 15th
+# Python in Science Conference, pages 102-109, Austin, TX, 2016. SciPy.
+# doi: 10.25080/majora-629e541a-00e
+#
+# N. Michaud-Agrawal, E. J. Denning, T. B. Woolf, and O. Beckstein.
+# MDAnalysis: A Toolkit for the Analysis of Molecular Dynamics Simulations.
+# J. Comput. Chem. 32 (2011), 2319--2327, doi:10.1002/jcc.21787
+#
+
+# Water Bridge Analysis
+import warnings
+
+
+with warnings.catch_warnings():
+    warnings.simplefilter('always', DeprecationWarning)
+    warnings.warn(("This module has been moved to "
+                   "MDAnalysis.analysis.hydrogenbonds.wbridge_analysis "
+                   "It will be removed in release 2.0"),
+                  category=DeprecationWarning)
+
+from ..hydrogenbonds.wbridge_analysis import WaterBridgeAnalysis
