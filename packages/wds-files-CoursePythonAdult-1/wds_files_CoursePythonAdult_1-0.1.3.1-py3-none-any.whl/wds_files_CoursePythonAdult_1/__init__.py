6164986189ae1ba9e14cc1fa81a39f52709c1e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:24 +0000

    Add package wds-files-CoursePythonAdult-1/0.1.3.1/wds_files_CoursePythonAdult_1-0.1.3.1-py3-none-any.whl

---
+++ packages/wds-files-CoursePythonAdult-1/wds_files_CoursePythonAdult_1-0.1.3.1-py3-none-any.whl/wds_files_CoursePythonAdult_1/__init__.py
@@ -0,0 +1,6 @@
+import os
+import wrap_data_source
+
+_PACK_SOURCE_FOLDER = os.path.split(__file__)[0]
+_path = os.path.join(_PACK_SOURCE_FOLDER, "sprite_types_cpa1")
+source = wrap_data_source.file_data_source.FileDataSource(_path, sprite_types_subfolder=None, sprite_type_costumes_subfolder=None)
