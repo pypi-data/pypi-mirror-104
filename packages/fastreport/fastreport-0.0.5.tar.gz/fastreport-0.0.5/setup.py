cd3abf94b8ded4d31575b4044ff44b50de1ccd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:15 +0000

    Add package fastreport/0.0.5/fastreport-0.0.5.tar.gz

---
+++ packages/fastreport/fastreport-0.0.5.tar.gz/fastreport-0.0.5/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 1 19:45:08 2021
+
+@author: kishore S
+"""
+
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Easy report with almost no coding'
+
+
+setup(
+    name="fastreport",
+    version=VERSION,
+    author="Kishore",
+    author_email="<kishoresshankar@gmail.com>",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kishore-s-gowda/fastreport",
+    license="MIT",
+    keywords=['python', 'regression', 'classification', 'algorithm'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    py_modules = ['report']
+)
