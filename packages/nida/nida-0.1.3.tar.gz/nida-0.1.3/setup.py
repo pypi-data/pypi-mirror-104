1222e38a43f879d4970207e316187bf883d3fd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package nida/0.1.3/nida-0.1.3.tar.gz

---
+++ packages/nida/nida-0.1.3.tar.gz/nida-0.1.3/setup.py
@@ -0,0 +1,47 @@
+from os import path
+from setuptools import setup
+
+# read the contents of your description file
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'description.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="nida",
+    version="0.1.3",
+    description="A python libary to Easy fetching user information based on National ID (Tanzania)",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Kalebu/nida",
+    download_url="https://github.com/Kalebu/Nida/archive/0.2.tar.gz",
+    author="Jordan Kalebu",
+    author_email="isaackeinstein@gmail.com",
+    license="MIT",
+    packages=["nida"],
+    keywords=[
+        "nida",
+        "nida api"
+        "nida python package"
+        "python nida"
+        "tanzania nida",
+        "python-tanzania",
+    ],
+    install_requires=[
+        'addict',
+        'requests',
+        'pillow'
+    ],
+    include_package_data=True,
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
