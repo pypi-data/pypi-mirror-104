3571e145b27080fae778b5c9262c5087a8296c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:00 +0000

    Add package cepimose/0.1.4/cepimose-0.1.4.tar.gz

---
+++ packages/cepimose/cepimose-0.1.4.tar.gz/cepimose-0.1.4/cepimose/parser.py
@@ -0,0 +1,362 @@
+import datetime
+
+from .types import (
+    VaccinationByDayRow,
+    VaccinationByAgeRow,
+    VaccineSupplyUsage,
+    VaccinationByRegionRow,
+    VaccinationByManufacturerRow,
+    VaccinationDose,
+    VaccinationByAgeRange,
+    VaccinationMunShare,
+)
+
+
+def parse_date(raw):
+    return datetime.datetime.utcfromtimestamp(float(raw) / 1000.0)
+
+
+def _parse_vaccinations_timestamp(data):
+    if "DS" not in data["results"][0]["result"]["data"]["dsr"]:
+        error = data["results"][0]["result"]["data"]["dsr"]["DataShapes"][0][
+            "odata.error"
+        ]
+        # ? raise exception or return error obj
+        return error
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    return resp[0]["M0"]
+
+
+def _parse_vaccinations_by_day(data) -> "list[VaccinationByDayRow]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    for element in resp:
+        date = parse_date(element["G0"])
+        people_vaccinated = element["X"][0]["M0"]
+        people_fully_vaccinated = element["X"][1]["M0"] if len(element["X"]) > 1 else 0
+
+        parsed_data.append(
+            VaccinationByDayRow(
+                date=date,
+                first_dose=people_vaccinated,
+                second_dose=people_fully_vaccinated,
+            )
+        )
+
+    return parsed_data
+
+
+def _parse_vaccinations_by_age(data) -> "list[VaccinationByAgeRow]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    for element in resp:
+        age_group = str(element["G0"])
+        count_first = int(element["X"][0]["C"][1])
+        count_second = int(element["X"][1]["C"][1])
+        share_first = float(element["X"][0]["C"][0]) / 100.0
+        share_second = float(element["X"][1]["C"][0]) / 100.0
+
+        parsed_data.append(
+            VaccinationByAgeRow(
+                age_group=age_group,
+                count_first=count_first,
+                count_second=count_second,
+                share_first=share_first,
+                share_second=share_second,
+            )
+        )
+
+    return parsed_data
+
+
+def _parse_vaccines_supplied_and_used(data) -> "list[VaccineSupplyUsage]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    for element in resp:
+
+        date = parse_date(element["C"][0])
+
+        if "Ø" in element:
+            supplied = int(element["C"][1]) if len(element["C"]) > 1 else 0
+            used = 0
+        else:
+            used = (
+                int(element["C"][1]) if len(element["C"]) > 1 else parsed_data[-1].used
+            )
+            supplied = (
+                int(element["C"][2])
+                if len(element["C"]) > 2
+                else parsed_data[-1].supplied
+            )
+
+        row = VaccineSupplyUsage(
+            date=date,
+            supplied=supplied,
+            used=used,
+        )
+        parsed_data.append(row)
+
+    return parsed_data
+
+
+def _parse_vaccinations_by_region(data) -> "list[VaccinationByRegionRow]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    for element in resp:
+        region = str(element["G0"])
+        count_first = int(element["X"][0]["C"][1])
+        count_second = int(element["X"][1]["C"][1])
+        share_first = float(element["X"][0]["C"][0]) / 100.0
+        share_second = float(element["X"][1]["C"][0]) / 100.0
+
+        parsed_data.append(
+            VaccinationByRegionRow(
+                region=region,
+                count_first=count_first,
+                count_second=count_second,
+                share_first=share_first,
+                share_second=share_second,
+            )
+        )
+
+    return parsed_data
+
+
+def _parse_vaccines_supplied_by_manufacturer(
+    data,
+) -> "list[VaccinationByManufacturerRow]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][1]["DM1"]
+    manufacturers = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["ValueDicts"][
+        "D0"
+    ]
+    parsed_data = []
+
+    if len(manufacturers) > 4:
+        print(manufacturers)
+        raise Exception("New manufacturer!")
+
+    def get_manufacturer(num):
+        manu_keys = ["pfizer", "moderna", "az", "janssen"]
+        if num > 3 or num == None:
+            print(num)
+            raise Exception("Missing manufacturer!")
+        return manu_keys[num]
+
+    r_list = [None, 1, 2, 6]
+
+    date = None
+    manufacturer = None
+    value = None
+
+    for element in resp:
+        R = element["R"] if "R" in element else None
+        C = element["C"]
+
+        if R not in r_list:
+            print(R, C, sep="\t")
+            raise Exception("Unknown R value!")
+
+        manu_row = VaccinationByManufacturerRow(
+            date=None, pfizer=None, moderna=None, az=None, janssen=None
+        )
+
+        if R == None:
+            # all data
+            date = parse_date(C[0])
+            manufacturer = get_manufacturer((C[1]))
+            value = int(C[2])
+            setattr(manu_row, "date", date)
+            setattr(manu_row, manufacturer, value)
+
+        if R == 1:
+            # same date as previous
+            manufacturer = get_manufacturer((C[0]))
+            value = int(C[1])
+            setattr(parsed_data[-1], manufacturer, value)
+
+        if R == 2:
+            # same manufacturer as previous
+            date = parse_date(C[0])
+            value = int(C[1])
+            setattr(manu_row, "date", date)
+            setattr(manu_row, manufacturer, value)
+
+        if R == 6:
+            # same manufacturer and value as previous
+            date = parse_date(C[0])
+            setattr(manu_row, "date", date)
+            setattr(manu_row, manufacturer, value)
+
+        if R != 1:
+            parsed_data.append(manu_row)
+    return parsed_data
+
+
+def _parse_vaccines_supplied_by_manufacturer_cum(
+    data,
+) -> "list[VaccinationByManufacturerRow]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    for element in resp:
+        elements = list(filter(lambda x: "M0" in x, element["X"]))
+
+        date = parse_date(element["G0"])
+        moderna = None
+        pfizer = None
+        az = None
+        janssen = None
+
+        if len(elements) == 1:
+            el = elements[0]
+            if el.get("I", None) == 1:
+                moderna = int(el["M0"])
+            elif el.get("I", None) == 2:
+                pfizer = int(el["M0"])
+            else:
+                az = int(el["M0"])
+
+        # ? what if some other combination
+        if len(elements) == 2:
+            az = elements[0]["M0"]
+            moderna = elements[1]["M0"]
+
+        if len(elements) == 3:
+            az = round(elements[0]["M0"])
+            moderna = round(elements[1]["M0"])
+            pfizer = round(elements[2]["M0"])
+
+        parsed_data.append(
+            VaccinationByManufacturerRow(
+                date=date,
+                pfizer=pfizer,
+                moderna=moderna,
+                az=az,
+                janssen=janssen,
+            )
+        )
+
+    return parsed_data
+
+
+def _parse_vaccinations_by_age_range(data) -> "list[VaccinationDose]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    date = None
+    dose = None
+    r_list = [None, 1]
+    for element in resp:
+        date = parse_date(element["G0"])
+        R = R = element["X"][0]["R"] if "R" in element["X"][0] else None
+
+        if R not in r_list:
+            print(R)
+            raise Exception("Unknown R value!")
+
+        if R == None:
+            dose = element["X"][0]["M0"]
+
+        parsed_data.append(VaccinationDose(date=date, dose=dose))
+
+    return parsed_data
+
+
+# resp error example
+_resp_error = {
+    "DataShapes": [
+        {
+            "odata.error": {
+                "code": "InvalidOrMalformedSemanticQueryDefinition",
+                "source": "PowerBI",
+                "message": {
+                    "lang": "en-US",
+                    "value": "Encountered invalid QueryYearConstantExpression. Failed to parse type encoded string",
+                },
+                "azure:values": [
+                    {"timestamp": "2021-04-11T12:13:03.0228291Z"},
+                    {
+                        "additionalMessages": [
+                            {
+                                "Code": "InvalidOrMalformedSemanticQueryDefinition",
+                                "Severity": "Error",
+                                "Message": "Encountered invalid QueryYearConstantExpression. Failed to parse type encoded string",
+                            }
+                        ]
+                    },
+                ],
+            }
+        }
+    ]
+}
+
+# ? most likely we can refactor _parse_vaccinations_by_day
+def _parse_vaccinations_by_region_by_day(data):
+
+    if "DS" not in data["results"][0]["result"]["data"]["dsr"]:
+        error = data["results"][0]["result"]["data"]["dsr"]["DataShapes"][0][
+            "odata.error"
+        ]
+        # it happens when region name passed to _get_default_by_region_by_day_condition_values func is misspelled or not wrapped in single quotes
+        # ? raise exception or return error obj
+        return error
+
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    r_list = [None, 1]
+
+    people_vaccinated = None
+    people_fully_vaccinated = None
+    for element in resp:
+        date = parse_date(element["G0"])
+
+        R = element["X"][0]["R"] if "R" in element["X"][0] else None
+
+        if R == None:
+            people_vaccinated = element["X"][0]["M0"]
+            people_fully_vaccinated = (
+                element["X"][1]["M0"] if len(element["X"]) > 1 else 0
+            )
+
+        if R == 1:
+            # as far as we know people_vaccinated same as previous
+            people_fully_vaccinated = (
+                element["X"][1]["M0"] if len(element["X"]) > 1 else 0
+            )
+
+        parsed_data.append(
+            VaccinationByDayRow(
+                date=date,
+                first_dose=people_vaccinated,
+                second_dose=people_fully_vaccinated,
+            )
+        )
+
+    return parsed_data
+
+
+def _parse_vaccinations_by_municipalities_share(data) -> "list[VaccinationMunShare]":
+    resp = data["results"][0]["result"]["data"]["dsr"]["DS"][0]["PH"][0]["DM0"]
+    parsed_data = []
+
+    for el in resp:
+        name, share2, share1, population = el["C"]
+        dose1 = round(int(population) * float(share1))
+        dose2 = round(int(population) * float(share2))
+        parsed_data.append(
+            VaccinationMunShare(
+                name=name,
+                dose1=dose1,
+                share1=float(share1),
+                dose2=dose2,
+                share2=float(share2),
+                population=population,
+            )
+        )
+
+    return parsed_data
---
+++ packages/cepimose/cepimose-0.1.4.tar.gz/cepimose-0.1.4/test/test.py
@@ -0,0 +1,270 @@
+import unittest
+import cepimose
+import datetime
+
+
+class CepimoseTestCase(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def assertDatesIncreaseSince(self, data, startDate):
+        previousDate = startDate - datetime.timedelta(days=1)
+        for row in data:
+            self.assertGreater(row.date, previousDate, row)
+            previousDate = row.date
+
+    def test_vaccinations_by_day(self):
+        # Test feature one.
+        data = cepimose.vaccinations_by_day()
+        self.assertGreater(len(data), 100)
+
+        def assertRow(row, expected_date, expected_first, expected_second):
+            self.assertEqual(row.date, expected_date)
+            self.assertAlmostEqual(row.first_dose, expected_first, delta=30)
+            self.assertAlmostEqual(row.second_dose, expected_second, delta=30)
+
+        #! NIJZ is changing data tests could fail in the future
+        assertRow(data[9], datetime.datetime(2021, 1, 5), 15711, 0)
+        assertRow(data[22], datetime.datetime(2021, 1, 18), 48711, 315)
+
+        self.assertDatesIncreaseSince(data, datetime.datetime(2020, 12, 27))
+
+    def test_vaccinations_by_age(self):
+        # Test feature one.
+        data = {row.age_group: row for row in cepimose.vaccinations_by_age()}
+
+        expected_age_groups = [
+            "0-17",
+            "18-24",
+            "25-29",
+            "30-34",
+            "35-39",
+            "40-44",
+            "45-49",
+            "50-54",
+            "55-59",
+            "60-64",
+            "65-69",
+            "70-74",
+            "75-79",
+            "80-84",
+            "85-89",
+            "90+",
+        ]
+
+        self.assertTrue(len(data), len(expected_age_groups))
+
+        for grp in expected_age_groups:
+            self.assertGreater(data[grp].count_first, 0)
+            self.assertGreater(data[grp].share_first, 0)
+            self.assertGreater(data[grp].count_second, 0)
+            self.assertGreater(data[grp].share_second, 0)
+
+    def test_vaccinations_by_region(self):
+        # Test feature one.
+        data = {row.region: row for row in cepimose.vaccinations_by_region()}
+
+        expected_regions = [
+            "Koroška",
+            "Zasavska",
+            "Goriška",
+            "Posavska",
+            "Gorenjska",
+            "Podravska",
+            "Pomurska",
+            "Osrednjeslovenska",
+            "Jugovzhodna Slovenija",
+            "Primorsko-notranjska",
+            "Savinjska",
+            "Obalno-kraška",
+        ]
+
+        print(data.keys())
+
+        self.assertTrue(len(data), len(expected_regions))
+
+        for grp in expected_regions:
+            self.assertGreater(data[grp].count_first, 0)
+            self.assertGreater(data[grp].share_first, 0)
+            self.assertGreater(data[grp].count_second, 0)
+            self.assertGreater(data[grp].share_second, 0)
+
+    def test_vaccine_supply_and_usage(self):
+        data = cepimose.vaccines_supplied_and_used()
+        self.assertGreater(len(data), 100)
+
+        def assertRow(row, expected_date, expected_supp, expected_used):
+            self.assertEqual(row.date, expected_date)
+            self.assertAlmostEqual(row.supplied, expected_supp, delta=30)
+            self.assertAlmostEqual(row.used, expected_used, delta=30)
+
+        #! NIJZ is changing data tests could fail in the future
+        assertRow(data[9], datetime.datetime(2021, 1, 4), 39780, 13248)
+        assertRow(data[22], datetime.datetime(2021, 1, 17), 60870, 48799)
+
+        self.assertDatesIncreaseSince(data, datetime.datetime(2020, 12, 26))
+
+    def test_supplied_by_manufacturer(self):
+        data = cepimose.vaccines_supplied_by_manufacturer()
+        self.assertTrue(len(data) > 10)
+
+        def assertRow(row, expected_date, expected):
+            self.assertEqual(row.date, expected_date)
+            self.assertEqual(row.pfizer, expected[0])
+            self.assertEqual(row.moderna, expected[1])
+            self.assertEqual(row.az, expected[2])
+            self.assertEqual(row.janssen, expected[3])
+
+        assertRow(data[1], datetime.datetime(2020, 12, 30), [8190, None, None, None])  # R = 2
+        assertRow(data[3], datetime.datetime(2021, 1, 11), [19890, None, None, None])  # R = 6
+        assertRow(
+            data[16], datetime.datetime(2021, 2, 25), [None, 8400, 16800, None]
+        )  # combined: two response data items with same date; second has R = 1
+
+        self.assertDatesIncreaseSince(data, datetime.datetime(2020, 12, 26))
+
+    def test_supplied_by_manufacturer_cumulative(self):
+        data = cepimose.vaccines_supplied_by_manufacturer_cumulative()
+        self.assertTrue(len(data) > 10)
+
+        def assertRow(row, expected_date, expected):
+            self.assertEqual(row.date, expected_date)
+            self.assertEqual(row.pfizer, expected[0])
+            self.assertEqual(row.moderna, expected[1])
+            self.assertEqual(row.az, expected[2])
+            self.assertEqual(row.janssen, expected[3])
+
+        assertRow(data[3], datetime.datetime(2021, 1, 11), [59670, None, None, None])
+        assertRow(data[7], datetime.datetime(2021, 1, 31), [None, 3600, None, None])
+        assertRow(data[10], datetime.datetime(2021, 2, 6), [None, None, 9600, None])
+        assertRow(data[16], datetime.datetime(2021, 2, 25), [None, 16800, 52800, None])
+        # assertRow(
+        #     data[len(data) - 1], datetime.datetime(2021, 4, 2), [285480, 46800, 144000]
+        # )  # this test will fail in the future
+
+        self.assertDatesIncreaseSince(data, datetime.datetime(2020, 12, 26))
+
+    def test_vaccinations_by_age_range_90(self):
+        data = cepimose.vaccinations_by_age_range_90()
+        data_dose1 = data.dose1
+        data_dose2 = data.dose2
+
+        self.assertTrue(len(data_dose1) > 10)
+        self.assertTrue(len(data_dose2) > 10)
+        self.assertTrue(len(data_dose1) > len(data_dose2))
+        self.assertTrue(len(data_dose1) - len(data_dose2) == 12)
+
+        def assertRow(row, expected_date, expected_dose):
+            self.assertEqual(row.date, expected_date)
+            self.assertAlmostEqual(row.dose, expected_dose, delta=30)
+
+        assertRow(data_dose1[21], datetime.datetime(2021, 1, 17), 3580)
+        assertRow(data_dose1[70], datetime.datetime(2021, 3, 7), 7866)
+        assertRow(data_dose2[9], datetime.datetime(2021, 1, 17), 1)
+        assertRow(data_dose2[58], datetime.datetime(2021, 3, 7), 4821)
+
+        self.assertDatesIncreaseSince(data_dose1, datetime.datetime(2020, 12, 26))
+        self.assertDatesIncreaseSince(data_dose2, datetime.datetime(2020, 12, 26))
+
+    def test_vaccinations_by_age_rage(self):
+        data = cepimose.vaccinations_by_age_range()
+        expected_keys = [
+            "0-17",
+            "18-24",
+            "25-29",
+            "30-34",
+            "35-39",
+            "40-44",
+            "45-49",
+            "50-54",
+            "55-59",
+            "60-64",
+            "65-69",
+            "70-74",
+            "75-79",
+            "80-84",
+            "85-90",
+            "90+",
+        ]
+
+        self.assertEquals(expected_keys, list(data.keys()), "Object keys")
+
+        range_90_data = data["90+"]
+        data_dose1 = range_90_data.dose1
+        data_dose2 = range_90_data.dose2
+
+        self.assertTrue(len(data_dose1) > 10)
+        self.assertTrue(len(data_dose2) > 10)
+        self.assertTrue(len(data_dose1) > len(data_dose2))
+        self.assertTrue(len(data_dose1) - len(data_dose2) == 12)
+
+        def assertRow(row, expected_date, expected_dose):
+            self.assertEqual(row.date, expected_date)
+            self.assertAlmostEqual(row.dose, expected_dose, delta=30)
+
+        assertRow(data_dose1[21], datetime.datetime(2021, 1, 17), 3580)
+        assertRow(data_dose1[70], datetime.datetime(2021, 3, 7), 7866)
+        assertRow(data_dose2[9], datetime.datetime(2021, 1, 17), 1)
+        assertRow(data_dose2[58], datetime.datetime(2021, 3, 7), 4821)
+
+        self.assertDatesIncreaseSince(data_dose1, datetime.datetime(2020, 12, 26))
+        self.assertDatesIncreaseSince(data_dose2, datetime.datetime(2020, 12, 26))
+
+    def test_vaccinations_pomurska_by_day(self):
+        data = cepimose.vaccinations_pomurska_by_day()
+
+        self.assertGreater(len(data), 100)
+
+        def assertRow(row, expected_date, expected_first, expected_second):
+            self.assertEqual(row.date, expected_date)
+            self.assertAlmostEqual(row.first_dose, expected_first, delta=30)
+            self.assertAlmostEqual(row.second_dose, expected_second, delta=30)
+
+        assertRow(data[9], datetime.datetime(2021, 1, 5), 988, 0)
+        assertRow(data[22], datetime.datetime(2021, 1, 18), 2847, 5)
+
+        self.assertDatesIncreaseSince(data, datetime.datetime(2020, 12, 27))
+
+    def test_vaccinations_by_region_by_day(self):
+        data = cepimose.vaccinations_by_region_by_day()
+        expected_keys = [
+            "Goriška",
+            "Zasavska",
+            "Koroška",
+            "Gorenjska",
+            "Osrednjeslovenska",
+            "Posavska",
+            "Podravska",
+            "Pomurska",
+            "Savinjska",
+            "Jugovzhodna Slovenija",
+            "Primorsko-notranjska",
+            "Obalno-kraška",
+        ]
+
+        self.assertEquals(expected_keys, list(data.keys()), "Object keys")
+
+        for element in data.items():
+            print(element[0])
+            self.assertTrue(len(element[1]) != 0)
+            self.assertDatesIncreaseSince(element[1], datetime.datetime(2020, 12, 27))
+
+        pomurska_region = data["Pomurska"]
+
+        def assertRow(row, expected_date, expected_first, expected_second):
+            self.assertEqual(row.date, expected_date)
+            self.assertAlmostEqual(row.first_dose, expected_first, delta=30)
+            self.assertAlmostEqual(row.second_dose, expected_second, delta=30)
+
+        assertRow(pomurska_region[9], datetime.datetime(2021, 1, 5), 988, 0)
+        assertRow(pomurska_region[22], datetime.datetime(2021, 1, 18), 2847, 5)
+
+    def test_vaccinations_by_municipalities_share(self):
+        data = cepimose.vaccinations_by_municipalities_share()
+
+        mun_number = 212
+
+        self.assertTrue(len(data), mun_number)
