418a36d0c436e29bd905bbdde54678243b563e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:18 +0000

    Add package math-binomial-gaussian-distribution/0.1/math_binomial_gaussian_distribution-0.1.tar.gz

---
+++ packages/math-binomial-gaussian-distribution/math_binomial_gaussian_distribution-0.1.tar.gz/math_binomial_gaussian_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='math_binomial_gaussian_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['math_binomial_gaussian_distribution'],
+      author='Rishant Rokaha',
+      author_email='rixantrokaha@gmail.com',
+      zip_safe=False)
