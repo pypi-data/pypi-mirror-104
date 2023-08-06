39ed95f6098a44b3d8b6b7cc6701ac759e2c008e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:53 +0000

    Add package cmc-csci046-gerges/1.0.0/cmc_csci046_gerges-1.0.0.tar.gz

---
+++ packages/cmc-csci046-gerges/cmc_csci046_gerges-1.0.0.tar.gz/cmc_csci046_gerges-1.0.0/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages, setup
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cmc_csci046_gerges",
+    version="1.0.0",
+    description="This is for a CS class",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mariagerges/week8_containers",
+    author="Maria Gerges",
+    author_email="mgerges23@cmc.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    #packages=["reader"],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    #install_requires=["feedparser", "html2text"],
+    #entry_points={
+        #"console_scripts": [
+            #"realpython=reader.__main__:main",
+        #]
+    #},
+)
