cc74f43b0945f438539df268c33cfe1b69396378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:13 +0000

    Add package mde/0.0.2/mde-0.0.2.tar.gz

---
+++ packages/mde/mde-0.0.2.tar.gz/mde-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mde']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['prompt-toolkit>=3.0.18,<4.0.0', 'rich>=10.1.0,<11.0.0']
+
+entry_points = \
+{'console_scripts': ['mde = mde.cli:start']}
+
+setup_kwargs = {
+    'name': 'mde',
+    'version': '0.0.2',
+    'description': 'CLI Markdown Editor',
+    'long_description': 'mde\n_________________\n\n[![PyPI version](https://badge.fury.io/py/mde.svg)](http://badge.fury.io/py/mde)\n[![Test Status](https://github.com/timothycrosley/mde/workflows/Test/badge.svg?branch=develop)](https://github.com/timothycrosley/mde/actions?query=workflow%3ATest)\n[![Lint Status](https://github.com/timothycrosley/mde/workflows/Lint/badge.svg?branch=develop)](https://github.com/timothycrosley/mde/actions?query=workflow%3ALint)\n[![codecov](https://codecov.io/gh/timothycrosley/mde/branch/main/graph/badge.svg)](https://codecov.io/gh/timothycrosley/mde)\n[![Join the chat at https://gitter.im/timothycrosley/mde](https://badges.gitter.im/timothycrosley/mde.svg)](https://gitter.im/timothycrosley/mde?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)\n[![License](https://img.shields.io/github/license/mashape/apistatus.svg)](https://pypi.python.org/pypi/mde/)\n[![Downloads](https://pepy.tech/badge/mde)](https://pepy.tech/project/mde)\n[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)\n[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://timothycrosley.github.io/isort/)\n_________________\n\n[Read Latest Documentation](https://timothycrosley.github.io/mde/) - [Browse GitHub Code Repository](https://github.com/timothycrosley/mde/)\n_________________\n\n**mde** CLI Markdown Editor\n',
+    'author': 'Timothy Crosley',
+    'author_email': 'timothy.crosley@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
