0182de5432a6f7c77c65c1344d2f1aef42efeeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:32 +0000

    Add package pythw/1.0/pythw-1.0.tar.gz

---
+++ packages/pythw/pythw-1.0.tar.gz/pythw-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'pythw',
+    version = '1.0',
+    description = ['Pyth Console'],
+    package_dir = {'': 'src'},
+    install_requires = [
+        'pyfiglet'
+    ]
+)
