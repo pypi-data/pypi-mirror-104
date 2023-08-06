93cd93d796559a741b48d7ded6b7d02b98546094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:39 +0000

    Add package pyridy/0.1.9/pyridy-0.1.9-py3-none-any.whl

---
+++ packages/pyridy/pyridy-0.1.9-py3-none-any.whl/pyridy/file.py
@@ -0,0 +1,239 @@
+import json
+import logging
+import os
+import sqlite3
+from sqlite3 import Connection, DatabaseError
+from typing import Optional, List
+
+import pandas as pd
+from pandas.io.sql import DatabaseError as PandasDatabaseError
+
+from pyridy.utils import Sensor, AccelerationSeries, LinearAccelerationSeries, MagnetometerSeries, OrientationSeries, \
+    GyroSeries, RotationSeries, GPSSeries, PressureSeries, HumiditySeries, TemperatureSeries, WzSeries, LightSeries, \
+    SubjectiveComfortSeries
+from pyridy.utils.device import Device
+
+logger = logging.getLogger(__name__)
+
+
+class RDYFile:
+    def __init__(self, path: str = ""):
+        self.path = path
+        self.filename: Optional[str] = ""
+        self.extension: Optional[str] = ""
+
+        self.db_con: Optional[Connection] = None
+
+        # RDY File Infos
+        self.rdy_format_version: Optional[float] = None
+        self.rdy_info_name: Optional[str] = None
+        self.rdy_info_sex: Optional[str] = None
+        self.rdy_info_age: Optional[int] = None
+        self.rdy_info_height: Optional[float] = None
+        self.rdy_info_weight: Optional[float] = None
+
+        self.t0: Optional[str] = None
+        self.timestamp_when_started: Optional[int] = None
+        self.timestamp_when_stopped: Optional[int] = None
+        self.device: Optional[Device] = None
+
+        # Sensors
+        self.sensors: Optional[List[Sensor]] = []
+
+        # Measurement Series
+        self.acc_series: Optional[AccelerationSeries] = None
+        self.lin_acc_series: Optional[LinearAccelerationSeries] = None
+        self.mag_series: Optional[MagnetometerSeries] = None
+        self.orient_series: Optional[OrientationSeries] = None
+        self.gyro_series: Optional[GyroSeries] = None
+        self.rot_series: Optional[RotationSeries] = None
+        self.gps_series: Optional[GPSSeries] = None
+        self.pressure_series: Optional[PressureSeries] = None
+        self.temperature_series: Optional[TemperatureSeries] = None
+        self.humidity_series: Optional[HumiditySeries] = None
+        self.light_series: Optional[LightSeries] = None
+        self.wz_series: Optional[WzSeries] = None
+        self.subjective_comfort_series: Optional[SubjectiveComfortSeries] = None
+
+        if self.path:
+            self.load_file(self.path)
+        pass
+
+    def __repr__(self):
+        return "RDYFile: %s" % self.filename
+
+    def load_file(self, path: str):
+        logger.info("Loading file: %s" % path)
+
+        _, self.extension = os.path.splitext(path)
+        _, self.filename = os.path.split(path)
+
+        if self.extension == ".rdy":
+            with open(path, 'r') as file:
+                rdy = json.load(file)
+
+            self.rdy_format_version = rdy['RDY_Format_Version']
+            self.rdy_info_name = rdy['RDY_Info_Name']
+            self.rdy_info_sex = rdy['RDY_Info_Sex']
+            self.rdy_info_age = rdy['RDY_Info_Age']
+            self.rdy_info_height = rdy['RDY_Info_Height']
+            self.rdy_info_weight = rdy['RDY_Info_Weight']
+
+            self.t0 = rdy['t0']
+            self.timestamp_when_started = rdy['timestamp_when_started']
+            self.timestamp_when_stopped = rdy['timestamp_when_stopped']
+
+            self.device = Device(**rdy['device_info'])
+
+            for sensor in rdy['sensors']:
+                self.sensors.append(Sensor(**sensor))
+
+            self.acc_series = AccelerationSeries(**rdy['acc_series'])
+            self.lin_acc_series = LinearAccelerationSeries(**rdy['lin_acc_series'])
+            self.mag_series = MagnetometerSeries(**rdy['mag_series'])
+            self.orient_series = OrientationSeries(**rdy['orient_series'])
+            self.gyro_series = GyroSeries(**rdy['gyro_series'])
+            self.rot_series = RotationSeries(**rdy['rot_series'])
+            self.gps_series = GPSSeries(**rdy['gps_series'])
+            self.pressure_series = PressureSeries(**rdy['pressure_series'])
+            self.temperature_series = TemperatureSeries(**rdy['temperature_series'])
+            self.humidity_series = HumiditySeries(**rdy['humidity_series'])
+            self.light_series = LightSeries(**rdy['light_series'])
+            self.wz_series = WzSeries(**rdy['wz_series'])
+            self.subjective_comfort_series = SubjectiveComfortSeries(**rdy['subjective_comfort_series'])
+            pass
+
+        elif self.extension == ".sqlite":
+            self.db_con = sqlite3.connect(path)
+
+            try:
+                info = dict(pd.read_sql_query("SELECT * from measurement_information_table", self.db_con))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing measurement_information_table, file: %s" % self.filename)
+                try:
+                    info = dict(pd.read_sql_query("SELECT * from measurment_information_table", self.db_con))  # Older files can contain wrong table name
+                except (DatabaseError, PandasDatabaseError) as e:
+                    info = None
+
+            try:
+                sensor_df = pd.read_sql_query("SELECT * from sensor_descriptions_table", self.db_con)
+                for _, row in sensor_df.iterrows():
+                    self.sensors.append(Sensor(**dict(row)))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing sensor_descriptions_table, file: %s" % self.filename)
+
+            try:
+                device_df = pd.read_sql_query("SELECT * from device_information_table", self.db_con)
+                self.device = Device(**dict(device_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing device_information_table, file: %s" % self.filename)
+                self.device = Device()
+
+            # Info
+            if info is not None:
+                self.rdy_format_version = info['rdy_format_version'][0]
+                self.rdy_info_name = info['rdy_info_name'][0]
+                self.rdy_info_sex = info['rdy_info_sex'][0]
+                self.rdy_info_age = info['rdy_info_age'][0]
+                self.rdy_info_height = info['rdy_info_height'][0]
+                self.rdy_info_weight = info['rdy_info_weight'][0]
+
+                self.t0 = info['t0'][0]
+                self.timestamp_when_started = info['timestamp_when_started'][0]
+                self.timestamp_when_stopped = info['timestamp_when_stopped'][0]
+
+            # Measurements
+            try:
+                acc_df = pd.read_sql_query("SELECT * from acc_measurements_table", self.db_con)
+                self.acc_series = AccelerationSeries(**dict(acc_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing acc_measurements_table, file: %s" % self.filename)
+                self.acc_series = AccelerationSeries()
+
+            try:
+                lin_acc_df = pd.read_sql_query("SELECT * from lin_acc_measurements_table", self.db_con)
+                self.lin_acc_series = LinearAccelerationSeries(**dict(lin_acc_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing lin_acc_measurements_table, file: %s" % self.filename)
+                self.lin_acc_series = LinearAccelerationSeries()
+
+            try:
+                mag_df = pd.read_sql_query("SELECT * from mag_measurements_table", self.db_con)
+                self.mag_series = MagnetometerSeries(**dict(mag_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing mag_measurements_table, file: %s" % self.filename)
+                self.mag_series = MagnetometerSeries()
+
+            try:
+                orient_df = pd.read_sql_query("SELECT * from orient_measurements_table", self.db_con)
+                self.orient_series = OrientationSeries(**dict(orient_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing orient_measurements_table, file: %s" % self.filename)
+                self.orient_series = OrientationSeries()
+
+            try:
+                gyro_df = pd.read_sql_query("SELECT * from gyro_measurements_table", self.db_con)
+                self.gyro_series = GyroSeries(**dict(gyro_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing gyro_measurements_table, file: %s" % self.filename)
+                self.gyro_series = GyroSeries()
+
+            try:
+                rot_df = pd.read_sql_query("SELECT * from rot_measurements_table", self.db_con)
+                self.rot_series = RotationSeries(**dict(rot_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing rot_measurements_table, file: %s" % self.filename)
+                self.rot_series = RotationSeries()
+
+            try:
+                gps_df = pd.read_sql_query("SELECT * from gps_measurements_table", self.db_con)
+                self.gps_series = GPSSeries(**dict(gps_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing gps_measurements_table, file: %s" % self.filename)
+                self.gps_series = GPSSeries()
+
+            try:
+                pressure_df = pd.read_sql_query("SELECT * from pressure_measurements_table", self.db_con)
+                self.pressure_series = PressureSeries(**dict(pressure_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing pressure_measurements_table, file: %s" % self.filename)
+                self.pressure_series = PressureSeries()
+
+            try:
+                temperature_df = pd.read_sql_query("SELECT * from temperature_measurements_table", self.db_con)
+                self.temperature_series = TemperatureSeries(**dict(temperature_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing temperature_measurements_table, file: %s" % self.filename)
+                self.temperature_series = TemperatureSeries()
+
+            try:
+                humidity_df = pd.read_sql_query("SELECT * from humidity_measurements_table", self.db_con)
+                self.humidity_series = HumiditySeries(**dict(humidity_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing humidity_measurements_table, file: %s" % self.filename)
+                self.humidity_series = HumiditySeries()
+
+            try:
+                light_df = pd.read_sql_query("SELECT * from light_measurements_table", self.db_con)
+                self.light_series = LightSeries(**dict(light_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing light_measurements_table, file: %s" % self.filename)
+                self.light_series = LightSeries()
+
+            try:
+                wz_df = pd.read_sql_query("SELECT * from wz_measurements_table", self.db_con)
+                self.wz_series = WzSeries(**dict(wz_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing wz_measurements_table, file: %s" % self.filename)
+                self.wz_series = WzSeries()
+
+            try:
+                subjective_comfort_df = pd.read_sql_query("SELECT * from subjective_comfort_measurements_table",
+                                                          self.db_con)
+                self.subjective_comfort_series = SubjectiveComfortSeries(**dict(subjective_comfort_df))
+            except (DatabaseError, PandasDatabaseError) as e:
+                logger.error("DatabaseError occurred when accessing subjective_comfort_measurements_table, file: %s" % self.filename)
+                self.subjective_comfort_series = SubjectiveComfortSeries()
+
+        else:
+            raise ValueError("File extension %s is not supported" % self.extension)
---
+++ packages/pyridy/pyridy-0.1.9-py3-none-any.whl/pyridy/utils/timeseries.py
@@ -0,0 +1,154 @@
+from abc import ABC
+from typing import Union
+
+import numpy as np
+import pandas as pd
+
+
+class TimeSeries(ABC):
+    def __init__(self, time: Union[list, np.ndarray] = None):
+        self.time: np.ndarray = np.array(time)
+
+    def to_df(self) -> pd.DataFrame:
+        return pd.DataFrame(self.__dict__)
+
+
+class AccelerationSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 acc_x: Union[list, np.ndarray] = None,
+                 acc_y: Union[list, np.ndarray] = None,
+                 acc_z: Union[list, np.ndarray] = None):
+        super(AccelerationSeries, self).__init__(time=time)
+        self.acc_x: np.ndarray = np.array(acc_x)
+        self.acc_y: np.ndarray = np.array(acc_y)
+        self.acc_z: np.ndarray = np.array(acc_z)
+
+
+class LinearAccelerationSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 lin_acc_x: Union[list, np.ndarray] = None,
+                 lin_acc_y: Union[list, np.ndarray] = None,
+                 lin_acc_z: Union[list, np.ndarray] = None):
+        super(LinearAccelerationSeries, self).__init__(time=time)
+        self.lin_acc_x: np.ndarray = np.array(lin_acc_x)
+        self.lin_acc_y: np.ndarray = np.array(lin_acc_y)
+        self.lin_acc_z: np.ndarray = np.array(lin_acc_z)
+
+
+class MagnetometerSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 mag_x: Union[list, np.ndarray] = None,
+                 mag_y: Union[list, np.ndarray] = None,
+                 mag_z: Union[list, np.ndarray] = None):
+        super(MagnetometerSeries, self).__init__(time=time)
+        self.mag_x: np.ndarray = np.array(mag_x)
+        self.mag_y: np.ndarray = np.array(mag_y)
+        self.mag_z: np.ndarray = np.array(mag_z)
+
+
+class OrientationSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 azimuth: Union[list, np.ndarray] = None,
+                 pitch: Union[list, np.ndarray] = None,
+                 roll: Union[list, np.ndarray] = None):
+        super(OrientationSeries, self).__init__(time=time)
+        self.azimuth: np.ndarray = np.array(azimuth)
+        self.pitch: np.ndarray = np.array(pitch)
+        self.roll: np.ndarray = np.array(roll)
+
+
+class GyroSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 w_x: Union[list, np.ndarray] = None,
+                 w_y: Union[list, np.ndarray] = None,
+                 w_z: Union[list, np.ndarray] = None):
+        super(GyroSeries, self).__init__(time=time)
+        self.w_x: np.ndarray = np.array(w_x)
+        self.w_y: np.ndarray = np.array(w_y)
+        self.w_z: np.ndarray = np.array(w_z)
+
+
+class RotationSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 rot_x: Union[list, np.ndarray] = None,
+                 rot_y: Union[list, np.ndarray] = None,
+                 rot_z: Union[list, np.ndarray] = None,
+                 cos_phi: Union[list, np.ndarray] = None,
+                 heading_acc: Union[list, np.ndarray] = None):
+        super(RotationSeries, self).__init__(time=time)
+        self.rot_x: np.ndarray = np.array(rot_x)
+        self.rot_y: np.ndarray = np.array(rot_y)
+        self.rot_z: np.ndarray = np.array(rot_z)
+        self.cos_phi: np.ndarray = np.array(cos_phi)
+        self.heading_acc: np.ndarray = np.array(heading_acc)
+
+
+class GPSSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 lat: Union[list, np.ndarray] = None,
+                 lon: Union[list, np.ndarray] = None,
+                 altitude: Union[list, np.ndarray] = None,
+                 bearing: Union[list, np.ndarray] = None,
+                 speed: Union[list, np.ndarray] = None,
+                 hor_acc: Union[list, np.ndarray] = None,
+                 ver_acc: Union[list, np.ndarray] = None,
+                 bear_acc: Union[list, np.ndarray] = None,
+                 speed_acc: Union[list, np.ndarray] = None,
+                 utc_time: Union[list, np.ndarray] = None):
+        super(GPSSeries, self).__init__(time=time)
+        self.lat: np.ndarray = np.array(lat)
+        self.lon: np.ndarray = np.array(lon)
+        self.altitude: np.ndarray = np.array(altitude)
+        self.bearing: np.ndarray = np.array(bearing)
+        self.speed: np.ndarray = np.array(speed)
+        self.hor_acc: np.ndarray = np.array(hor_acc)
+        self.ver_acc: np.ndarray = np.array(ver_acc)
+        self.bear_acc: np.ndarray = np.array(bear_acc)
+        self.speed_acc: np.ndarray = np.array(speed_acc)
+        self.utc_time: np.ndarray = np.array(utc_time)
+
+
+class PressureSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 pressure: Union[list, np.ndarray] = None):
+        super(PressureSeries, self).__init__(time=time)
+        self.pressure: np.ndarray = np.array(pressure)
+
+
+class TemperatureSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 temperature: Union[list, np.ndarray] = None):
+        super(TemperatureSeries, self).__init__(time=time)
+        self.temperature: np.ndarray = np.array(temperature)
+
+
+class HumiditySeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 humidity: Union[list, np.ndarray] = None):
+        super(HumiditySeries, self).__init__(time=time)
+        self.humidity: np.ndarray = np.array(humidity)
+
+
+class LightSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 light: Union[list, np.ndarray] = None):
+        super(LightSeries, self).__init__(time=time)
+        self.light: np.ndarray = np.array(light)
+
+
+class WzSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 wz_x: Union[list, np.ndarray] = None,
+                 wz_y: Union[list, np.ndarray] = None,
+                 wz_z: Union[list, np.ndarray] = None):
+        super(WzSeries, self).__init__(time=time)
+        self.wz_x: np.ndarray = np.array(wz_x)
+        self.wz_y: np.ndarray = np.array(wz_y)
+        self.wz_z: np.ndarray = np.array(wz_z)
+
+
+class SubjectiveComfortSeries(TimeSeries):
+    def __init__(self, time: Union[list, np.ndarray] = None,
+                 comfort: Union[list, np.ndarray] = None):
+        super(SubjectiveComfortSeries, self).__init__(time=time)
+        self.comfort: np.ndarray = np.array(comfort)
