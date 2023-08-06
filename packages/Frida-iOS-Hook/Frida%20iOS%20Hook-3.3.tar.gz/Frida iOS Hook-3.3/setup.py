ba2a12351f9565270270b9789313fddb9edff2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:28 +0000

    Add package Frida-iOS-Hook/3.3/Frida%20iOS%20Hook-3.3.tar.gz

---
+++ packages/Frida-iOS-Hook/Frida%20iOS%20Hook-3.3.tar.gz/Frida iOS Hook-3.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open ("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Frida iOS Hook',
+    version='3.3',
+    description='Trace Class/Func & Modify Return Value',
+    author='noobpk',
+    author_email='ltp.noobpk@gmail.com',
+    long_description =long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/noobpk/frida-ios-hook/',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+
+    ],
+    python_requires='>=3.0'
+)
