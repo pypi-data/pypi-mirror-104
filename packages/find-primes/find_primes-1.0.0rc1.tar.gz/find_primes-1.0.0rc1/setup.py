f27fa79e081136e3c87fb7189c482d6596ccc3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:29 +0000

    Add package find-primes/1.0.0rc1/find_primes-1.0.0rc1.tar.gz

---
+++ packages/find-primes/find_primes-1.0.0rc1.tar.gz/find_primes-1.0.0rc1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'find_primes',
+    version = '1.0.0rc1',
+    author = 'JamesJ',
+    author_email = 'GGJamesQQ@yeah.net',
+    description = 'A module for finding primes.',
+    classifiers = [
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta'
+    ],
+    packages = ['find_primes'],
+    python_requires = '>=3.6'
+)
