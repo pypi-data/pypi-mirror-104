1b64e898a5c6fb070cc198049fd8c0d5659cb182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:07 +0000

    Add package quazar/1.0.0/quazar-1.0.0.tar.gz

---
+++ packages/quazar/quazar-1.0.0.tar.gz/quazar-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['quazar']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.7.4,<4.0.0']
+
+setup_kwargs = {
+    'name': 'quazar',
+    'version': '1.0.0',
+    'description': 'A Discord bot library written in Python.',
+    'long_description': None,
+    'author': 'vcokltfre',
+    'author_email': 'vcokltfre@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
