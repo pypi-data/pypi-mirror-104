6c9ca4fc30e4632901ae47c6fa46a234c1d5d19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:17 +0000

    Add package nlu/3.0.1rc4/nlu-3.0.1rc4-py3-none-any.whl

---
+++ packages/nlu/nlu-3.0.1rc4-py3-none-any.whl/nlu/component_infos/output_levels.py
@@ -0,0 +1,22 @@
+"""
+Mapping of col to tyoes
+
+
+"""
+DOCUMENT = "document"
+TOKEN = "token"
+WORDPIECE = "wordpiece"
+WORD_EMBEDDINGS = "word_embeddings"
+SENTENCE_EMBEDDINGS = "sentence_embeddings"
+CATEGORY = "category" # Multi Class etc
+DATE = "date"
+SENTIMENT = "sentiment"
+POS = "pos"
+CHUNK = "chunk" # Multiple Token text piece. i.e. extracted entity
+NAMED_ENTITY = "named_entity" # exxtracted IOB
+ENTITY = "entity" # converted entity, pretty
+NEGEX = "negex" # ?
+UNDEPENDENCY = "unlabeled_dependency"
+LABELED_DEPENDENCY = "labeled_dependency"
+LANGUAGE = "language"
+KEYWORD = "keyword"
