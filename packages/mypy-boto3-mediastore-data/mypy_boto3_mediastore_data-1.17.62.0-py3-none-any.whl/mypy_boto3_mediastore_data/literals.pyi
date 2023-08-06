099eac0593a3f9e01e8b9fd373c6459e16f530c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package mypy-boto3-mediastore-data/1.17.62.0/mypy_boto3_mediastore_data-1.17.62.0-py3-none-any.whl

---
+++ packages/mypy-boto3-mediastore-data/mypy_boto3_mediastore_data-1.17.62.0-py3-none-any.whl/mypy_boto3_mediastore_data/literals.pyi
@@ -0,0 +1,24 @@
+"""
+Main interface for mediastore-data service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_mediastore_data.literals import ItemType
+
+    data: ItemType = "FOLDER"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("ItemType", "ListItemsPaginatorName", "StorageClass", "UploadAvailability")
+
+ItemType = Literal["FOLDER", "OBJECT"]
+ListItemsPaginatorName = Literal["list_items"]
+StorageClass = Literal["TEMPORAL"]
+UploadAvailability = Literal["STANDARD", "STREAMING"]
