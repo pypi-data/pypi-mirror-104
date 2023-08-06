105789002c62f32703c401a85721db5b4a9f89a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:58 +0000

    Add package vertis-periodtask/0.8.0/vertis_periodtask-0.8.0.tar.gz

---
+++ packages/vertis-periodtask/vertis_periodtask-0.8.0.tar.gz/vertis_periodtask-0.8.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+import io
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+VERSION = '0.8.0'
+
+setup(
+    name='vertis_periodtask',
+    description='Periodic task with timezone',
+    long_description=long_description,
+    version=VERSION,
+    url='https://github.com/vertisfinance/periodtask',
+    author='Egyed Gábor',
+    author_email='gabor.egyed@vertis.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8'
+    ],
+    install_requires=[
+        'pytz >= 2021.1',
+        'mako == 1.1.4',
+    ],
+    license='MIT',
+    packages=['periodtask'],
+    package_data={
+        '': ['templates/*', 'VERSION'],
+    }
+)
