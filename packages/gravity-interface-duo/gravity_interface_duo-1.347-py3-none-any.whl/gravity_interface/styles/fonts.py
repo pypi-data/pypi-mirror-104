39cd202921938c0455a2df4f99edf9fb8e2f1abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:00 +0000

    Add package gravity-interface-duo/1.347/gravity_interface_duo-1.347-py3-none-any.whl

---
+++ packages/gravity-interface-duo/gravity_interface_duo-1.347-py3-none-any.whl/gravity_interface/styles/fonts.py
@@ -0,0 +1,19 @@
+# Общие шрифты
+orup_font = '"Montserrat SemiBold" 11'
+general_entry_font = '"Montserrat SemiBold" 11'
+general_text_font = '"Montserrat Regular" 11'
+general_error_text = '"Montserrat Regular" 11'      # Шрифт текста в окне ошибки ОРУП
+general_optionmenu_font = '"Montserrat Regular" 11'
+
+# Окно статистики
+statistic_filtres = '"Montserrat SemiBold" 14'
+statistic_calendars = '"Montserrat SemiBold" 14'
+statistic_tree_font = '"Roboto" 12'
+
+
+# Главное меню
+status_text = '"Roboto" 12'
+main_tree_font = '"Roboto" 12'
+time_font = '"Montserrat" 14'
+date_font = '"Montserrat" 32'
+weight_font = '"Montserrat SemiBold" 30'
---
+++ packages/gravity-interface-duo/gravity_interface_duo-1.347-py3-none-any.whl/gravity_interface/tests/set_res_test.py
@@ -0,0 +1,3 @@
+from gravity_interface.tools import set_res
+
+set_res.set_res(int('1920'), int('1080'))
