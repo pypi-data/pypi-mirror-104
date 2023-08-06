b96c9c2c4939c87cb3de20b9b2fbef96bbceff19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:16 +0000

    Add package podcast_search/0.1.1/podcast_search-0.1.1-py2.py3-none-any.whl

---
+++ packages/podcast_search/podcast_search-0.1.1-py2.py3-none-any.whl/podcast_search/__init__.py
@@ -0,0 +1,26 @@
+"""Let's find some podcasts!"""
+
+__version__ = "0.1.1"
+
+from dataclasses import dataclass
+from typing import Optional, List
+
+SEARCH_URL = "https://itunes.apple.com/search"
+
+@dataclass
+class Podcast:
+    """Podcast metadata."""
+    id: str
+    name: str
+    author: str
+    url: str
+    feed: Optional[str] = None
+    category: Optional[str] = None
+    image: Optional[str] = None
+
+
+def search(name: str, limit: int = 5) -> List[Podcast]:
+    """Search podcast by name."""
+    params = {"term": name, "limit": limit, "media": "podcast"}
+    response = _get(url=SEARCH_URL, params=params)
+    return _parse(response)
