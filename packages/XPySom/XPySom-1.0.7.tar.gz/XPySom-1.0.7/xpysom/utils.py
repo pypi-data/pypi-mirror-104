4431f6de4b17b06fbf54229182c9be627d4d5daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:06 +0000

    Add package XPySom/1.0.7/XPySom-1.0.7.tar.gz

---
+++ packages/XPySom/XPySom-1.0.7.tar.gz/XPySom-1.0.7/xpysom/utils.py
@@ -0,0 +1,26 @@
+import subprocess
+import multiprocessing
+
+def find_max_cuda_threads():
+    try:
+        import cupy as cp
+        dev = cp.cuda.Device()
+        n_smp = dev.attributes['MultiProcessorCount']
+        max_thread_per_smp = dev.attributes['MaxThreadsPerMultiProcessor']
+        return n_smp*max_thread_per_smp
+    except:
+        print("Cupy is not available.")
+        return 0
+    try:
+        
+        return int(subprocess.check_output("nvidia-settings -q CUDACores -t", shell=True))
+    except:
+        print("Could not infer #cuda_cores")
+        return 0
+
+def find_cpu_cores():
+    try:
+        return multiprocessing.cpu_count()
+    except:
+        print("Could not infer #CPU_cores")
+        return 0
