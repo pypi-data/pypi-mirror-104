7279ac577acc94e3281499abfea6b5d4da4d08fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:27 +0000

    Add package CompleteTheSquare/1.1.0/CompleteTheSquare-1.1.0-py3-none-any.whl

---
+++ packages/CompleteTheSquare/CompleteTheSquare-1.1.0-py3-none-any.whl/CompleteTheSquare/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("CompleteTheSquare/README.md", "r") as fhandle:
+    long_description = fhandle.read() # Your README.md file will be used as the long description!
+
+setuptools.setup(
+    name="CompleteTheSquare", # Put your username here!
+    version="1.1.0", # The version of your package!
+    author="Zeliktric", # Your name here!
+    author_email="zeliktric@zeliktric.com", # Your e-mail here!
+    description="A Simple Python project to complete the square!", # A short description here!
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zeliktric/CompleteTheSquare", # Link your package website here! (most commonly a GitHub repo)
+    packages=setuptools.find_packages(), # A list of all packages for Python to distribute!
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], # Enter meta data into the classifiers list!
+    python_requires='>=3.6', # The version requirement for Python to run your package!
+)
