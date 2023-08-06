d00967bab95ab9110e31d17872a3cd0839ea4a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:53 +0000

    Add package cmc-csci046-gerges/1.0.1/cmc_csci046_gerges-1.0.1.tar.gz

---
+++ packages/cmc-csci046-gerges/cmc_csci046_gerges-1.0.1.tar.gz/cmc_csci046_gerges-1.0.1/README.md
@@ -0,0 +1,7 @@
+# containers
+
+Build statuses:
+
+1. [![](https://github.com/mariagerges/week8_containers/workflows/tests-fibonacci/badge.svg)](https://github.com/mariagerges/week8_containers/actions?query=workflow%3Atests-fibonacci)
+1. [![](https://github.com/mariagerges/week8_containers/workflows/tests-range/badge.svg)](https://github.com/mariagerges/week8_containers/actions?query=workflow%3Atests-range)
+1. [![](https://github.com/mariagerges/week8_containers/workflows/tests-unicode/badge.svg)](https://github.com/mariagerges/week8_containers/actions?query=workflow%3Atests-unicode)
---
+++ packages/cmc-csci046-gerges/cmc_csci046_gerges-1.0.1.tar.gz/cmc_csci046_gerges-1.0.1/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import find_packages, setup
+
+#i The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="cmc_csci046_gerges",
+    version="1.0.1",
+    description="Data Structures and Algorithms course package. This course was taught by mike izbicki from claremont mckenna college. Data Science major. the project has AvlTree, BinarySearch Tree, Unicode, and Heap",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="Real Python",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["feedparser", "html2text"],
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)
