8dd98c9c0bd93b97a07a12dc41e247ede74bbb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:02 +0000

    Add package shrim-py/0.0.1/shrim-py-0.0.1.tar.gz

---
+++ packages/shrim-py/shrim-py-0.0.1.tar.gz/shrim-py-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+install_requires = [
+    "requests>=2.25.1"
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shrim-py",
+    version="0.0.1",
+    author="Cryptogram Trade",
+    author_email="dev@cryptogram.trade",
+    description="The Better Shrimpy Python Client for Dev and User APIs",
+    license="GNU",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cryptogram-Trade/shrim-py",
+    install_requires=install_requires,
+    packages=["shrimpy"],
+    python_requires=">=3.6",
+    keywords=[
+        "shrimpy", "social", "bitcoin", "client", "api",
+        "exchange", "crypto", "currency", "trading",
+        "ETH", "BTC", "3commas"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
