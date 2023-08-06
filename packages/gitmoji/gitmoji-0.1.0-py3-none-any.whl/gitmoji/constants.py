24cdaabc3e4737f72bf7c7182abff9a6de93b249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package gitmoji/0.1.0/gitmoji-0.1.0-py3-none-any.whl

---
+++ packages/gitmoji/gitmoji-0.1.0-py3-none-any.whl/gitmoji/constants.py
@@ -0,0 +1,81 @@
+from typing import List, TypedDict
+
+GITMOJI_LIST_STR = """🎨 - :art: - Improve structure / format of the code.
+⚡️ - :zap: - Improve performance.
+🔥 - :fire: - Remove code or files.
+🐛 - :bug: - Fix a bug.
+🚑 - :ambulance: - Critical hotfix.
+✨ - :sparkles: - Introduce new features.
+📝 - :memo: - Add or update documentation.
+🚀 - :rocket: - Deploy stuff.
+💄 - :lipstick: - Add or update the UI and style files.
+🎉 - :tada: - Begin a project.
+✅ - :white_check_mark: - Add or update tests.
+🔒 - :lock: - Fix security issues.
+🔖 - :bookmark: - Release / Version tags.
+🚨 - :rotating_light: - Fix compiler / linter warnings.
+🚧 - :construction: - Work in progress.
+💚 - :green_heart: - Fix CI Build.
+⬇️ - :arrow_down: - Downgrade dependencies.
+⬆️ - :arrow_up: - Upgrade dependencies.
+📌 - :pushpin: - Pin dependencies to specific versions.
+👷 - :construction_worker: - Add or update CI build system.
+📈 - :chart_with_upwards_trend: - Add or update analytics or track code.
+♻️ - :recycle: - Refactor code.
+➕ - :heavy_plus_sign: - Add a dependency.
+➖ - :heavy_minus_sign: - Remove a dependency.
+🔧 - :wrench: - Add or update configuration files.
+🔨 - :hammer: - Add or update development scripts.
+🌐 - :globe_with_meridians: - Internationalization and localization.
+✏️ - :pencil2: - Fix typos.
+💩 - :poop: - Write bad code that needs to be improved.
+⏪ - :rewind: - Revert changes.
+🔀 - :twisted_rightwards_arrows: - Merge branches.
+📦 - :package: - Add or update compiled files or packages.
+👽 - :alien: - Update code due to external API changes.
+🚚 - :truck: - Move or rename resources (e.g.: files, paths, routes).
+📄 - :page_facing_up: - Add or update license.
+💥 - :boom: - Introduce breaking changes.
+🍱 - :bento: - Add or update assets.
+♿️ - :wheelchair: - Improve accessibility.
+💡 - :bulb: - Add or update comments in source code.
+🍻 - :beers: - Write code drunkenly.
+💬 - :speech_balloon: - Add or update text and literals.
+🗃 - :card_file_box: - Perform database related changes.
+🔊 - :loud_sound: - Add or update logs.
+🔇 - :mute: - Remove logs.
+👥 - :busts_in_silhouette: - Add or update contributor(s).
+🚸 - :children_crossing: - Improve user experience / usability.
+🏗 - :building_construction: - Make architectural changes.
+📱 - :iphone: - Work on responsive design.
+🤡 - :clown_face: - Mock things.
+🥚 - :egg: - Add or update an easter egg.
+🙈 - :see_no_evil: - Add or update a .gitignore file.
+📸 - :camera_flash: - Add or update snapshots.
+⚗ - :alembic: - Perform experiments.
+🔍 - :mag: - Improve SEO.
+🏷️ - :label: - Add or update types.
+🌱 - :seedling: - Add or update seed files.
+🚩 - :triangular_flag_on_post: - Add, update, or remove feature flags.
+🥅 - :goal_net: - Catch errors.
+💫 - :dizzy: - Add or update animations and transitions.
+🗑 - :wastebasket: - Deprecate code that needs to be cleaned up.
+🛂 - :passport_control: - Work on code related to authorization, roles and permissions.
+"""
+
+
+class Gitmoji(TypedDict):
+    emoji: str
+    command: str
+    description: str
+
+
+def split_gitmoji_list(gitmoji_list: str) -> List[Gitmoji]:
+    gitmoji_arr = []
+    for line in gitmoji_list.splitlines():
+        emoji, comm, desc = line.split(" - ")
+        gitmoji_arr.append({"emoji": emoji, "command": comm, "description": desc})
+    return gitmoji_arr
+
+
+GITMOJI_LIST = split_gitmoji_list(GITMOJI_LIST_STR)
