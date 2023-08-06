526095fb4583ed32bbe94a60403b04201acd8655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:49 +0000

    Add package refinenet/0.9.3/refinenet-0.9.3.tar.gz

---
+++ packages/refinenet/refinenet-0.9.3.tar.gz/refinenet-0.9.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import Extension, find_packages, setup
+from setuptools.command.install import install
+import subprocess
+
+import time
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='refinenet',
+      version='0.9.3',
+      author='Ben Talbot',
+      author_email='b.talbot@qut.edu.au',
+      description='RefineNet semantic image segmentation',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=[
+          'acrv_datasets', 'numpy', 'pillow', 'pycocotools', 'scipy', 'six',
+          'torch', 'torchvision'
+      ],
+      entry_points={'console_scripts': ['refinenet=refinenet.__main__:main']},
+      classifiers=(
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ))
