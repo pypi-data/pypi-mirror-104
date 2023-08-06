b0d1ea5ad4ec4257e7a262896fd665afc9ba1d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:30 +0000

    Add package thebrukhellomodule/5.0/thebrukhellomodule-5.0.tar.gz

---
+++ packages/thebrukhellomodule/thebrukhellomodule-5.0.tar.gz/thebrukhellomodule-5.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='thebrukhellomodule',
+      version='5.0',
+      description='Hello!',
+      packages=find_packages(),
+      author_email='thebrukprojects@gmail.com',
+      zip_safe=False)
