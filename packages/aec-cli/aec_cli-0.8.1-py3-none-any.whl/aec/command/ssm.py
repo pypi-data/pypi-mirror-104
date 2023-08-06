8cc1e4084b64b1dcdb49d7b01755ad297d5c8709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:31 +0000

    Add package aec-cli/0.8.1/aec_cli-0.8.1-py3-none-any.whl

---
+++ packages/aec-cli/aec_cli-0.8.1-py3-none-any.whl/aec/command/ssm.py
@@ -0,0 +1,45 @@
+from typing import Any, Dict, List, Optional
+
+import boto3
+
+from aec.util.config import Config
+from aec.util.list import first_or_else
+
+
+def describe(config: Config) -> List[Dict[str, Any]]:
+    """Describe instances running the SSM agent."""
+
+    instances_names = describe_instances_names(config)
+
+    client = boto3.client("ssm", region_name=config.get("region", None))
+
+    response = client.describe_instance_information()
+
+    instances = [
+        {
+            "ID": i["InstanceId"],
+            "Name": instances_names.get(i["InstanceId"], None),
+            "PingStatus": i["PingStatus"],
+            "Platform": f'{i["PlatformName"]} {i["PlatformVersion"]}',
+            "AgentVersion": i["AgentVersion"],
+        }
+        for i in response["InstanceInformationList"]
+    ]
+
+    return instances
+
+
+def describe_instances_names(config: Config) -> Dict[str, Optional[str]]:
+    """List EC2 instance names in the region."""
+
+    ec2_client = boto3.client("ec2", region_name=config.get("region", None))
+
+    response = ec2_client.describe_instances()
+
+    instances = {
+        i["InstanceId"]: first_or_else([t["Value"] for t in i.get("Tags", []) if t["Key"] == "Name"], None)
+        for r in response["Reservations"]
+        for i in r["Instances"]
+    }
+
+    return instances
