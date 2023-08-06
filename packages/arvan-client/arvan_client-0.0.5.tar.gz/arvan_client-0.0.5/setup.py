4a3b0ca85c7da1a43e47917abfabe5c901fa7f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:40 +0000

    Add package arvan-client/0.0.5/arvan_client-0.0.5.tar.gz

---
+++ packages/arvan-client/arvan_client-0.0.5.tar.gz/arvan_client-0.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+version = "0.0.5"
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arvan_client",
+    version=version,
+    author="Sajad Rahimi",
+    author_email="rahimisajad@outlook.com",
+    description="A Python SDK for ArvanCloud API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sajadrahimi/arvan-client",
+    project_urls={
+        "Bug Tracker": "https://github.com/sajadrahimi/arvan-client/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"arvan_client": "arvan_client"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
