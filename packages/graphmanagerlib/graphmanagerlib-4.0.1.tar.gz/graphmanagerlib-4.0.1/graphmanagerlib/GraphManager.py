9a91a1da5b9c3f1b9dcfd382681d29b82e413b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:41 +0000

    Add package graphmanagerlib/4.0.1/graphmanagerlib-4.0.1.tar.gz

---
+++ packages/graphmanagerlib/graphmanagerlib-4.0.1.tar.gz/graphmanagerlib-4.0.1/graphmanagerlib/GraphManager.py
@@ -0,0 +1,22 @@
+import csv
+from graphmanagerlib.Data_for_gcn import get_data
+from graphmanagerlib.Model_formation import CreateTrainAndSaveGCN
+from graphmanagerlib.Inference import predictionsUnique
+
+
+class GraphManager:
+    def CreateTrainAndSaveGCN(self,documents,saved_folder, tagsPath):
+        tags = self._getTagsList(tagsPath)
+        traindata, testdata = get_data(documents=documents, tags=tags)
+        return CreateTrainAndSaveGCN(traindata, testdata,saved_folder, len(tags))
+
+    def _getTagsList(self, tagsPath):
+        tags = []
+        with open(tagsPath, newline='') as inputfile:
+            for row in csv.reader(inputfile):
+                tags.append(row[0])
+        return tags
+
+    def single_prediction(self, saved_model_folder, document_to_predict_json, tagsPath):
+        tags = self._getTagsList(tagsPath)
+        return predictionsUnique(saved_model_folder, document_to_predict_json, tags)
