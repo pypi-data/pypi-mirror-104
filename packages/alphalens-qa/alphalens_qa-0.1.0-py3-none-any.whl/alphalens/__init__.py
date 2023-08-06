b3739f837debb6de5862f1ba5cf218f4b5ad53fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:41 +0000

    Add package alphalens-qa/0.1.0/alphalens_qa-0.1.0-py3-none-any.whl

---
+++ packages/alphalens-qa/alphalens_qa-0.1.0-py3-none-any.whl/alphalens/__init__.py
@@ -0,0 +1,11 @@
+from . import performance
+from . import plotting
+from . import tears
+from . import utils
+
+
+
+__version__ = '0.1.0'
+
+
+__all__ = ['performance', 'plotting', 'tears', 'utils']
