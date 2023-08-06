967f11076a2d9dfc76a651ad2660e6a32900a0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:07 +0000

    Add package freexgraph/1.2.2/freexgraph-1.2.2.tar.gz

---
+++ packages/freexgraph/freexgraph-1.2.2.tar.gz/freexgraph-1.2.2/freexgraph/standard_visitor.py
@@ -0,0 +1,146 @@
+# MIT License
+#
+# Copyright (c) 2021 Quentin Balland
+# Project : https://github.com/FreeYourSoul/FreExGraph
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import networkx as nx
+
+from typing import Callable, Optional, List
+
+from freexgraph import AbstractVisitor, FreExNode
+from freexgraph.freexgraph import root_node
+
+
+class FindFirstVisitor(AbstractVisitor):
+    """Find the first occurrence of a node that follow the given predicate
+
+    result value is set to a reference to the value found.
+    This is a mutable visitor : If a modification is to be done on the found node, it can be done in the predicate
+    """
+
+    result: Optional[FreExNode] = None
+    """reference to the node found after visitation that follow the predicate, stay None if none found """
+
+    _predicate: Callable
+
+    def __init__(self, predicate: Callable, **kwargs):
+        """
+        :param predicate: has to take an argument (FreExNode type) and return a bool, will determine what result is
+        expected from the graph
+        """
+        super().__init__(**kwargs)
+        self._predicate = predicate
+
+    def visit_standard(self, node: FreExNode) -> bool:
+        if node.id != root_node and self._predicate(node):
+            self.result = node
+            # result found, stop visitation
+            return False
+        return True
+
+    def hook_start(self):
+        self.result = None
+
+    def found(self) -> bool:
+        return self.result is not None
+
+
+class FindAllVisitor(AbstractVisitor):
+    results: List[FreExNode]
+    """reference list of nodes found after visitation that follow the predicate, stay None if none found """
+
+    _predicate: Callable
+
+    def __init__(self, predicate: Callable, **kwargs):
+        super().__init__(**kwargs)
+        self.results = []
+        self._predicate = predicate
+
+    def visit_standard(self, node: FreExNode) -> bool:
+        if node.id != root_node and self._predicate(node):
+            self.results.append(node)
+        return True
+
+    def hook_start(self):
+        self.results = []
+
+    def count(self) -> int:
+        return len(self.results)
+
+
+class ValidateGraphIntegrity(AbstractVisitor):
+    """Validate the integrity of the graph.
+
+    All the data are added if the API is used correctly, but if any bypass has been used (which you should never do)
+    This visitor will help figuring out what is the error.
+
+    * Check that each and every parents exists in the graph
+    * Check all node has a graph_ref assigned
+    * Check all node has an id set
+    * Check all node has a valid depth
+    * Check that the graph doesn't do a cycle (cyclic graph) Impossible by design, parent are checked at node add
+    """
+
+    _first_check = True
+    """Checks that require to be done only once are not done for each node visit"""
+
+    def visit_standard(self, node: FreExNode) -> bool:
+        if self._first_check:
+            try:
+                # impossible by design ( as we check if parents exists before adding them )
+                assert (
+                    len(list(nx.find_cycle(node.graph_ref))) == 0
+                ), "Error on graph : Cycle found"
+            except:
+                pass
+            self._first_check = False
+
+        assert isinstance(
+            node, FreExNode
+        ), f"Error on node type {type(node).__name__}. Every node in the graph should be deriving from FreExNode"
+
+        assert node.id, f"Error on node {node}, doesn't contains id"
+        assert (
+            node.id == root_node or len(node.parents) > 0
+        ), f"Internal library error: {node}, doesn't have parent (root should at least be added)"
+
+        assert (
+            node.graph_ref is not None
+        ), f"Error on node {node}, doesn't contains a ref_graph"
+        assert (
+            node.depth >= 0
+        ), f"Internal library error: {node}, impossible depth (should be positive)"
+
+        for p in node.parents:
+            assert node.graph_ref.has_node(
+                p
+            ), f"Error on node {node}, parent {p} doesn't exist in the graph"
+
+        return True
+
+
+def is_standard_visitor(visitor_instance):
+    return any(
+        [
+            isinstance(visitor_instance, c)
+            for c in [ValidateGraphIntegrity, FindAllVisitor, FindFirstVisitor]
+        ]
+    )
---
+++ packages/freexgraph/freexgraph-1.2.2.tar.gz/freexgraph-1.2.2/freexgraph/visitor.py
@@ -0,0 +1,225 @@
+# MIT License
+#
+# Copyright (c) 2021 Quentin Balland
+# Project : https://github.com/FreeYourSoul/FreExGraph
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import networkx as nx
+
+from typing import List, Tuple, Callable
+from tqdm import tqdm
+
+from freexgraph.freexgraph import FreExNode, GraphNode, AnyVisitor, root_node
+
+
+def _get_len(sorted_node_list: List[Tuple], with_progress_bar: bool) -> int:
+    if not with_progress_bar:
+        return 0
+    return len([n for n in sorted_node_list])
+
+
+def _filter_graph_root_for_visitation(root: FreExNode, is_reversed: bool):
+    if root.graph_ref is None:
+        return [root]
+    depth: int = root.depth
+    sorted_node_list = list(nx.lexicographical_topological_sort(root.graph_ref))
+    if is_reversed:
+        sorted_node_list = list(reversed(sorted_node_list))
+
+    if depth == 0:
+        return sorted_node_list
+    node_content = [
+        root.graph_ref.nodes[node_id]["content"] for node_id in sorted_node_list
+    ]
+    if is_reversed:
+        return [n.id for n in node_content if n.depth <= depth and n.id != root.id]
+    return [n.id for n in node_content if n.depth >= depth and n.id != root.id]
+
+
+class AbstractVisitor:
+    """Base class for Visitor.
+
+    Any custom (or standard visitor) has to be inheriting from this class. It should NOT override the visit method !
+
+    side_notes:
+        The different nodes implementation will override their accept method and redirect to a proper method of the
+        visitor. You can implement any method (of any name) that will be called by your custom nodes.
+
+        By convention, the names called by node.accept should match this regex `visit_*`.
+        but it is not enforced in any way
+    """
+
+    with_progress_bar: bool
+    is_reversed: bool
+
+    # List of Predicate/Hook
+    __custom_hooks: List[Tuple[Callable, Callable]]
+
+    def __init__(self, *, with_progress_bar: bool = False, is_reversed: bool = False):
+        """Base class instantiation
+
+        :param with_progress_bar: have a terminal tqdm progression bar while traversing the graph
+        :param is_reversed: traverse the graph in a reverse topological order
+        """
+        self.with_progress_bar = with_progress_bar
+        self.is_reversed = is_reversed
+        self.__custom_hooks = []
+
+    def visit(self, root: FreExNode) -> bool:
+        self.hook_start()
+        not_interrupted = self.apply_visitation_(root)
+        if not_interrupted:
+            self.hook_end()
+        return not_interrupted
+
+    def apply_visitation_(self, root: FreExNode) -> bool:
+        """do not override / directly use. Internal visitation method, use visit(root) instead"""
+
+        sorted_node_list = _filter_graph_root_for_visitation(root, self.is_reversed)
+
+        with tqdm(
+            total=_get_len(sorted_node_list, self.with_progress_bar),
+            disable=not self.with_progress_bar,
+        ) as pbar:
+            pbar.set_description(f"Processing Visitor {type(self).__name__}")
+            for node_id in sorted_node_list:
+                if len(sorted_node_list) == 1:
+                    node = root
+                else:
+                    node = root.graph_ref.nodes[node_id]["content"]
+
+                # Trigger custom hook
+                for predicate, hook in self.__custom_hooks:
+                    if node.id != root_node and predicate(node):
+                        hook(node)
+
+                if not node.apply_accept_(self):
+                    return False
+                pbar.set_postfix({"node": node_id})
+                pbar.update()
+        return True
+
+    def hook_start_graph_node(self, gn: GraphNode):
+        """Hook to implement in order to do an action at the start of a graph node
+
+        :param gn: graph node started
+        """
+        pass
+
+    def hook_end_graph_node(self, gn: GraphNode):
+        """Hook to implement in order to do an action at the start of a graph node
+
+        :param gn: graph node that finished
+        """
+        pass
+
+    def hook_start(self):
+        """Hook to implement in order to do an action at the start of the visitation"""
+        pass
+
+    def hook_end(self):
+        """Hook to implement in order to do an action at the end of the visitation"""
+        pass
+
+    def register_custom_hook(self, *, predicate: Callable, hook: Callable):
+        """Register a custom hook
+
+        Provided predicate which will be applied on each node during visitation, if it return true, the provided hook is
+        executed (with visited node given as parameter)
+
+        :param predicate: check node against to trigger hook
+        :param hook: Callable to trigger in case the predicate is returning True for a visited node
+        """
+        self.__custom_hooks.append((predicate, hook))
+
+
+class VisitorComposer:
+    """Class to compose visitor together"""
+
+    _sequential_before: List[AnyVisitor]
+    _action_composed: List[AnyVisitor]
+    _sequential_after: List[AnyVisitor]
+    _is_reversed: bool
+    _with_progress_bar: bool
+
+    def __init__(
+        self,
+        actions: List[AnyVisitor],
+        *,
+        before: List[AnyVisitor] = None,
+        after: List[AnyVisitor] = None,
+        progress_bar_on_actions: bool = False,
+    ):
+        """
+
+        :param actions:
+        :param before:
+        :param after:
+        :param progress_bar_on_actions:
+        """
+        reversed_action: List[bool] = [rev.is_reversed for rev in actions]
+        assert all(reversed_action) or not any(
+            reversed_action
+        ), "Cannot compose reversed and non reversed Visitor together"
+
+        self._is_reversed = reversed_action[0]
+        self._with_progress_bar = progress_bar_on_actions
+        self._action_composed = actions
+        self._sequential_before = before or []
+        self._sequential_after = after or []
+
+    def visit(self, root: FreExNode) -> bool:
+        to_continue = True
+        for b in self._sequential_before:
+            to_continue &= b.visit(root)
+            if not to_continue:
+                return False
+
+        if not self._composed_visit(root):
+            return False
+
+        for b in self._sequential_after:
+            to_continue &= b.visit(root)
+            if not to_continue:
+                return False
+
+        return to_continue
+
+    def _composed_visit(self, root: FreExNode) -> bool:
+        sorted_node_list = _filter_graph_root_for_visitation(root, self._is_reversed)
+
+        with tqdm(
+            total=_get_len(sorted_node_list, self._with_progress_bar),
+            disable=not self._with_progress_bar,
+        ) as pbar:
+            pbar.set_description("Processing Composed Visitor")
+
+            for node_id in sorted_node_list:
+                # do the visitation for the node on each action visitor
+                for action_visitor in self._action_composed:
+                    if not root.graph_ref.nodes[node_id]["content"].apply_accept_(
+                        action_visitor
+                    ):
+                        return False
+
+                pbar.set_postfix({"node": node_id})
+                pbar.update()
+
+        return True
