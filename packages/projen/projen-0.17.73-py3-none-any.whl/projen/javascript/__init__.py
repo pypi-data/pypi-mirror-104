e3502c0f864bdf4252d024835b93f62de9b50df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:05 +0000

    Add package projen/0.17.73/projen-0.17.73-py3-none-any.whl

---
+++ packages/projen/projen-0.17.73-py3-none-any.whl/projen/javascript/__init__.py
@@ -0,0 +1,108 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from .._jsii import *
+
+from .. import Component as _Component_2b0ad27f, Project as _Project_57d89203
+
+
+class Projenrc(
+    _Component_2b0ad27f,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="projen.javascript.Projenrc",
+):
+    '''(experimental) Sets up a typescript project to use TypeScript for projenrc.
+
+    :stability: experimental
+    '''
+
+    def __init__(
+        self,
+        project: _Project_57d89203,
+        *,
+        comments: typing.Optional[builtins.bool] = None,
+        filename: typing.Optional[builtins.str] = None,
+    ) -> None:
+        '''
+        :param project: -
+        :param comments: (experimental) Include commented out properties. Default: true
+        :param filename: (experimental) The name of the projenrc file. Default: ".projenrc.js"
+
+        :stability: experimental
+        '''
+        options = ProjenrcOptions(comments=comments, filename=filename)
+
+        jsii.create(Projenrc, self, [project, options])
+
+
+@jsii.data_type(
+    jsii_type="projen.javascript.ProjenrcOptions",
+    jsii_struct_bases=[],
+    name_mapping={"comments": "comments", "filename": "filename"},
+)
+class ProjenrcOptions:
+    def __init__(
+        self,
+        *,
+        comments: typing.Optional[builtins.bool] = None,
+        filename: typing.Optional[builtins.str] = None,
+    ) -> None:
+        '''
+        :param comments: (experimental) Include commented out properties. Default: true
+        :param filename: (experimental) The name of the projenrc file. Default: ".projenrc.js"
+
+        :stability: experimental
+        '''
+        self._values: typing.Dict[str, typing.Any] = {}
+        if comments is not None:
+            self._values["comments"] = comments
+        if filename is not None:
+            self._values["filename"] = filename
+
+    @builtins.property
+    def comments(self) -> typing.Optional[builtins.bool]:
+        '''(experimental) Include commented out properties.
+
+        :default: true
+
+        :stability: experimental
+        '''
+        result = self._values.get("comments")
+        return typing.cast(typing.Optional[builtins.bool], result)
+
+    @builtins.property
+    def filename(self) -> typing.Optional[builtins.str]:
+        '''(experimental) The name of the projenrc file.
+
+        :default: ".projenrc.js"
+
+        :stability: experimental
+        '''
+        result = self._values.get("filename")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "ProjenrcOptions(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "Projenrc",
+    "ProjenrcOptions",
+]
+
+publication.publish()
