ae904b67f8bfe86d0ce7edcdcdba57c669d0f211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:41 +0000

    Add package TCRnumba/0.1.1/TCRnumba-0.1.1.tar.gz

---
+++ packages/TCRnumba/TCRnumba-0.1.1.tar.gz/TCRnumba-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="TCRnumba",
+    version='0.1.1',
+    description="some description",
+    url="https://github.com/Paul44444/capybaras_test",
+    author='Paul',
+    author_email='richtepp@physik.hu-berlin.de',
+    license='BSD 2-clause',
+    packages=['TCRnumba'],
+    #install_requires=["mpi4py>=2.0","numpy", "numba", "scipy"], #TODO: finish this list
+    install_requires=["mpi4py>=2.0","numpy", "numba", "scipy"], 
+    #classifiers=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+) 
