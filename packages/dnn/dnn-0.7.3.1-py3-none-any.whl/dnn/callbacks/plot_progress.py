0648bec2e7a7da5359b93aae4a066f3e00b32b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:59 +0000

    Add package dnn/0.7.3.1/dnn-0.7.3.1-py3-none-any.whl

---
+++ packages/dnn/dnn-0.7.3.1-py3-none-any.whl/dnn/callbacks/plot_progress.py
@@ -0,0 +1,35 @@
+from tensorflow.keras.callbacks import Callback
+import matplotlib.pyplot as plt
+import os
+
+class PlotProgress (Callback):
+    def __init__(self, entity = 'loss'):
+        self.entity = entity
+        self.jpy = os.getenv ("JPY_PARENT_PID")
+
+    def on_train_begin (self, logs = {}):
+        if not self.jpy:
+            return
+        self.i = 0
+        self.x = []
+        self.losses = []
+        self.val_losses = []
+        self.fig = plt.figure ()
+        self.logs = []
+
+    def on_epoch_end (self, epoch, logs = {}):
+        if not self.jpy:
+            return
+        self.logs.append (logs)
+        self.x.append (self.i)
+        self.losses.append (logs.get('{}'.format (self.entity)))
+        val_val = logs.get('val_{}'.format (self.entity))
+        if val_val:
+            self.val_losses.append (logs.get('val_{}'.format (self.entity)))
+        self.i += 1
+
+        plt.plot (self.x, self.losses, label="{}".format (self.entity))
+        if self.val_losses:
+            plt.plot (self.x, self.val_losses, label="val_{}".format (self.entity))
+        plt.legend ()
+        plt.show ()
