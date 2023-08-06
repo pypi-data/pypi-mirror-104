16de00d27f6c39e6b7c6f0a03d5c098ff8cf272a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:38 +0000

    Add package mona-uds-client/0.0.8/mona_uds_client-0.0.8.tar.gz

---
+++ packages/mona-uds-client/mona_uds_client-0.0.8.tar.gz/mona_uds_client-0.0.8/mona_uds_client/mona_uds_client_test.py
@@ -0,0 +1,46 @@
+"""
+Simple example for how to run mona_uds_client.py
+"""
+import time
+import logging
+from os import environ
+from uuid import uuid1
+from random import random
+
+from mona_uds_client import MonaUdsClient, MonaSingleMessage
+
+NUM_BATCHES = 5
+
+
+def actual_run():
+    logging.getLogger().setLevel(logging.INFO)
+    client = MonaUdsClient("test_mona_user_id")
+    logging.getLogger().setLevel(environ.get("LOGLEVEL", logging.INFO))
+    logging.info("starting client test")
+    start_time = time.time()
+    test_batches = [
+        [{f"field_{i}": random() for i in range(150)} for _ in range(50)]  # nosec
+        for _ in range(NUM_BATCHES)
+    ]
+    successes = 0
+    for ind, batch in enumerate(test_batches):
+        messages = [
+            MonaSingleMessage(
+                contextId=str(uuid1()),
+                message=msg,
+                arcClass="TEST_CLASS",
+                exportTimestamp=1234567890,
+            )
+            for msg in batch
+        ]
+        if client.export(messages):
+            successes += 1
+        else:
+            logging.warning(f"failed on index {ind}")
+
+    logging.info(f"sent messges: {successes}/{NUM_BATCHES}")
+    logging.info(f"took {time.time() - start_time} sec")
+
+
+if __name__ == "__main__":
+    actual_run()
