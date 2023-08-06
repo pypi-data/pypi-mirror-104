9f4f592cb0e79c8666e83424326834c78a60918c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:37 +0000

    Add package kh-probability/0.1/kh_probability-0.1.tar.gz

---
+++ packages/kh-probability/kh_probability-0.1.tar.gz/kh_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='kh_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['kh_probability'],
+      zip_safe=False)
