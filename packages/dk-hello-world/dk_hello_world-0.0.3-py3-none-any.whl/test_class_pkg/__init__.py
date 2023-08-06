ef4df6c62c4a391f2353b7c2f45e2392a9632392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package dk-hello-world/0.0.3/dk_hello_world-0.0.3-py3-none-any.whl

---
+++ packages/dk-hello-world/dk_hello_world-0.0.3-py3-none-any.whl/test_class_pkg/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 30 23:01:14 2021
+
+@author: danish
+"""
+
+class gt_lib:
+    
+    def __init__(self):
+        pass
+        
+    def fun1(self, n):
+        print(n, 'fn1')
+        
+    def fun2(self, n):
+        print(n, 'fn2')
+        
+
+
+gt_lib().fun1(n = 2)
