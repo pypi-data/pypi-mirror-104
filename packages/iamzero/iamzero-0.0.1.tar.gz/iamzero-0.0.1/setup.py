1ca1ed34c52ea43472f9d025b1592f51fc65c675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package iamzero/0.0.1/iamzero-0.0.1.tar.gz

---
+++ packages/iamzero/iamzero-0.0.1.tar.gz/iamzero-0.0.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['iamzero', 'iamzero.instrumentation']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['botocore>=1.20.54,<2.0.0',
+ 'requests>=2.25.1,<3.0.0',
+ 'statsd>=3.3.0,<4.0.0',
+ 'wrapt>=1.12.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'iamzero',
+    'version': '0.0.1',
+    'description': 'iam-zero least-privilege instrumentation client for Python',
+    'long_description': None,
+    'author': 'Chris Norman',
+    'author_email': 'chris@exponentlabs.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
