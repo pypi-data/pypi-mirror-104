2c8645c28fd48201a69720c9826adda6b29033fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:07 +0000

    Add package lcapy/0.88/lcapy-0.88.tar.gz

---
+++ packages/lcapy/lcapy-0.88.tar.gz/lcapy-0.88/lcapy/tests/test_functions.py
@@ -0,0 +1,60 @@
+from lcapy import *
+from lcapy.sym import tausym
+from lcapy.texpr import TimeDomainExpression
+from lcapy.omegaexpr import AngularFourierDomainExpression
+import unittest
+
+
+class LcapyTester(unittest.TestCase):
+
+    """Unit tests for lcapy
+
+    """
+
+    def test_functions(self):
+
+        self.assertEqual(sinc(0), 1, "sinc(0)")
+        self.assertEqual(sinc(1), 0, "sinc(1)")
+        self.assertEqual(rect(0), 1, "rect(0)")
+        self.assertEqual(rect(1), 0, "rect(1)")
+        self.assertEqual(ui(1), 0, "ui(1)")
+        self.assertEqual(ui(0), 1, "ui(0)")
+        self.assertEqual(us(1), 1, "us(1)")
+        self.assertEqual(us(0), 1, "us(0)")                                
+        self.assertEqual(us(-1), 0, "us(-1)")
+
+        self.assertEqual(rect(n).rewrite(), us(n + 1 / 2) - us(n - 1 / 2), "rect(n)")
+
+        integral = Function(sym.Integral)
+        r = integral(u(t - tausym), (tausym, 0, oo))
+        e = u(t).convolve(u(t)).simplify()
+        
+        self.assertEqual(e, r, "simplify integral with u(t)")        
+
+        f0 = symbol('f_0')
+        c = cos(2 * pi * f0 * t)
+        self.assertEqual(type(c), TimeDomainExpression, "cos")
+
+        a = atan2(1, omega)
+        self.assertEqual(type(a), AngularFourierDomainExpression, "atan2")
+        
+    def test_heaviside(self):
+
+        self.assertEqual((u(t) * 3 * u(t)).simplify(), 3 * u(t), "u(t) * 3 * u(t)")                
+
+    def test_dirac_delta_simplify(self):
+
+        self.assertEqual(expr('delta(t) * exp(-3 * t)').simplify(),
+                         expr('delta(t)'),
+                         "delta(t) * exp(-3 * t)")
+
+        self.assertEqual(expr('exp(-3 * t) * delta(t)').simplify(),
+                         expr('delta(t)'),
+                         "exp(-3 * t) * delta(t)")
+
+        self.assertEqual((rect(t / 4) * delta(t - 1)).simplify(),
+                         delta(t - 1),
+                         "rect(t / 4) * delta(t - 1)")
+        
+
+        
