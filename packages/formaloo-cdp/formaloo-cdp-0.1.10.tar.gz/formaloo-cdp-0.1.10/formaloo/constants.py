4b0a45e736a183a458b197caffcc3e75595df228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:04 +0000

    Add package formaloo-cdp/0.1.10/formaloo-cdp-0.1.10.tar.gz

---
+++ packages/formaloo-cdp/formaloo-cdp-0.1.10.tar.gz/formaloo-cdp-0.1.10/formaloo/constants.py
@@ -0,0 +1,37 @@
+from .settings import V_1_0_API_BASE
+
+# ENDPOINTS
+V_1_0_AUTHORIZATION_TOKEN_ENDPOINT = '%soauth2/authorization-token/' % V_1_0_API_BASE
+
+V_1_0_CREATE_ACTIVITY_ENDPOINT = '%sactivities/' % V_1_0_API_BASE
+V_1_0_ACTIVITY_BATCH_ENDPOINT = '%sactivities/batch/' % V_1_0_API_BASE
+
+V_1_0_CREATE_CUSTOMER_ENDPOINT = '%scustomers/' % V_1_0_API_BASE
+V_1_0_CUSTOMERS_LIST_ENDPOINT = '%scustomers/' % V_1_0_API_BASE
+V_1_0_CUSTOMER_BATCH_ENDPOINT = '%scustomers/batch/' % V_1_0_API_BASE
+
+V_1_0_CREATE_TAGS_ENDPOINT = '%stags/' % V_1_0_API_BASE
+
+V_1_0_CREATE_GAMIFICATION_CALCULATION_JOB = '%sgamification-calculation-jobs/' % V_1_0_API_BASE
+
+# HEADERS
+APPLICATION_HEADER = 'x-api-key'
+AUTHORIZATION_HEADER = 'Authorization'
+AUTHORIZATION_BEARER = 'JWT'
+CREDENTIAL_BEARER = 'Basic'
+CREDENTIAL_GRAT_TYPE = 'client_credentials'
+
+# Settings
+AUTHORIZATION_TOKEN_TIMEOUT = 5*60
+
+# Helpers
+CUSTOMER_BASE_FIELDS = [
+    'first_name',
+    'last_name',
+    'full_name',
+    'email',
+    'phone_number',
+    'score',
+    'city',
+    'language',
+]
