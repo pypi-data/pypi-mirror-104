cdfe69ada8d2f2f6cf60d88f6c8f8ee227c5f998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:08 +0000

    Add package sid-kemendesa/0.2.0/sid-kemendesa-0.2.0.tar.gz

---
+++ packages/sid-kemendesa/sid-kemendesa-0.2.0.tar.gz/sid-kemendesa-0.2.0/sid_kemendesa/__init__.py
@@ -0,0 +1,19 @@
+from .deskripsi import DeskripsiDesa
+from .idm import IdmDesa
+from .desa import Desa
+from .search import ListResponse, HasilDesa, listKab, listKec, listDesa, search
+
+__version__ = "0.2.0"
+
+
+__all__ = [
+    "DeskripsiDesa",
+    "IdmDesa",
+    "Desa",
+    "ListResponse",
+    "HasilDesa",
+    "listKab",
+    "listKec",
+    "listDesa",
+    "search",
+]
