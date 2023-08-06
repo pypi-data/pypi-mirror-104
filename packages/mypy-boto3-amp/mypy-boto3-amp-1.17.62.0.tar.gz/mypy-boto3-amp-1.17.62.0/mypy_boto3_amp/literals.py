908b2446ed0d5bc4d1e9da854ae763fd0b58a69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package mypy-boto3-amp/1.17.62.0/mypy-boto3-amp-1.17.62.0.tar.gz

---
+++ packages/mypy-boto3-amp/mypy-boto3-amp-1.17.62.0.tar.gz/mypy-boto3-amp-1.17.62.0/mypy_boto3_amp/literals.py
@@ -0,0 +1,24 @@
+"""
+Main interface for amp service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_amp.literals import ListWorkspacesPaginatorName
+
+    data: ListWorkspacesPaginatorName = "list_workspaces"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("ListWorkspacesPaginatorName", "WorkspaceStatusCode")
+
+
+ListWorkspacesPaginatorName = Literal["list_workspaces"]
+WorkspaceStatusCode = Literal["ACTIVE", "CREATING", "CREATION_FAILED", "DELETING", "UPDATING"]
