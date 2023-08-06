aedea411c40f164bbf9ab1f101628dd159803a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:30 +0000

    Add package Finance-Ultron/0.3.0/Finance-Ultron-0.3.0.tar.gz

---
+++ packages/Finance-Ultron/Finance-Ultron-0.3.0.tar.gz/Finance-Ultron-0.3.0/ultron/factor/utilities.py
@@ -0,0 +1,20 @@
+import numpy as np
+import numba as nb
+
+@nb.njit(nogil=True, cache=True)
+def agg_mean(groups, x):
+    max_g = groups.max()
+    length, width = x.shape
+    res = np.zeros((max_g+1, width), dtype=np.float64)
+    bin_count = np.zeros(max_g+1, dtype=np.int32)
+
+    for i in range(length):
+        for j in range(width):
+            res[groups[i], j] += x[i, j]
+        bin_count[groups[i]] += 1
+
+    for i in range(max_g+1):
+        curr = bin_count[i]
+        for j in range(width):
+            res[i, j] /= curr
+    return res
