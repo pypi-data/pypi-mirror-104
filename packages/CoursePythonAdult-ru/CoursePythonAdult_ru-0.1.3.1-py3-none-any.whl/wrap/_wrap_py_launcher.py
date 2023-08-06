77f2fab13fcf9bb2393a53b111bc590f248797f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:25 +0000

    Add package CoursePythonAdult-ru/0.1.3.1/CoursePythonAdult_ru-0.1.3.1-py3-none-any.whl

---
+++ packages/CoursePythonAdult-ru/CoursePythonAdult_ru-0.1.3.1-py3-none-any.whl/wrap/_wrap_py_launcher.py
@@ -0,0 +1,25 @@
+# set module language
+import wrap_py
+from wrap_py import _transl
+
+_transl.set_lang("ru_RU")
+
+# translator for module strings
+from wrap_py._transl import translator as _
+
+# translate window title
+wrap_py.app.set_title(_(wrap_py.app.get_title()))
+
+# configure wrap_py
+wrap_py.make_nice_errors()
+
+# prepare data source
+import wds_files_CoursePythonAdult_1
+
+#first looks in specific sprites
+wrap_py.site.sprite_data_sources.append(wds_files_CoursePythonAdult_1.source)
+
+wrap_py.site.sprite_data_preload = False
+
+# start in multithreaded mode
+wrap_py.init()
