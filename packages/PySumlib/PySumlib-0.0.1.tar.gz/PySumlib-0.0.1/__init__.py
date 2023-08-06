7770b3f8385cb44db126cf8e901feba7bc2f8449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:13 +0000

    Add package PySumlib/0.0.1/PySumlib-0.0.1.tar.gz

---
+++ packages/PySumlib/PySumlib-0.0.1.tar.gz/PySumlib-0.0.1/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 17:26:05 2021
+
+@author: Dev SynerSense 3500
+"""
+
+
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
