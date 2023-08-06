c3d30f1cb7379ed5bb150646d59c4231900a0450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:27 +0000

    Add package firstuploaddistributions/0.1/firstuploaddistributions-0.1.tar.gz

---
+++ packages/firstuploaddistributions/firstuploaddistributions-0.1.tar.gz/firstuploaddistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='firstuploaddistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['firstuploaddistributions'],
+      zip_safe=False)
