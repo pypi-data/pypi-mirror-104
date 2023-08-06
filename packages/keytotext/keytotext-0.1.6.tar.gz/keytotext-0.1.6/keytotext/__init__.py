2d7173be6dcd12c427e6ec8be30c896bc5786ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:38 +0000

    Add package keytotext/0.1.6/keytotext-0.1.6.tar.gz

---
+++ packages/keytotext/keytotext-0.1.6.tar.gz/keytotext-0.1.6/keytotext/__init__.py
@@ -0,0 +1,11 @@
+"""
+Text Generation Using Keywords
+Keytotext converts a list of Keywords to sentences and larger texts
+"""
+
+
+__version__ = "0.1.6"
+
+from .pipeline import pipeline
+from .eval import eval
+from .trainer import trainer
