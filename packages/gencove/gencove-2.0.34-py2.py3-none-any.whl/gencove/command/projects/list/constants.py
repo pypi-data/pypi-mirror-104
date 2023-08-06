71dd877b794a29aa5c0b0b70b3dec0f25a73f945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:19 +0000

    Add package gencove/2.0.34/gencove-2.0.34-py2.py3-none-any.whl

---
+++ packages/gencove/gencove-2.0.34-py2.py3-none-any.whl/gencove/command/projects/list/constants.py
@@ -0,0 +1,18 @@
+"""Describe constants in projects subcommand."""
+from collections import namedtuple
+
+Project = namedtuple(
+    "Project",
+    [
+        "id",
+        "name",
+        "description",
+        "created",
+        "organization",
+        "sample_count",
+        "pipeline_capabilities",
+    ],
+)
+PipelineCapabilities = namedtuple(
+    "PipelineCapabilities", ["id", "name", "private", "merge_vcfs_enabled"]
+)
