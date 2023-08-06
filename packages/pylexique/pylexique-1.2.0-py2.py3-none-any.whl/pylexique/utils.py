3176b1da2e21a9102b45da7d1e995e4bbec669c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:36 +0000

    Add package pylexique/1.2.0/pylexique-1.2.0-py2.py3-none-any.whl

---
+++ packages/pylexique/pylexique-1.2.0-py2.py3-none-any.whl/pylexique/utils.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+
+
+def dataclass_with_default_init(_cls=None, *args, **kwargs):
+    """
+    | This class allows to define Frozen Dataclasses by bypassing the __init__.
+    | It is useful when the attributes of the Dataclass are dynamically generated,
+    | while still allowing the attributes of the class to be frozen after initialisation.
+
+    :rtype: object
+    """
+    def wrap(cls):
+        # Save the current __init__ and remove it so dataclass will
+        # create the default __init__.
+        user_init = getattr(cls, "__init__")
+        delattr(cls, "__init__")
+
+        # let dataclass process our class.
+        result = dataclass(cls, *args, **kwargs)
+
+        # Restore the user's __init__ save the default init to __default_init__.
+        setattr(result, "__default_init__", result.__init__)
+        setattr(result, "__init__", user_init)
+
+        # Just in case that dataclass will return a new instance,
+        # (currently, does not happen), restore cls's __init__.
+        if result is not cls:
+            setattr(cls, "__init__", user_init)
+
+        return result
+
+    # Support both dataclass_with_default_init() and dataclass_with_default_init
+    if _cls is None:
+        return wrap
+    else:
+        return wrap(_cls)
+
