286017376b3cac8af424d5e9bd41acdab94abc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:44 +0000

    Add package ZEVIT-AIH-SDK/0.2.3/ZEVIT_AIH_SDK-0.2.3.tar.gz

---
+++ packages/ZEVIT-AIH-SDK/ZEVIT_AIH_SDK-0.2.3.tar.gz/ZEVIT_AIH_SDK-0.2.3/AIH_SDK/v2/DataUpload/DataType.py
@@ -0,0 +1,9 @@
+from AIH_SDK.v2.DataUpload.DataUploadObject import DataUploadObject
+
+
+class DataType(DataUploadObject):
+    
+    def __init__(self):
+        super().__init__()
+        self._endpoint = 'DataTypes'
+
