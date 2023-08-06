f0a119358269effb5498625482f4c3b63b22c8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:40 +0000

    Add package gecco-tool/0.6.2/gecco_tool-0.6.2-py2.py3-none-any.whl

---
+++ packages/gecco-tool/gecco_tool-0.6.2-py2.py3-none-any.whl/gecco/__init__.py
@@ -0,0 +1,11 @@
+"""Biosynthetic Gene Cluster prediction with Conditional Random Fields.
+"""
+
+__author__ = "Martin Larralde"
+__license__ = "GPLv3"
+__version__ = (
+    __import__("pkg_resources")
+    .resource_string(__name__, "_version.txt")
+    .strip()
+    .decode("ascii")
+)
