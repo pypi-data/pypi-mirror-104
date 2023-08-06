113bdbd1f4ed5a898b654b8450920460d71220b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:33 +0000

    Add package covml/0.1/covml-0.1.tar.gz

---
+++ packages/covml/covml-0.1.tar.gz/covml-0.1/setup.py
@@ -0,0 +1,17 @@
+# import pathlib
+from setuptools import setup
+
+# This call to setup() does all the work
+setup(
+    name="covml",
+    version="0.1",
+    description="Corona ML predictor",
+    author="Shaurya Pratap Singh",
+    author_email="shaurya.p.singh21@gmail.com",
+    license="MIT",
+    packages=["covml"],
+    include_package_data=True,
+    # scripts=['bin/pyserved', 'bin/pyserved-c'],
+)
+
+
