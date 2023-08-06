71f86c91f31438fdd8628a4df1cbc83a8f9e35b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:12 +0000

    Add package clustergrammer-pandas2/1.13.8/clustergrammer-pandas2-1.13.8.tar.gz

---
+++ packages/clustergrammer-pandas2/clustergrammer-pandas2-1.13.8.tar.gz/clustergrammer-pandas2-1.13.8/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'clustergrammer-pandas2',
+  packages = ['clustergrammer'],
+  version = '1.13.8',
+  description = 'A python module for the Clustergrammer visualization project',
+  author = 'Nicolas Fernandez',
+  author_email = 'nickfloresfernandez@gmail.com',
+  url = 'https://github.com/scvannost/clustergrammer-py',
+  download_url = 'https://github.com/scvannost/clustergrammer-py/archive/refs/tags/1.13.8.tar.gz',
+  keywords = ['testing'],
+  classifiers = [],
+)
