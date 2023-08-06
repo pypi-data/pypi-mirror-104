b32e35ee057932e464190d3e370f64c045f7e7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:58 +0000

    Add package TCRnumba/0.2.5/TCRnumba-0.2.5.tar.gz

---
+++ packages/TCRnumba/TCRnumba-0.2.5.tar.gz/TCRnumba-0.2.5/TCRnumba/__init__.py
@@ -0,0 +1,11 @@
+"""
+info: TCR_numba
+"""
+
+__version__ = "0.1.0"
+__author__ = "Paul Richter"
+__credits__ = "ETH Zurich"
+
+from . import graph_numba
+#from . import make_net
+from . import convert_pure
