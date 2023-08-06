961bfd12532b9191d9aed4abb19d0aa8bf7679cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:11 +0000

    Add package loginhelper/0.0.5/loginhelper-0.0.5.tar.gz

---
+++ packages/loginhelper/loginhelper-0.0.5.tar.gz/loginhelper-0.0.5/release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rm -rf build/ dist/
+python3 setup.py sdist bdist_wheel
+
+export TWINE_REPOSITORY_URL=https://upload.pypi.org/legacy/
+export TWINE_USERNAME=ssfanli
+
+## set password to keyring
+# keyring set https://upload.pypi.org/legacy/ $TWINE_USERNAME
+
+exec twine upload dist/*
