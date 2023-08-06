acea02b1e280f6437dafe8e143cc11d1dcf4c069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:15 +0000

    Add package everysk/1.0.1/everysk-1.0.1.tar.gz

---
+++ packages/everysk/everysk-1.0.1.tar.gz/everysk-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='everysk',
+      packages=['everysk', 'everysk.api_resources'],
+      version='1.0.1',
+      description='Python client for Everysk API',
+      author='Everysk Technologies',
+      author_email='contact@everysk.com',
+      url='https://github.com/Everysk/Everysk-API-Python',
+      install_requires=['requests'],
+      keywords=['everysk'],
+      license='MIT',
+      zip_safe=False)
