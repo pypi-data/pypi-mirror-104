df494959d7f49aeef88bbe269bd391b027aee1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:18 +0000

    Add package mysql_autoxtrabackup/2.0.2/mysql_autoxtrabackup-2.0.2-py3-none-any.whl

---
+++ packages/mysql_autoxtrabackup/mysql_autoxtrabackup-2.0.2-py3-none-any.whl/mysql_autoxtrabackup/__init__.py
@@ -0,0 +1,5 @@
+"""MySQL-AutoXtrabackup command-line tool, for automating tedious MySQL physical backups management
+using Percona Xtrabackup"""
+from .utils import version
+
+__version__ = version.VERSION
