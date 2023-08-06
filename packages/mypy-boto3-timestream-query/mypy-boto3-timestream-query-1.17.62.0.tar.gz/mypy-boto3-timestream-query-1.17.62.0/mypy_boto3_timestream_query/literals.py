6c6dbc625f46e0eff323e98623a736852197d38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package mypy-boto3-timestream-query/1.17.62.0/mypy-boto3-timestream-query-1.17.62.0.tar.gz

---
+++ packages/mypy-boto3-timestream-query/mypy-boto3-timestream-query-1.17.62.0.tar.gz/mypy-boto3-timestream-query-1.17.62.0/mypy_boto3_timestream_query/literals.py
@@ -0,0 +1,23 @@
+"""
+Main interface for timestream-query service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_timestream_query.literals import QueryPaginatorName
+
+    data: QueryPaginatorName = "query"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("QueryPaginatorName",)
+
+
+QueryPaginatorName = Literal["query"]
