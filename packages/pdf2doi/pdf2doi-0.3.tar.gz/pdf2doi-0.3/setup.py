dad7bf96bc508b53217bc66372505f4ae2eb518b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:48 +0000

    Add package pdf2doi/0.3/pdf2doi-0.3.tar.gz

---
+++ packages/pdf2doi/pdf2doi-0.3.tar.gz/pdf2doi-0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+setuptools.setup(name='pdf2doi',
+      version='0.3',
+      description='A python library/command-line tool to retrieve the DOI or other identifiers (e.g. arXiv) from pdf files of a publications.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/MicheleCotrufo/pdf2doi',
+      author='Michele Cotrufo',
+      author_email='michele.cotrufo@gmail.com',
+      license='MIT',
+      entry_points = {
+        'console_scripts': ["pdf2doi = pdf2doi.pdf2doi:main"],
+      },
+      packages=['pdf2doi'],
+      install_requires= required_packages,
+      zip_safe=False)
