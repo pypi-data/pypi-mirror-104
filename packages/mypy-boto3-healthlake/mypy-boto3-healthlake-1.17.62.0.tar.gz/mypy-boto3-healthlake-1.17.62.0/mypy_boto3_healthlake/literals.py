9f2c97745143cea349450fff835a59176afb9b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:21 +0000

    Add package mypy-boto3-healthlake/1.17.62.0/mypy-boto3-healthlake-1.17.62.0.tar.gz

---
+++ packages/mypy-boto3-healthlake/mypy-boto3-healthlake-1.17.62.0.tar.gz/mypy-boto3-healthlake-1.17.62.0/mypy_boto3_healthlake/literals.py
@@ -0,0 +1,26 @@
+"""
+Main interface for healthlake service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_healthlake.literals import DatastoreStatus
+
+    data: DatastoreStatus = "ACTIVE"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("DatastoreStatus", "FHIRVersion", "JobStatus", "PreloadDataType")
+
+
+DatastoreStatus = Literal["ACTIVE", "CREATING", "DELETED", "DELETING"]
+FHIRVersion = Literal["R4"]
+JobStatus = Literal["COMPLETED", "FAILED", "IN_PROGRESS", "SUBMITTED"]
+PreloadDataType = Literal["SYNTHEA"]
