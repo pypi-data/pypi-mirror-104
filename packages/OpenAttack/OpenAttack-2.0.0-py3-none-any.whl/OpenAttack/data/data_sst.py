dc085ced978df774c3dbf63133682bbcc2806339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package OpenAttack/2.0.0/OpenAttack-2.0.0-py3-none-any.whl

---
+++ packages/OpenAttack/OpenAttack-2.0.0-py3-none-any.whl/OpenAttack/data/data_sst.py
@@ -0,0 +1,29 @@
+"""
+:type: a tuple of three :py:class:`.Dataset` s, `(train, valid, test)`.
+:Size: 1.116MB
+
+SST dataset which is used to train victim models.
+`[page] <https://nlp.stanford.edu/sentiment/>`__
+"""
+import pickle
+import os
+
+NAME = "Dataset.SST"
+DOWNLOAD = "https://cdn.data.thunlp.org/TAADToolbox/dataset/sst.pkl"
+
+
+def LOAD(path):
+    def mapping(data):
+        from OpenAttack.utils.dataset import Dataset
+        return Dataset([
+            DataInstance(
+                x=it[0],
+                y=it[1]
+            ) for it in data
+        ], copy=False)
+    
+    train, valid, test = pickle.load(open(path, "rb"))
+    return mapping(train), mapping(valid), mapping(test)
+
+    # return datasets.load_dataset(os.path.join(os.getcwd(), "data", "sst_model.py"), data_files=path)
+    #return datasets.load_dataset('sst')
