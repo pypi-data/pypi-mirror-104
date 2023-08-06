fce6cede09e04e7d0f2e60f2a8084849c67c480f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:27 +0000

    Add package eventual/0.4.2/eventual-0.4.2.tar.gz

---
+++ packages/eventual/eventual-0.4.2.tar.gz/eventual-0.4.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eventual', 'eventual.dispatch', 'eventual.model', 'eventual.util']
+
+package_data = \
+{'': ['*']}
+
+extras_require = \
+{'rmq': ['eventual-rmq>=0,<1'], 'tortoise': ['eventual-tortoise>=0,<1']}
+
+setup_kwargs = {
+    'name': 'eventual',
+    'version': '0.4.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Ivan Dmitriesvkii',
+    'author_email': 'ivan.dmitrievsky@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
