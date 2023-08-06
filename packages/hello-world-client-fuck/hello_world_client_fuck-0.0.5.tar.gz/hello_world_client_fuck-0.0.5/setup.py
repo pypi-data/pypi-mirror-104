b280edfef2f82ac6a971595243677281c920d5ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:10 +0000

    Add package hello-world-client-fuck/0.0.5/hello_world_client_fuck-0.0.5.tar.gz

---
+++ packages/hello-world-client-fuck/hello_world_client_fuck-0.0.5.tar.gz/hello_world_client_fuck-0.0.5/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(name='hello_world_client_fuck',
+      version='0.0.5',
+      author='hello world',
+      include_package_data=True,
+      zip_safe=False,
+      url="https://pypi.org/",
+      author_email='',
+      python_requires=">=3.3",
+      packages=['src'],
+      license="MIT",
+      install_requires=[])
