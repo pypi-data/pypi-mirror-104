4e6c82ed1f5e03013bc82161b40bdaf7baff4190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:56 +0000

    Add package neobulkmp/0.0.3.dev0/neobulkmp-0.0.3.dev0.tar.gz

---
+++ packages/neobulkmp/neobulkmp-0.0.3.dev0.tar.gz/neobulkmp-0.0.3.dev0/demo.py
@@ -0,0 +1,62 @@
+
+if __name__ == "__main__":
+    SCRIPT_DIR = os.path.dirname(
+        os.path.realpath(os.path.join(
+            os.getcwd(), os.path.expanduser(__file__)))
+    )
+    MODULE_ROOT_DIR = os.path.join(SCRIPT_DIR, "..")
+    sys.path.insert(0, os.path.normpath(MODULE_ROOT_DIR))
+from neobulkmp import Manager, WorkerSourcing
+import sys
+import os
+import logging
+import graphio
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(processName)-8s %(module)-8s %(levelname)-8s:  %(message)s',
+                    handlers=[logging.StreamHandler((sys.stdout))])
+
+
+movies = []
+movies.append({"title": "Matrix", "Actors": [
+              "Keanu Reeves", "Carrie-Anne Moss", "Laurence Fishburne"]})
+movies.append({"title": "Ghost in the Shell", "Actors": [
+              "Atsuko Tanaka", "Akio Otsuka", "Iemasa Kayumi"]})
+movies.append({"title": "Bill & Ted's Excellent Adventure", "Actors": [
+              "Keanu Reeves", "Alex Winter", "George Carlin"]})
+movies.append({"title": "Dogma", "Actors": [
+              "Ben Affleck", "Jason Lee", "George Carlin"]})
+movies.append({"title": "Almost Famous", "Actors": [
+              "Patrick Fugit", "Jason Lee", "Frances McDormand"]})
+
+
+def parsing_func(worker, title, Actors):
+    # Create nodeset with movie
+    movieset = graphio.NodeSet(["Movie"], merge_keys=["title"])
+    movieset.add_node({"title": title})
+    # Send Nodeset with movie to neobulkmp manager
+    yield movieset
+    # Create Actor Nodeset and connect actors to movie
+    actorset = graphio.NodeSet(["Actor"], merge_keys=["name"])
+    acts_in_movie = graphio.RelationshipSet(
+        "ACTS_IN", ["Actor"], ["Movie"], ["name"], ["title"])
+    for actor in Actors:
+        actorset.add_node({"name": actor})
+        acts_in_movie.add_relationship(
+            {"name": actor}, {"title": title}, {})
+
+    # Send actorset to neobulkmp manager
+    yield actorset
+    # Send actor to movie relation to neobulkmp manager
+    yield acts_in_movie
+
+
+# Mount parsing function on parsing workers
+WorkerSourcing.sourcing_func = parsing_func
+
+# Init manager, provide parsing worker and worker parameters
+manager = Manager(worker_sourcing_class=WorkerSourcing,
+                  worker_parameters=movies)
+#manager.max_concurrently_worker_count = 1
+manager.create_indexes = True
+# manager.create_unique_constraints = True  # not yet implemented
+manager.merge(graph_params={"host": "localhost"})
