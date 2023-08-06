ade12a245266a73b633444db2a7e01f92eaa4118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:48 +0000

    Add package chinna-distributions/0.1/chinna-distributions-0.1.tar.gz

---
+++ packages/chinna-distributions/chinna-distributions-0.1.tar.gz/chinna-distributions-0.1/README.md
@@ -0,0 +1,10 @@
+# Distribution Package
+Package for calculating distributions, including Gaussian and Binomial distributions
+
+## Installation
+Install using pip
+```pip install chinna-probability```
+
+## Requirements
+* Python 3
+* matplotlib
---
+++ packages/chinna-distributions/chinna-distributions-0.1.tar.gz/chinna-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chinna-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['chinna-distributions'],
+      author = 'Chinnaporn Chinotaikul',
+      author_email = 'chinnaporn.chinotaikul@gmail.com',
+      zip_safe=False)
