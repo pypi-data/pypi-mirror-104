ea3d98f7ababaa2cb920ada6627ffb9f5a961375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:10 +0000

    Add package rescape-region/0.1.89/rescape-region-0.1.89.tar.gz

---
+++ packages/rescape-region/rescape-region-0.1.89.tar.gz/rescape-region-0.1.89/rescape_region/models/search_location.py
@@ -0,0 +1,38 @@
+import reversion
+from django.db.models import JSONField
+from safedelete.models import SafeDeleteModel
+
+from rescape_region.models.revision_mixin import RevisionModelMixin
+
+
+def default_search_identification():
+    return dict()
+
+def default_search_street():
+    return dict()
+
+def data_default():
+    return dict()
+
+def geojson_default():
+    return dict()
+
+@reversion.register()
+class SearchLocation(SafeDeleteModel, RevisionModelMixin):
+    """
+        Models a location search that can match zero to many locations
+    """
+
+    # Search for matches with the id and key (location.id, location.key)
+    identification = JSONField(null=True, default=default_search_identification)
+
+    # Search for matches with the street name (location.name)
+    street = JSONField(null=True, default=default_search_street)
+
+    # Search for matches with True location.geojson fields
+    geojson = JSONField(null=True, default=geojson_default)
+    # Search for matches with the location.data fields
+    data = JSONField(null=True, default=data_default)
+
+    class Meta:
+        app_label = "rescape_region"
