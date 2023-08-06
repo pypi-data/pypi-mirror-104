fa22b38dd9d891e99fd2efc3907537cd8ffc8904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:16 +0000

    Add package stockx-wrapper-py/1.3.1/stockx_wrapper_py-1.3.1-py3-none-any.whl

---
+++ packages/stockx-wrapper-py/stockx_wrapper_py-1.3.1-py3-none-any.whl/stockx_wrapper/settings.py
@@ -0,0 +1,30 @@
+# Urls
+API_URL = 'https://stockx.com/api'
+ALGOLIA_URL = (
+    "https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query?x-algolia-agent=Algolia for JavaScript (4.8.6); "
+    "Browser "
+)
+
+# End points
+GET_PRODUCT = 'products'
+SEARCH_PRODUCTS = 'browse'
+CHART_DATA = 'chart'
+SOLD_DATA = 'activity'
+
+# Algolia headers
+ALGOLIA_HEADERS = {
+    'x-algolia-api-key': 'ZTEwZjZhMzE3NWRiNDRkMTBkYWQ1NjRkMDA2MmI3NGNlYjZkNTNhM2RjZDRmZTI0YmEyZjEzM2Y2MGE4NzAxYnZhbGlkVW50aWw9MTYxNzY1NDEwOA==',
+    'x-algolia-application-id': 'XW7SBCT9V6'
+}
+
+# Limits
+SOLD_DATA_LIMIT = 250
+
+SEARCH_PRODUCTS_OLD_API_PRODUCTS_LIMIT = 50
+SEARCH_PRODUCTS_OLD_API_PAGES_LIMIT = 20
+SEARCH_PRODUCTS_OLD_API_HITS_LIMIT = SEARCH_PRODUCTS_OLD_API_PRODUCTS_LIMIT*SEARCH_PRODUCTS_OLD_API_PAGES_LIMIT
+
+SEARCH_PRODUCTS_NEW_API_HITS_LIMIT = 1000
+
+# States
+SOLD_STATE = 480
---
+++ packages/stockx-wrapper-py/stockx_wrapper_py-1.3.1-py3-none-any.whl/stockx_wrapper/stockx.py
@@ -0,0 +1,8 @@
+from stockx_wrapper.products import Products
+
+
+class Stockx:
+
+    def __init__(self):
+
+        self.products = Products()
