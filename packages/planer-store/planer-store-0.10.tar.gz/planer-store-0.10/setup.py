202780c1053916597c7b2abee1499440eb83ca99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:36 +0000

    Add package planer-store/0.10/planer-store-0.10.tar.gz

---
+++ packages/planer-store/planer-store-0.10.tar.gz/planer-store-0.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+descr = """Some useful models based on planer"""
+
+if __name__ == '__main__':
+    setup(name='planer-store',
+        version='0.10',
+        url='https://github.com/Image-Py/planer-store',
+        description='toolbox of planer',
+        long_description=descr,
+        author='Y.Dong, YXDragon',
+        author_email='yxdragon@imagepy.org',
+        license='BSD 3-clause',
+        packages=find_packages(),
+        package_data={},
+        install_requires=[
+            'planer'
+        ],
+    )
