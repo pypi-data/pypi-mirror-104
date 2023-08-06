4afc474867ee51c03105cd2c1124b3993ce644bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:22 +0000

    Add package signal2html/0.2.0/signal2html-0.2.0-py3-none-any.whl

---
+++ packages/signal2html/signal2html-0.2.0-py3-none-any.whl/signal2html/exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Various exceptions
+
+Author: G.J.J. van den Burg
+License: See LICENSE file.
+Copyright: 2020, G.J.J. van den Burg
+
+"""
+
+
+class DatabaseNotFound(Exception):
+    pass
+
+
+class DatabaseVersionNotFound(Exception):
+    pass
