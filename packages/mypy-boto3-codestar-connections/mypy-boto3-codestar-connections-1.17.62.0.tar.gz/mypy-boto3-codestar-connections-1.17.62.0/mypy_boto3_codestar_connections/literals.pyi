3a2c1f524290199661224f6702409d2e64b65781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:16 +0000

    Add package mypy-boto3-codestar-connections/1.17.62.0/mypy-boto3-codestar-connections-1.17.62.0.tar.gz

---
+++ packages/mypy-boto3-codestar-connections/mypy-boto3-codestar-connections-1.17.62.0.tar.gz/mypy-boto3-codestar-connections-1.17.62.0/mypy_boto3_codestar_connections/literals.pyi
@@ -0,0 +1,22 @@
+"""
+Main interface for codestar-connections service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_codestar_connections.literals import ConnectionStatus
+
+    data: ConnectionStatus = "AVAILABLE"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("ConnectionStatus", "ProviderType")
+
+ConnectionStatus = Literal["AVAILABLE", "ERROR", "PENDING"]
+ProviderType = Literal["Bitbucket", "GitHub", "GitHubEnterpriseServer"]
