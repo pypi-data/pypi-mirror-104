2c556e8d6c52973b46ff2561f731473abdc63600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:10 +0000

    Add package micro5125a/0.1.1/micro5125a-0.1.1-py3-none-any.whl

---
+++ packages/micro5125a/micro5125a-0.1.1-py3-none-any.whl/micro5125a/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+""" micro5125a/version.py """
+__version__ = "0.1.1"
+
+# 0.1.1 (03/05/2021): Correct parsing problem of adev outpouts when length of
+#                     adev value and adev noise floor is different.
+#                     Also correct display problem of argument parser.
+# 0.1.0 (16/10/2019): First version.
