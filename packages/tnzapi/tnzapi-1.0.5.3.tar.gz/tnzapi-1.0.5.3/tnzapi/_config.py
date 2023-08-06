0c65def51acc7081937fc1b8facdd0bf8dd37939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:44 +0000

    Add package tnzapi/1.0.5.3/tnzapi-1.0.5.3.tar.gz

---
+++ packages/tnzapi/tnzapi-1.0.5.3.tar.gz/tnzapi-1.0.5.3/tnzapi/_config.py
@@ -0,0 +1,9 @@
+__Sender__      = ""
+__APIKey__      = ""
+__APIVersion__  = "1.05"
+__APIURL__      = "https://www.tnz.co.nz/api/json"
+#__APIURL__      = "http://192.168.1.79/api/json"
+__APIHeaders__  = {
+    "Content-Type": "application/json",
+    "encoding": "utf-8"
+}
