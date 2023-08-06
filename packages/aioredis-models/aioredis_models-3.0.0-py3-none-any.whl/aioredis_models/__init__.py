ee0d0d2914c24c35aa5c24cff3bd18ff0fbc46ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:32 +0000

    Add package aioredis-models/3.0.0/aioredis_models-3.0.0-py3-none-any.whl

---
+++ packages/aioredis-models/aioredis_models-3.0.0-py3-none-any.whl/aioredis_models/__init__.py
@@ -0,0 +1,20 @@
+"""
+This module contains the following classes:
+- RedisClient
+- RedisModel
+- RedisKey
+- RedisList
+- RedisHash
+- RedisSet
+- RedisString
+- RedisDoubleHash
+"""
+
+from .redis_client import RedisClient
+from .redis_model import RedisModel
+from .redis_key import RedisKey
+from .redis_list import RedisList
+from .redis_hash import RedisHash
+from .redis_set import RedisSet
+from .redis_string import RedisString
+from .redis_double_hash import RedisDoubleHash
