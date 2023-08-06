e55437e117d64160f96717f06e68683c6993a0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:30 +0000

    Add package dask-image/0.6.0/dask-image-0.6.0.tar.gz

---
+++ packages/dask-image/dask-image-0.6.0.tar.gz/dask-image-0.6.0/dask_image/ndmorph/__init__.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """John Kirkham"""
+__email__ = "kirkhamj@janelia.hhmi.org"
+
+
+import scipy.ndimage
+
+from . import _utils
+from . import _ops
+from ..dispatch._dispatch_ndmorph import (
+    dispatch_binary_dilation,
+    dispatch_binary_erosion)
+
+__all__ = [
+    "binary_closing",
+    "binary_dilation",
+    "binary_erosion",
+    "binary_opening",
+]
+
+
+@_utils._update_wrapper(scipy.ndimage.binary_closing)
+def binary_closing(image,
+                   structure=None,
+                   iterations=1,
+                   origin=0):
+    image = (image != 0)
+
+    structure = _utils._get_structure(image, structure)
+    iterations = _utils._get_iterations(iterations)
+    origin = _utils._get_origin(structure.shape, origin)
+
+    result = image
+    result = binary_dilation(
+        result, structure=structure, iterations=iterations, origin=origin
+    )
+    result = binary_erosion(
+        result, structure=structure, iterations=iterations, origin=origin
+    )
+
+    return result
+
+
+@_utils._update_wrapper(scipy.ndimage.binary_dilation)
+def binary_dilation(image,
+                    structure=None,
+                    iterations=1,
+                    mask=None,
+                    border_value=0,
+                    origin=0,
+                    brute_force=False):
+    border_value = _utils._get_border_value(border_value)
+
+    result = _ops._binary_op(
+        dispatch_binary_dilation(image),
+        image,
+        structure=structure,
+        iterations=iterations,
+        mask=mask,
+        origin=origin,
+        brute_force=brute_force,
+        border_value=border_value
+    )
+
+    return result
+
+
+@_utils._update_wrapper(scipy.ndimage.binary_erosion)
+def binary_erosion(image,
+                   structure=None,
+                   iterations=1,
+                   mask=None,
+                   border_value=0,
+                   origin=0,
+                   brute_force=False):
+    border_value = _utils._get_border_value(border_value)
+
+    result = _ops._binary_op(
+        dispatch_binary_erosion(image),
+        image,
+        structure=structure,
+        iterations=iterations,
+        mask=mask,
+        origin=origin,
+        brute_force=brute_force,
+        border_value=border_value
+    )
+
+    return result
+
+
+@_utils._update_wrapper(scipy.ndimage.binary_opening)
+def binary_opening(image,
+                   structure=None,
+                   iterations=1,
+                   origin=0):
+    image = (image != 0)
+
+    structure = _utils._get_structure(image, structure)
+    iterations = _utils._get_iterations(iterations)
+    origin = _utils._get_origin(structure.shape, origin)
+
+    result = image
+    result = binary_erosion(
+        result, structure=structure, iterations=iterations, origin=origin
+    )
+    result = binary_dilation(
+        result, structure=structure, iterations=iterations, origin=origin
+    )
+
+    return result
