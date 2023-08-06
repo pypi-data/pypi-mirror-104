46f8a526aa5501b227d522136fd2755affae12be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package invincible/0.0.1/invincible-0.0.1.tar.gz

---
+++ packages/invincible/invincible-0.0.1.tar.gz/invincible-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['invincible']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['loguru>=0.5.3,<0.6.0']
+
+entry_points = \
+{'console_scripts': ['invincible = invincible:main']}
+
+setup_kwargs = {
+    'name': 'invincible',
+    'version': '0.0.1',
+    'description': 'Keep restarting process for ever',
+    'long_description': '',
+    'author': 'Hernan Ezequiel Di Giorgi',
+    'author_email': 'hernan.digiorgi@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
