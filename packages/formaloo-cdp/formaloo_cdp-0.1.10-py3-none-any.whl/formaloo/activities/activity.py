e70275fb233a1ba6a5cb93c18a55d81cd1ed0fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:04 +0000

    Add package formaloo-cdp/0.1.10/formaloo_cdp-0.1.10-py3-none-any.whl

---
+++ packages/formaloo-cdp/formaloo_cdp-0.1.10-py3-none-any.whl/formaloo/activities/activity.py
@@ -0,0 +1,42 @@
+from formaloo import constants, client
+from formaloo.tags import Tag
+
+
+class Activity:
+
+    def __init__(self, action, customer_data, activity_data=None, activity_date=None, tags=None):
+        if not activity_data:
+            activity_data = {}
+
+        if not tags:
+            tags = []
+
+        self.client = client.Client()
+        self.action = action
+        self.customer_data = customer_data
+        self.activity_data = activity_data
+        self.activity_date = activity_date
+        self.tags = tags
+
+    def create(self):
+        body = self.get_body()
+
+        response = self.client.post(
+            constants.V_1_0_CREATE_ACTIVITY_ENDPOINT,
+            body=body
+        )
+
+        return response.json()
+
+    def get_body(self):
+        tags_body = Tag.get_list_body(self.tags)
+
+        body = {
+            'action': self.action,
+            'customer': self.customer_data,
+            'activity_data': self.activity_data,
+            'activity_date': self.activity_date,
+            'tags': tags_body
+        }
+
+        return body
---
+++ packages/formaloo-cdp/formaloo_cdp-0.1.10-py3-none-any.whl/formaloo/tags.py
@@ -0,0 +1,43 @@
+from formaloo import constants, client
+
+
+class Tag:
+    def __init__(self, title, description=None, slug=None):
+        self.client = client.Client()
+        self.title = title
+        self.description = description
+        self.slug = slug
+
+    def create(self):
+        body = self.get_body()
+
+        response = self.client.post(
+            constants.V_1_0_CREATE_TAGS_ENDPOINT,
+            body=body
+        )
+
+        return response.json()
+
+    def get_body(self):
+        body = {
+            'title': self.title
+        }
+
+        if self.description:
+            body['description'] = self.description
+
+        if self.slug:
+            body['slug'] = self.slug
+
+        return body
+
+    @staticmethod
+    def get_list_body(tags):
+        body = []
+
+        for tag in tags:
+            body.append(
+                tag.get_body()
+            )
+
+        return body
