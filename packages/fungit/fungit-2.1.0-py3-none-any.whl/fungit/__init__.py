1eccdd9c7546b7d20402cfa7c7bc623117d5e489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:11 +0000

    Add package fungit/2.1.0/fungit-2.1.0-py3-none-any.whl

---
+++ packages/fungit/fungit-2.1.0-py3-none-any.whl/fungit/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+__license__ = "MIT"
+__version__ = "2.1.0"
+__author__ = "Zachary Zhang"
+__email__ = "zlj19971222@outlook.com"
+__git_url__ = "https://github.com/zlj-zz/fungit.git"
+__HOME__ = os.environ["HOME"]
+__FUNGITDIR__ = __HOME__ + "/.config/fungit"
