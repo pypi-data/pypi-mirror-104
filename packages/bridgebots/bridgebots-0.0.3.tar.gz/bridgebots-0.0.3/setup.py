dc7fbff7040402f7693deec0949bdb28f1a04c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:50 +0000

    Add package bridgebots/0.0.3/bridgebots-0.0.3.tar.gz

---
+++ packages/bridgebots/bridgebots-0.0.3.tar.gz/bridgebots-0.0.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bridgebots']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'bridgebots',
+    'version': '0.0.3',
+    'description': 'Data processing for Contract Bridge',
+    'long_description': None,
+    'author': 'Forrest Rice',
+    'author_email': 'forrest.d.rice@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
