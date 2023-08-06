81c15172113892a5e359e26588b43d0c6b2a9305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:07 +0000

    Add package distributions-udacity-dsnd-chp03lesson04-5-exercise-upload-to-pypi/0.1/distributions_udacity_dsnd_chp03lesson04_5_exercise_upload_to_pypi-0.1.tar.gz

---
+++ packages/distributions-udacity-dsnd-chp03lesson04-5-exercise-upload-to-pypi/distributions_udacity_dsnd_chp03lesson04_5_exercise_upload_to_pypi-0.1.tar.gz/distributions_udacity_dsnd_chp03lesson04_5_exercise_upload_to_pypi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udacity_dsnd_chp03lesson04_5_exercise_upload_to_pypi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udacity_dsnd_chp03lesson04_5_exercise_upload_to_pypi'],
+      zip_safe=False)
