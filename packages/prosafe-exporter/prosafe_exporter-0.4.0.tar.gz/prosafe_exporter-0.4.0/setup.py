8ca0fe90256c59d24ebbe53405d70d24c1524560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:51 +0000

    Add package prosafe-exporter/0.4.0/prosafe_exporter-0.4.0.tar.gz

---
+++ packages/prosafe-exporter/prosafe_exporter-0.4.0.tar.gz/prosafe_exporter-0.4.0/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+INSTALL_REQUIRED = (HERE / "requirements.txt").read_text()
+SETUP_REQUIRED = (HERE / "setup_requirements.txt").read_text()
+TEST_REQUIRED = (HERE / "test_requirements.txt").read_text()
+
+setup(
+    name='prosafe_exporter',
+    packages=['prosafe_exporter'],
+    version='0.4.0',
+    description='Prometheus metrics exporter for NETGEAR switches of the Smart Managed Plus series.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Till Steinbach',
+    keywords='prometheus, netgear, metrics-exporter, prosafe, prosafe-exporter',
+    url='https://github.com/tillsteinbach/prosafe_exporter_python',
+    license='MIT',
+    install_requires=INSTALL_REQUIRED,
+    entry_points={
+        'console_scripts': [
+            'prosafe_exporter = prosafe_exporter.prosafe_exporter:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System :: Networking :: Monitoring ',
+      ],
+    python_requires='>=3.6',
+    setup_requires=SETUP_REQUIRED,
+    tests_require=TEST_REQUIRED,
+)
