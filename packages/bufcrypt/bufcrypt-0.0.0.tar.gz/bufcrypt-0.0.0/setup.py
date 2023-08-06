e30aa040a7eaba2e923ba15f220dcdcc74079365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:17 +0000

    Add package bufcrypt/0.0.0/bufcrypt-0.0.0.tar.gz

---
+++ packages/bufcrypt/bufcrypt-0.0.0.tar.gz/bufcrypt-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='bufcrypt',
+    packages=["bufcrypt"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com'
+)
