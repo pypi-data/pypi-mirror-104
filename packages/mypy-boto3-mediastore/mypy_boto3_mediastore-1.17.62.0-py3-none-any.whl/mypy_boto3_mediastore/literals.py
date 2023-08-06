5f2d3e6148657293aaa22e1209890ff01a2718be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:22 +0000

    Add package mypy-boto3-mediastore/1.17.62.0/mypy_boto3_mediastore-1.17.62.0-py3-none-any.whl

---
+++ packages/mypy-boto3-mediastore/mypy_boto3_mediastore-1.17.62.0-py3-none-any.whl/mypy_boto3_mediastore/literals.py
@@ -0,0 +1,26 @@
+"""
+Main interface for mediastore service literal definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_mediastore.literals import ContainerLevelMetrics
+
+    data: ContainerLevelMetrics = "DISABLED"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("ContainerLevelMetrics", "ContainerStatus", "ListContainersPaginatorName", "MethodName")
+
+
+ContainerLevelMetrics = Literal["DISABLED", "ENABLED"]
+ContainerStatus = Literal["ACTIVE", "CREATING", "DELETING"]
+ListContainersPaginatorName = Literal["list_containers"]
+MethodName = Literal["DELETE", "GET", "HEAD", "PUT"]
