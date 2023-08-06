5bb6f9d57d87169f8bd9c519ff1aa63b9a78a2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:06 +0000

    Add package saumya/0.0.2/saumya-0.0.2.tar.gz

---
+++ packages/saumya/saumya-0.0.2.tar.gz/saumya-0.0.2/script/__init__.py
@@ -0,0 +1,8 @@
+import pathlib
+def log(msg):
+    if pathlib.Path("action_logs.txt").exists:
+        with open("action_logs.txt", "a") as file:
+            file.write("\n"+msg)
+    else:
+        with open("action_logs.txt", "w") as file:
+            file.write(msg)
