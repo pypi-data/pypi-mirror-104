c0021bea5686131b55d999fe556f4a41fdd8032b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:38 +0000

    Add package mengheng-probability/0.1/mengheng_probability-0.1.tar.gz

---
+++ packages/mengheng-probability/mengheng_probability-0.1.tar.gz/mengheng_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mengheng_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mengheng_probability'],
+      author='Mengheng Xue',
+      author_email='mx586@nyu.edu',
+      zip_safe=False)
