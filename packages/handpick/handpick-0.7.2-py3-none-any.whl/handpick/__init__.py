c6db26e26218b70852a758e75aaa10dc00879afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:04 +0000

    Add package handpick/0.7.2/handpick-0.7.2-py3-none-any.whl

---
+++ packages/handpick/handpick-0.7.2-py3-none-any.whl/handpick/__init__.py
@@ -0,0 +1,23 @@
+"""
+Handpick is a tool to traverse nested data structures recursively and
+find all objects that meet certain criteria.
+"""
+
+from .core import (
+    pick,
+    predicate,
+    ALL,
+    NO_CONTAINERS,
+    NO_LIST_DICT,
+    is_type,
+    not_type,
+    flat,
+    max_depth,
+)
+
+__version__ = '0.7.2'
+
+__all__ = [
+    '__version__', 'pick', 'predicate', 'ALL', 'NO_CONTAINERS', 'NO_LIST_DICT',
+    'is_type', 'not_type', 'flat', 'max_depth'
+]
