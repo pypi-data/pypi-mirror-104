3d5f9f079c61436757bcc8e3c3dd41435764997d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:42 +0000

    Add package sid-kemendesa/0.1.1/sid_kemendesa-0.1.1-py3-none-any.whl

---
+++ packages/sid-kemendesa/sid_kemendesa-0.1.1-py3-none-any.whl/sid_kemendesa/__init__.py
@@ -0,0 +1,13 @@
+from .search import ListResponse, HasilDesa, listKab, listKec, listDesa, search
+
+__version__ = "0.1.1"
+
+
+__all__ = [
+    "ListResponse",
+    "HasilDesa",
+    "listKab",
+    "listKec",
+    "listDesa",
+    "search",
+]
