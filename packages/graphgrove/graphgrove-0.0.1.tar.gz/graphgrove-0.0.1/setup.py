8adb342cc90701dc3f01c21e46f28f868a6ef541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:23 +0000

    Add package graphgrove/0.0.1/graphgrove-0.0.1.tar.gz

---
+++ packages/graphgrove/graphgrove-0.0.1.tar.gz/graphgrove-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='graphgrove',
+      version='0.0.1',
+      url='https://github.com/nmonath/graphgrove',
+      author='Nicholas Monath',
+      author_email='nmonath@cs.umass.edu',
+      packages=['graphgrove'],
+      package_dir={'graphgrove': 'graphgrove'}
+      )
