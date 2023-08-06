7718fcb1543ddd16713065566598816489caa35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:27 +0000

    Add package az-account-switcher/1.1.0/az-account-switcher-1.1.0.tar.gz

---
+++ packages/az-account-switcher/az-account-switcher-1.1.0.tar.gz/az-account-switcher-1.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from os import path
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='az-account-switcher',
+      version='1.1.0',
+      description='Utility to switch Azure subscriptions',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Alexander Bij',
+      author_email='Alexander.Bij@gmail.com',
+      url='https://github.com/abij/az-account-switcher',
+      classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+      ],
+      packages=['az_account_switcher'],
+      python_requires='>=3.6',
+      install_requires=[
+          'azure-cli-core',
+          'click'
+      ],
+      entry_points='''
+          [console_scripts]
+          az-switch=az_account_switcher:main
+          az-account-switch=az_account_switcher:main
+          az-account-switcher=az_account_switcher:main
+      ''',
+      )
