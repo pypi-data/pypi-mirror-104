4aa36ead699a1464501da1aea6079ccd974af8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:48 +0000

    Add package deeploy/0.1.0/deeploy-0.1.0.tar.gz

---
+++ packages/deeploy/deeploy-0.1.0.tar.gz/deeploy-0.1.0/deeploy/models/__init__.py
@@ -0,0 +1,12 @@
+from .deployment import Deployment
+from .repository import Repository
+
+from .client_options import ClientConfig
+
+from .create_deployment import CreateDeployment
+
+from .workspace import Workspace
+
+from .deploy_options import DeployOptions
+
+from .prediction import V1Prediction, V2Prediction
