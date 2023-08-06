d8ee6375be9c45c70d39dcc574b399128f3de4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:15 +0000

    Add package hum/0.0.11/hum-0.0.11.tar.gz

---
+++ packages/hum/hum-0.0.11.tar.gz/hum-0.0.11/hum/gen/__init__.py
@@ -0,0 +1,19 @@
+from contextlib import suppress
+
+with suppress(ModuleNotFoundError):
+    from hum.gen.diagnosis_sounds import (
+        WfGen, TimeSound, BinarySound, slow_mask, wf_with_timed_bleeps, mk_sounds_with_timed_bleeps
+    )
+
+with suppress(ModuleNotFoundError):
+    from hum.gen.sine_mix import mk_sine_wf, freq_based_stationary_wf
+
+with suppress(ModuleNotFoundError):
+    from hum.gen.voiced_time import Voicer, tell_time_continuously
+
+with suppress(ModuleNotFoundError):
+    from hum.gen.signal_generators import (
+        normal_dist, gen_words, categorical_gen, alphabet_to_bins,
+        call_repeatedly, bernoulli, bernoulli_gen, inlier_outlier, signal, create_session, string_to_num,
+        session_to_df
+    )
