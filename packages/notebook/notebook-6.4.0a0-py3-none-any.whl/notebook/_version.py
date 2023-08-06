408b7f16aa541cc6ac5fcf94db742149c017e1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:04 +0000

    Add package notebook/6.4.0a0/notebook-6.4.0a0-py3-none-any.whl

---
+++ packages/notebook/notebook-6.4.0a0-py3-none-any.whl/notebook/_version.py
@@ -0,0 +1,17 @@
+"""
+store the current version info of the notebook.
+
+"""
+# Version string must appear intact for tbump versioning
+__version__ = '6.4.0a0'
+
+try:
+    # When installing from a tarball, this import will not yet be available
+    from jupyter_packaging import get_version_info
+    version_info = get_version_info(__version__)
+except ImportError:
+    version_info = ()
+
+# Downstream maintainer, when running `python.setup.py jsversion`,
+# the version string is propagated to the JavaScript files,  do not forget to
+# patch the JavaScript files in `.postN` release done by distributions.
