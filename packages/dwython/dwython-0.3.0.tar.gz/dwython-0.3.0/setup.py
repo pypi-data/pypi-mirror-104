0b43546b53e173ab2336f2d526eaee0e8b0ed1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:56 +0000

    Add package dwython/0.3.0/dwython-0.3.0.tar.gz

---
+++ packages/dwython/dwython-0.3.0.tar.gz/dwython-0.3.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dwython',
+    version='0.3.0',
+    description='Datawave Python API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/phrocker/dwython',
+    author='Marc Parisi',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+    keywords='dwython',
+
+    packages=find_packages(),
+
+    install_requires=['requests', 'certifi', 'chardet', 'idna', 'urllib3'],
+)
