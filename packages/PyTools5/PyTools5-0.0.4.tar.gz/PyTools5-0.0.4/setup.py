2980fd0b3a59755f8b680de898a11806f5087a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:32 +0000

    Add package PyTools5/0.0.4/PyTools5-0.0.4.tar.gz

---
+++ packages/PyTools5/PyTools5-0.0.4.tar.gz/PyTools5-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='PyTools5',
+  version='0.0.4',
+  description='A very basic way to do complex stuff in python.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Luke Dixon',
+  author_email='lukeodixon@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='system', 
+  packages=find_packages(),
+  install_requires=['Pillow', 'PyQt5'] 
+)
