c32444c6023a29721aa675048350370a860e19f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:10 +0000

    Add package tkinterie/1.0.0/tkinterie-1.0.0-py2.py3-none-any.whl

---
+++ packages/tkinterie/tkinterie-1.0.0-py2.py3-none-any.whl/tkinterie/tkinterIE.py
@@ -0,0 +1,41 @@
+#基于InternetExplorer.Application的tkinter webview组件
+#事实上，该com组件依赖于系统的浏览器内核
+#所以，以Windows10为例，内核是edge而非ie
+from tkinter import Tk,Frame
+from comtypes.client import CreateObject,GetEvents,PumpEvents
+from ctypes import windll
+SetParent=windll.user32.SetParent
+MoveWindow=windll.user32.MoveWindow
+GetWindowLong=windll.user32.GetWindowLongA
+SetWindowLong=windll.user32.SetWindowLongA
+
+
+class WebView(Frame):
+
+    def __init__(self,parent,width:int,height:int,url='',**kw):
+        Frame.__init__(self,parent,width=width,height=height,**kw)
+        ie=CreateObject('InternetExplorer.Application')
+        ie.AddressBar=False
+        ie_id=ie.HWND
+        frame_id=self.winfo_id()
+        style=GetWindowLong(ie_id,-16)
+        SetWindowLong(ie_id,-16,style&~12582912&~262144)
+        SetParent(ie_id,frame_id)
+        MoveWindow(ie_id,0,0,width,height,True)
+        ie.Visible=True
+        if url!='':
+            ie.Navigate2(url)
+        self.ie=ie
+
+    def navigate(self,url:str):
+        self.ie.Navigate2(url)
+
+
+if __name__=='__main__':
+    a=Tk()
+    a.geometry('1200x600')
+
+    w=WebView(a,600,500,'www.baidu.com')
+    w.place(x=10,y=20)
+
+    a.mainloop()
