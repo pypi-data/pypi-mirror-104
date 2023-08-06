dc90b06b57e897d1444cdd5678e882e88f03c0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:17 +0000

    Add package hydbedscraper/0.1.0/hydbedscraper-0.1.0-py3-none-any.whl

---
+++ packages/hydbedscraper/hydbedscraper-0.1.0-py3-none-any.whl/hydbedscraper/drivers/hyderabad.py
@@ -0,0 +1,20 @@
+from hydbedscraper.parsers.hyderabad import (
+    parse_government_hospitals,
+    parse_private_hospitals,
+)
+from hydbedscraper.requesters.hyderabad import (
+    get_government_hospitals_page_soup,
+    get_private_hospitals_page_soup,
+)
+from hydbedscraper.types import t_DataFrameDict
+from hydbedscraper.writers.hyderabad import to_dataframe_dict
+
+
+def work() -> t_DataFrameDict:
+    govt_soup = get_government_hospitals_page_soup()
+    private_soup = get_private_hospitals_page_soup()
+
+    govt_info = parse_government_hospitals(govt_soup)
+    private_info = parse_private_hospitals(private_soup)
+
+    return to_dataframe_dict(govt_info, private_info)
