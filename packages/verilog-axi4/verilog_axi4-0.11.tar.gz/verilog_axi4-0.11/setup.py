45e9b5f4890eb1a76f06a11b1d7de580ffa785b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:20 +0000

    Add package verilog-axi4/0.11/verilog_axi4-0.11.tar.gz

---
+++ packages/verilog-axi4/verilog_axi4-0.11.tar.gz/verilog_axi4-0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='verilog_axi4',
+    version='v0.11',
+    description='A  module for generating verilog axi4 code',
+    py_modules=['verilog_axi4'],
+    author='likmi',
+    author_email='likmi@qq.com',
+    license='MIT'
+)
