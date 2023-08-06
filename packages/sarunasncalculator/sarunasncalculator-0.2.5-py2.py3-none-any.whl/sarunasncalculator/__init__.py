b16c693b8aff5aaa85318581766c11b92c3ced90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:28 +0000

    Add package sarunasncalculator/0.2.5/sarunasncalculator-0.2.5-py2.py3-none-any.whl

---
+++ packages/sarunasncalculator/sarunasncalculator-0.2.5-py2.py3-none-any.whl/sarunasncalculator/__init__.py
@@ -0,0 +1,14 @@
+"""1st sprint project Turing College Calculator"""
+__version__ = "0.2.5"
+
+from sarunasncalculator.calculator import Calculator
+
+callc = Calculator()
+callc.add(2)
+callc.add(7)
+
+#we can print current running memory , function returns float
+print(callc.get_current_result())
+
+#we can print action sequence , function returns str
+print(callc.get_action_seq())
