8b5b4c802baa28473a7c9edea508af618a6a69f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:30 +0000

    Add package npttf2utf/0.3.6/npttf2utf-0.3.6-py3-none-any.whl

---
+++ packages/npttf2utf/npttf2utf-0.3.6-py3-none-any.whl/npttf2utf/base/txthandler.py
@@ -0,0 +1,29 @@
+from .fontmapper import FontMapper
+from .exceptions import TxtAutoModeException, UnsupportedMapToException
+
+class TxtHandler:
+    
+    def __init__(self, rules_file):
+        self.mapper = FontMapper(rules_file)
+        self.supported_ttf_fonts = self.mapper.supported_maps
+    
+    def detect_used_fonts(self, docx_file_path):
+        return []
+
+    def map_fonts(self, orginal_file_path, output_file_path="mapped.txt", from_font="Preeti", to_font="unicode", components=[], known_unicode_fonts=[]):
+        if from_font != "auto":
+            with open(orginal_file_path, "r") as orginal_file:
+                lines_orginal = orginal_file.readlines()
+            output_file =  open(output_file_path, "a")
+            for line in lines_orginal:
+                if to_font.lower() == "unicode":
+                    output_file.write(self.mapper.map_to_unicode(line, from_font, False))
+                elif to_font.lower() == "preeti":
+                    output_file.write(self.mapper.map_to_preeti(line, from_font, False))
+                else:
+                    raise UnsupportedMapToException
+            output_file.close()
+        else:
+            raise TxtAutoModeException
+        return True
+
