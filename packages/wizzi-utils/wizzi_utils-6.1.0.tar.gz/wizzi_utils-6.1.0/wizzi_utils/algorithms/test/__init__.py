7485062fcb7809331941ffb8579315a52587f5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:48 +0000

    Add package wizzi-utils/6.1.0/wizzi_utils-6.1.0.tar.gz

---
+++ packages/wizzi-utils/wizzi_utils-6.1.0.tar.gz/wizzi_utils-6.1.0/wizzi_utils/algorithms/test/__init__.py
@@ -0,0 +1,7 @@
+"""
+:requires: matplotlib
+"""
+try:
+    from wizzi_utils.algorithms.test.test_algorithms import *
+except ModuleNotFoundError as e:
+    pass
---
+++ packages/wizzi-utils/wizzi_utils-6.1.0.tar.gz/wizzi_utils-6.1.0/wizzi_utils/algorithms/test/test_algorithms.py
@@ -0,0 +1,34 @@
+from wizzi_utils.algorithms import algorithms as alg
+from wizzi_utils.misc import misc_tools as mt
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def find_centers_test():
+    """
+    if you get No module named 'sklearn.__check_build._check_build'
+    pip uninstall scikit-learn
+    pip uninstall sklearn
+    pip install sklearn
+    :return:
+    """
+    mt.get_function_name(ack=True, tabs=0)
+    A = np.array([[-1, -1], [-1, 1], [1, 1], [1, -1]])
+    centers = alg.find_centers(A, k=1)
+    if centers is not None:
+        X, y = mt.de_augment_numpy(A)
+        X_c, y_c = mt.de_augment_numpy(centers)
+        plt.title('1 mean(of a square) example')
+        plt.scatter(X, y, color='g', marker='.', label='A(the points)')
+        plt.scatter(X_c, y_c, color='r', marker='o', label='one center({})'.format(centers[0]))
+        plt.legend(loc='upper center', ncol=1, fancybox=True, framealpha=0.5, edgecolor='black')
+        print('\tVisual test: square and it\'s center')
+        plt.show()
+    return
+
+
+def test_all():
+    print('{}{}:'.format('-' * 5, mt.get_base_file_and_function_name()))
+    find_centers_test()
+    print('{}'.format('-' * 20))
+    return
