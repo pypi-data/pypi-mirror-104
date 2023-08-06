da6d283b0590f00b9dd70ba809eaa8759e0579ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:13 +0000

    Add package django-dirtyfields/1.7.0/django-dirtyfields-1.7.0.tar.gz

---
+++ packages/django-dirtyfields/django-dirtyfields-1.7.0.tar.gz/django-dirtyfields-1.7.0/setup.py
@@ -0,0 +1,7 @@
+# All package metadata is in setup.cfg
+# This is still required because travis builds with "python setup.py"
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
