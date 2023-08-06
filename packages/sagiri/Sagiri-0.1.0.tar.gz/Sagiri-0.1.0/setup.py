f44267367e26bea1ac8a3ec3f0a363de85aaa7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:58 +0000

    Add package sagiri/0.1.0/Sagiri-0.1.0.tar.gz

---
+++ packages/sagiri/Sagiri-0.1.0.tar.gz/Sagiri-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['sagiri']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'sagiri',
+    'version': '0.1.0',
+    'description': 'A simple and powerful asgi web framework.',
+    'long_description': None,
+    'author': 'zzp198',
+    'author_email': 'zzp198@163.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
