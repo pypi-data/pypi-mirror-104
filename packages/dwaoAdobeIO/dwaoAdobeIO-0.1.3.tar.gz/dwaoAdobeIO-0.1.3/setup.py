4bcdd07a4089c6bbd68b279e90b93e6de7d19eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:47 +0000

    Add package dwaoAdobeIO/0.1.3/dwaoAdobeIO-0.1.3.tar.gz

---
+++ packages/dwaoAdobeIO/dwaoAdobeIO-0.1.3.tar.gz/dwaoAdobeIO-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='dwaoAdobeIO',
+    version='0.1.3',
+    author="Ketav Sharma",
+    author_email="ketavsharma@dwao.in",
+    description="Adobe IO Helping Utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=['dwaoAdobeIO'],
+classifiers=[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+            ],
+            install_requires = ["requests","cryptography>=3.4.7","PyJWT>=2.0.1"]
+)
