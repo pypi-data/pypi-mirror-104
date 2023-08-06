adb90b624559d77b1ddc38fc7cb6cad5a3f124cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:18 +0000

    Add package polidoro-cli/3.0.0rc2/polidoro-cli-3.0.0rc2.tar.gz

---
+++ packages/polidoro-cli/polidoro-cli-3.0.0rc2.tar.gz/polidoro-cli-3.0.0rc2/polidoro_cli/__init__.py
@@ -0,0 +1,2 @@
+from polidoro_cli.cli import CLI, CLI_DIR
+from polidoro_cli.main import main, VERSION
---
+++ packages/polidoro-cli/polidoro-cli-3.0.0rc2.tar.gz/polidoro-cli-3.0.0rc2/setup.py
@@ -0,0 +1,41 @@
+"""
+Setup to create the package
+"""
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+import polidoro_cli
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        print("oi")
+
+
+setup(
+    name='polidoro-cli',
+    version='3.0.0-RC2',
+    # version=polidoro_cli.VERSION,
+    description='Generic CLI.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/heitorpolidoro/cli',
+    author='Heitor Polidoro',
+    scripts=['bin/cli'],
+    license='unlicense',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+    install_requires=['polidoro-argument==3.*'],
+    cmdclass={
+        'install': PostInstallCommand
+    }
+)
