e12d38a4fd27fe6fd8e8d896586f94ca5be703df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:23 +0000

    Add package hololive/0.0.9/hololive-0.0.9-py3-none-any.whl

---
+++ packages/hololive/hololive-0.0.9-py3-none-any.whl/hololive/hololive.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from typing import List
+import aiohttp
+
+class Stream:
+  title_jp: str
+  talent_jp: str
+  url: str
+  starttime: datetime
+  
+async def get_streams() -> List[Stream]:
+  streams: list[Stream] = []
+  session = aiohttp.ClientSession()
+  API_schedule = await (await session.get("https://hololive-api.marnixah.com/")).json()
+  for day in API_schedule["schedule"]:
+    date_month = day["date"].split("/")[0]
+    date_day = day["date"].split("/")[1]
+    for stream in day["schedules"]:
+      stream_obj = Stream()
+      stream_obj.url = str(stream["youtube_url"])
+      stream_obj.title_jp = str(stream["title"])
+      stream_obj.talent_jp = str(stream["member"])
+      
+      time_arr = stream["time"].split(":")
+      hour = int(time_arr[0])
+      minute = int(time_arr[1])
+
+      current_time = datetime.utcnow()
+      year = current_time.year
+
+      stream_obj.starttime = datetime(
+        year, int(date_month), int(date_day), hour, minute
+      ) - timedelta(hours=9)  # JST is 9 hours ahead of UTC
+      streams.append(stream_obj)
+  await session.close()
+  return streams
