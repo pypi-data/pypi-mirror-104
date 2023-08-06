a202e08d1f772530edb4596caba6b0ca4138eacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:34 +0000

    Add package amxfirmware/0.0.2/amxfirmware-0.0.2.tar.gz

---
+++ packages/amxfirmware/amxfirmware-0.0.2.tar.gz/amxfirmware-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amxfirmware",
+    version="0.0.2",
+    author="Logan Vaughn",
+    # author_email="logantv@gmail.com",
+    description="amx telnet info, firmware checks, logs, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/logantv/amxfirmware",
+    project_urls={
+        "Bug Tracker": "https://github.com/logantv/amxfirmware/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+		# 'amxtelnet',
+	],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
