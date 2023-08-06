5d2538878b2c261a83f41ea01dfb608169de867f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:31 +0000

    Add package django-task-queue/2.0.1/django_task_queue-2.0.1-py3-none-any.whl

---
+++ packages/django-task-queue/django_task_queue-2.0.1-py3-none-any.whl/django_task_queue/models.py
@@ -0,0 +1,36 @@
+import json
+
+from django.db import models
+from django.db.models import Manager
+
+
+class TaskType:
+    objects: Manager
+
+
+class Task(models.Model, TaskType):
+    created_datetime = models.DateTimeField(db_index=True, auto_now_add=True)
+    last_attempt_datetime = models.DateTimeField(db_index=True, null=True)
+    lock_id = models.CharField(blank=True, max_length=64)
+    queue_class_name = models.CharField(db_index=True, max_length=256)
+    data_json = models.TextField(default='null')
+    last_error = models.TextField(blank=True)
+    tag = models.CharField(db_index=True, max_length=128)
+    error_count = models.IntegerField(default=0)
+    retry_allowed = models.BooleanField(default=True, db_index=True)
+    max_attempts = models.IntegerField()
+    priority = models.IntegerField()
+
+    def __str__(self):
+        return '%s (%s)' % (self.queue_class_name, self.data_json[:50])
+
+    def __repr__(self):
+        return '%s (%s)' % (self.queue_class_name, self.data)
+
+    @property
+    def data(self):
+        return json.loads(self.data_json)
+
+    @data.setter
+    def data(self, data):
+        self.data_json = json.dumps(data, default=str)
