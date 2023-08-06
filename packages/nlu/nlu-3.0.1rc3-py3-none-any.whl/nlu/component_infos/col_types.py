d787eefa4ca7d7b02c991afa54c0eb30d8e4a917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:45 +0000

    Add package nlu/3.0.1rc3/nlu-3.0.1rc3-py3-none-any.whl

---
+++ packages/nlu/nlu-3.0.1rc3-py3-none-any.whl/nlu/component_infos/col_types.py
@@ -0,0 +1,23 @@
+"""
+Mapping of col to tyoes
+
+
+"""
+DOCUMENT = "document"
+TOKEN = "token"
+WORDPIECE = "wordpiece"
+WORD_EMBEDDINGS = "word_embeddings"
+SENTENCE_EMBEDDINGS = "sentence_embeddings"
+CATEGORY = "category" # Multi Class etc
+DATE = "date"
+ENTITY = "entity"
+SENTIMENT = "sentiment"
+POS = "pos"
+CHUNK = "chunk"
+NAMED_ENTITY = "named_entity"
+NEGEX = "negex"
+DEPENDENCY = "dependency"
+LABELED_DEPENDENCY = "labeled_dependency"
+LANGUAGE = "language"
+KEYWORD = "keyword"
+DUMMY = "dummy"
