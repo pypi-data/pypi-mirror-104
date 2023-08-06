e12563e65855c16c66b6d1794144d53b5fdfd62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:13 +0000

    Add package seven_wxapp_zxchj/1.0.13/seven_wxapp_zxchj-1.0.13.tar.gz

---
+++ packages/seven_wxapp_zxchj/seven_wxapp_zxchj-1.0.13.tar.gz/seven_wxapp_zxchj-1.0.13/seven_wxapp_zxchj/models/db_models/user/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: HuangJianYi
+@Date: 2021-04-13 18:38:46
+@LastEditTime: 2021-04-23 11:57:41
+@LastEditors: HuangJianYi
+@Description: 
+"""
+__all__=["user_coupon_model","user_info_model",]
