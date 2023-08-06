dd5057716e4e67de9cded7ca0cbdf80909f20098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:48 +0000

    Add package nanome/0.24.5/nanome-0.24.5.tar.gz

---
+++ packages/nanome/nanome-0.24.5.tar.gz/nanome-0.24.5/nanome/api/shapes/sphere.py
@@ -0,0 +1,16 @@
+from nanome._internal._shapes._sphere import _Sphere
+from nanome.util.enums import ShapeType
+from . import Shape
+
+class Sphere(_Sphere, Shape):
+    def __init__(self):
+        Shape.__init__(self, ShapeType.Sphere)
+        _Sphere.__init__(self)
+
+    @property
+    def radius(self):
+        return self._radius
+    @radius.setter
+    def radius(self, value):
+        self._radius = value
+_Sphere._create = Sphere
