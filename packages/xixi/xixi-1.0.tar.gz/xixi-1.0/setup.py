a64bd1266b4211f50528094a12e36bffec976a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:27 +0000

    Add package xixi/1.0/xixi-1.0.tar.gz

---
+++ packages/xixi/xixi-1.0.tar.gz/xixi-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="xixi",
+    version = "1.0",
+    description = "test print xixi",
+    author = "piePaul",
+    author_email = "gmraphael@163.com",
+    py_modules = ["xiprint.xixi"]
+)
+
