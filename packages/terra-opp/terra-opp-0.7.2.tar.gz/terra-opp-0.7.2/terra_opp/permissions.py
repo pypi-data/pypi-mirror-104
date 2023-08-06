e5ba1d86d953b0951cba6e69140ca90d7dc4bd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:18 +0000

    Add package terra-opp/0.7.2/terra-opp-0.7.2.tar.gz

---
+++ packages/terra-opp/terra-opp-0.7.2.tar.gz/terra-opp-0.7.2/terra_opp/permissions.py
@@ -0,0 +1,90 @@
+from .models import Picture, Campaign
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class ViewpointPermission(BasePermission):
+    """Read only for anonymous users or has terra permission"""
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return not request.user.is_anonymous and request.user.has_terra_perm(
+            "can_manage_viewpoints"
+        )
+
+
+class CampaignPermission(BasePermission):
+    """Read only for authenticated users or has terra permission"""
+
+    def has_permission(self, request, view):
+
+        # Allow all sheets download
+        if "all_sheets" in request.path:
+            return True
+
+        if request.user.is_anonymous:
+            return False
+
+        if request.method in SAFE_METHODS:
+            return request.user.has_terra_perm(
+                "can_add_pictures"
+            ) or request.user.has_terra_perm("can_manage_campaigns")
+
+        return request.user.has_terra_perm("can_manage_campaigns")
+
+    def has_object_permission(self, request, view, obj):
+        """
+        User with permission can_add_pictures can only
+        modify assigned campaings
+        """
+
+        # Allow all sheets download
+        if "all_sheets" in request.path:
+            return True
+
+        if request.method in SAFE_METHODS:
+            if request.user.has_terra_perm("can_manage_campaigns"):
+                return True
+            elif request.user.has_terra_perm("can_add_pictures"):
+                return obj.assignee == request.user and obj.state != Campaign.DRAFT
+
+        return request.user.has_terra_perm("can_manage_campaigns")
+
+
+class PicturePermission(BasePermission):
+    """Read only for anonymous users or has terra permissions"""
+
+    def has_permission(self, request, view):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        if request.user.is_anonymous:
+            return False
+
+        if request.user.has_terra_perm("can_manage_pictures"):
+            return True
+
+        if request.user.has_terra_perm("can_add_pictures"):
+            return True
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        """
+        User with permission can_add_pictures can only
+        modify self picture in state DRAFT or REFUSED
+        """
+        if request.method in SAFE_METHODS:
+            return True
+
+        if request.user.has_terra_perm("can_manage_pictures"):
+            return True
+
+        if request.user.has_terra_perm("can_add_pictures"):
+            return obj.owner == request.user and obj.state in [
+                Picture.DRAFT,
+                Picture.REFUSED,
+            ]
+
+        return False
---
+++ packages/terra-opp/terra-opp-0.7.2.tar.gz/terra-opp-0.7.2/terra_opp/tests/mixins.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import Permission
+
+
+class TestPermissionsMixin:
+    def _clean_permissions(self):
+        self.user.user_permissions.clear()
+        self._clean_perm_cache()
+
+    def _set_permissions(self, perms, user=None):
+        if user is None:
+            self.user.user_permissions.add(
+                *Permission.objects.filter(codename__in=perms)
+            )
+        else:
+            user.user_permissions.add(*Permission.objects.filter(codename__in=perms))
+        self._clean_perm_cache()
+
+    def _clean_perm_cache(self):
+        for cache in ["_user_perm_cache", "_perm_cache"]:
+            if hasattr(self.user, cache):
+                delattr(self.user, cache)
