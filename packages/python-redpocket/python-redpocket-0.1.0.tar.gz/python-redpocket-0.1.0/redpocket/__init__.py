e3cc45c86df700680c4be74364d42ca52cfd0bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:08 +0000

    Add package python-redpocket/0.1.0/python-redpocket-0.1.0.tar.gz

---
+++ packages/python-redpocket/python-redpocket-0.1.0.tar.gz/python-redpocket-0.1.0/redpocket/__init__.py
@@ -0,0 +1,14 @@
+"""
+python-redpocket
+Author: Marc Billow
+License: MIT
+"""
+from .api import RedPocket  # noqa: F401
+from .exceptions import (
+    RedPocketException,
+    RedPocketAuthError,
+    RedPocketAPIError,
+)  # noqa: F401
+
+
+__version__ = "0.1.0"
