f2189a0e67afac82bd6fb6048f874bdf89ca2577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:30 +0000

    Add package bangladesh/0.0.4/bangladesh-0.0.4.tar.gz

---
+++ packages/bangladesh/bangladesh-0.0.4.tar.gz/bangladesh-0.0.4/README.md
@@ -0,0 +1,18 @@
+# Bangladesh
+
+Bangladesh information like District, Division, Thana, post code etc.
+This package allows you to direct use those data into the python code.
+
+Under construction! Not ready for use yet! Currently experimenting and planning!
+Developed by Zahid Hasan (c) 2021
+
+## Examples of How To Use (Alpha Version)
+
+from bangladesh import get_districts, get_divisions, get_postcodes, get_upazilas
+
+print(get_districts())
+print(get_divisions())
+print(get_upazilas())
+print(get_postcodes())
+
+Check out: https://github.com/Druvo/
---
+++ packages/bangladesh/bangladesh-0.0.4.tar.gz/bangladesh-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+setup(
+    name="bangladesh",
+    version='0.0.4',
+    author="zhdruvo (Zahid Hasan)",
+    author_email="<zhdruvo@gmail.com>",
+    description='A python package For Bangladesh information (District, Division, Thana, post code and etc)',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    license='Apache',
+    url='https://github.com/Druvo',
+    install_requires=[],
+    Platform='python',
+    keywords=['Bangladesh', 'District', 'Division', 'Thana',
+              'post code', 'Bangladesh District', 'Division District'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
