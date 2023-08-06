ab6acc9ee94ff15858a34fe569cc2825221e9634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:44 +0000

    Add package vtb-authorizer-utils/0.0.6/vtb_authorizer_utils-0.0.6-py3-none-any.whl

---
+++ packages/vtb-authorizer-utils/vtb_authorizer_utils-0.0.6-py3-none-any.whl/vtb_authorizer_utils/errors.py
@@ -0,0 +1,16 @@
+from typing import Optional, FrozenSet
+
+
+class NotInHierarchyError(Exception):
+    """ Не найден в иерархии """
+
+
+class NotAllowedParameterError(Exception):
+    """
+    Параметр запроса не разрешен
+    """
+
+    def __init__(self, not_allowed_parameters: FrozenSet[str],
+                 allowed_parameters: Optional[FrozenSet[str]] = frozenset({})):
+        self.message = f'Parameters "{",".join(not_allowed_parameters)}" are not allowed. List of allowed parameters: "{",".join(allowed_parameters)}"'
+        super().__init__(self.message)
