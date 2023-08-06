4fd1a618c562bd1fffccedac560648a2f4532aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:18 +0000

    Add package Kaggler/0.9.3/Kaggler-0.9.3-py3.8-macosx-10.9-x86_64.egg

---
+++ packages/Kaggler/Kaggler-0.9.3-py3.8-macosx-10.9-x86_64.egg/kaggler/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.9.3'
+__all__ = ['const',
+           'data_io',
+           'ensemble',
+           'feature_selection',
+           'metrics',
+           'model',
+           'online_model',
+           'preprocessing',
+           'util']
