5c4352df07327a9885aab0757420441fb4402913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:47 +0000

    Add package ocds-babel/0.3.4/ocds_babel-0.3.4-py3-none-any.whl

---
+++ packages/ocds-babel/ocds_babel-0.3.4-py3-none-any.whl/ocds_babel/translate_markdown.py
@@ -0,0 +1,31 @@
+from markdown_it.main import MarkdownIt
+from markdown_it.utils import AttrDict
+from mdformat.renderer import MDRenderer
+
+
+def translate_markdown(io, translator, **kwargs):
+    """
+    Accepts a Markdown file as an IO object, and returns its translated contents in Markdown format.
+    """
+    name = io.name
+    text = io.read()
+
+    return translate_markdown_data(name, text, translator, **kwargs)
+
+
+def translate_markdown_data(name, md, translator, **kwargs):
+    """
+    Accepts a Markdown file as its filename and contents, and returns its translated contents in Markdown format.
+    """
+    parser = MarkdownIt()
+    env = AttrDict()
+
+    tokens = []
+    for token in parser.parse(md, env):
+        if token.type == 'inline':
+            level = token.level
+            token = parser.parse(translator.gettext(token.content))[1]
+            token.level = level
+        tokens.append(token)
+
+    return MDRenderer().render(tokens, parser.options, env)
