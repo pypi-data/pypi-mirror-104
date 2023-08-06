936a1643ffb65591b019b4a7cb49d23d89d1a688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:34 +0000

    Add package distributions-jfrr/0.1/distributions_jfrr-0.1.tar.gz

---
+++ packages/distributions-jfrr/distributions_jfrr-0.1.tar.gz/distributions_jfrr-0.1/README.md
@@ -0,0 +1,11 @@
+# Hello
+
+This is a test.
+
+## Installation
+Run the following code on terminal:
+```
+$ pip install distributions-jfrr
+```
+### Version: 0.0.1
+Hello
---
+++ packages/distributions-jfrr/distributions_jfrr-0.1.tar.gz/distributions_jfrr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_jfrr',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_jfrr'],
+      zip_safe=False)
