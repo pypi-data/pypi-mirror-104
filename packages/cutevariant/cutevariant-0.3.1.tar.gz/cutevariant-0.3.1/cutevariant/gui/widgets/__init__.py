e7559c1847fcfaaa20f706cd74db404140d06bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package cutevariant/0.3.1/cutevariant-0.3.1.tar.gz

---
+++ packages/cutevariant/cutevariant-0.3.1.tar.gz/cutevariant-0.3.1/cutevariant/gui/widgets/__init__.py
@@ -0,0 +1,11 @@
+from .vqleditor import VqlSyntaxHighlighter, VqlEditor  # deprecated
+
+from .markdown_editor import MarkdownEditor
+
+from .dict_widget import DictModel, DictWidget
+
+from .codeedit import CodeEdit
+
+from .file_edit import FileEdit
+
+from .field_editor_widget import FieldsEditorWidget
