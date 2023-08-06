132c318dae13433a738410873310331973ea4f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:44 +0000

    Add package terality/0.8.11/terality-0.8.11.tar.gz

---
+++ packages/terality/terality-0.8.11.tar.gz/terality-0.8.11/common_client_scheduler/__init__.py
@@ -0,0 +1,11 @@
+from .exceptions import TeralityError, TeralityAuthError, TeralityInternalError, TeralityNotSupportedError, TeralityInvalidRequest
+from .config import TransferConfigLocal, TransferConfig, SessionInfoLocal, SessionInfo
+from .structs import (
+    StructRef, IndexColNames, PandasIndexMetadata, PandasSeriesMetadata, PandasDFMetadata,
+    Display, PandasFunctionRequest
+)
+from .requests_responses import (
+    CreateSessionResponse, DeleteSessionResponse, ComputationResponse, PendingComputationResponse, ErrorResponse,
+    UploadRequest, ExportRequest, ExportResponse
+)
+from .serde_config import serde_config_client_to_scheduler, serde_config_scheduler_to_client
