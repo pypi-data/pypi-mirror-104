3034948a8db71c5681e881705c91c51b9085dc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:04 +0000

    Add package djangolive/0.0.3/djangolive-0.0.3-py3-none-any.whl

---
+++ packages/djangolive/djangolive-0.0.3-py3-none-any.whl/djangolive/core/utils.py
@@ -0,0 +1,21 @@
+from .interfaces import DefaultCommand
+from typing import List
+
+
+def generate_command_map(commands: List):
+    return dict([cls.name, cls] for cls in commands)
+
+
+def perform_strategy(command: str, view, request, command_map):
+    data = request.data
+    if isinstance(data, list):
+        params = {"data": data}
+    elif isinstance(data, dict):
+        params = data
+    else:
+        params = {}
+
+    command_cls = command_map.setdefault(command, DefaultCommand)
+    command = command_cls(view, request, **params)
+    command.execute()
+    return command
