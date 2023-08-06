07cea3cc6608387004feeb0914e86fc164e5faaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:02 +0000

    Add package vivisect/1.0.2/vivisect-1.0.2-py3-none-any.whl

---
+++ packages/vivisect/vivisect-1.0.2-py3-none-any.whl/envi/tests/test_bits.py
@@ -0,0 +1,16 @@
+import unittest
+import envi.bits as e_bits
+
+
+class EnviBitsTest(unittest.TestCase):
+    def test_envi_bits_masktest(self):
+        self.assertTrue(e_bits.masktest('11001100')(0xcc))
+        self.assertTrue(e_bits.masktest('1100110011001100')(0xcccc))
+        self.assertTrue(e_bits.masktest('1100xxxx1100xxxx')(0xc2c2))
+        self.assertFalse(e_bits.masktest('110011xx110011xx')(0xc2c2))
+
+    def test_ieee754_encode(self):
+        pass
+
+    def test_byteswap(self):
+        self.assertEqual(0x12345678, e_bits.byteswap(0x78563412, 4))
