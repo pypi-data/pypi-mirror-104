54cef58552ccdf72062be588261c34fe3fd1116d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:15 +0000

    Add package charmonium.cache/1.0.0/charmonium.cache-1.0.0-py3-none-any.whl

---
+++ packages/charmonium.cache/charmonium.cache-1.0.0-py3-none-any.whl/charmonium/cache/obj_store.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+
+import shutil
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Callable
+
+import attr
+
+from .pathlike import PathLike, PathLikeFrom, pathlike_from
+from .util import GetAttr
+
+if TYPE_CHECKING:
+    from typing import Protocol
+else:
+    Protocol = object
+
+
+
+class ObjStore(Protocol):
+    """An `object-store`_ is a persistent mapping from int to bytes.
+
+    .. _`object-store`: https://en.wikipedia.org/wiki/Object_storage
+
+    """
+    def __setitem__(self, key: int, val: bytes) -> None:
+        ...
+
+    def __getitem__(self, key: int) -> bytes:
+        ...
+
+    def __delitem__(self, key: int) -> None:
+        ...
+
+    def __contains__(self, key: int) -> bool:
+        """The implementation is often slow, so it will be called rarely."""
+        ...
+
+    def __hash__(self) -> int:
+        ...
+
+    def clear(self) -> None:  # pylint: disable=no-self-use
+        # Doesn't use self because I'm defining a Protocol
+        ...
+
+
+# pyright thinks attrs has ambiguous overload
+@attr.frozen(init=False)  # type: ignore
+class DirObjStore(ObjStore):
+    """Use a directory in the filesystem as an object-store.
+
+    Each object is a file in the directory.
+
+    Note that this directory must not contain any other files.
+
+    """
+
+    path: PathLike
+    _key_bytes: int
+
+    def __determ_hash__(self) -> Any:
+        return (str(self.path), self._key_bytes)
+
+    def __init__(self, path: PathLikeFrom, key_bytes: int = 8) -> None:
+        """
+        :param path: a 'PathLike' object which will be the directory of the object store.
+        :param key_bytes: the number of bytes to use as keys
+        """
+        super().__init__()
+        object.__setattr__(self, "path", pathlike_from(path))
+        object.__setattr__(self, "_key_bytes", key_bytes)
+
+        if self.path.exists() and any(
+            not self._is_key(path) and not path.name.startswith(".") for path in self.path.iterdir()
+        ):
+            raise ValueError(
+                f"{self.path.resolve()} contains junk I didn't make."
+            )
+
+    def _int2str(self, key: int) -> str:
+        assert key < (1 << (8 * self._key_bytes))
+        return f"{key:0{2*self._key_bytes}x}"
+
+    def _is_key(self, path: PathLike) -> bool:
+        return len(path.name) == 2 * self._key_bytes and all(
+            letter in "0123456789abcdef" for letter in path.name
+        )
+
+    def __setitem__(self, key: int, val: bytes) -> None:
+        self.path.mkdir(exist_ok=True)
+        (self.path / self._int2str(key)).write_bytes(val)
+
+    def __getitem__(self, key: int) -> bytes:
+        path = self.path / self._int2str(key)
+        if not path.exists():
+            raise KeyError(key)
+        else:
+            return path.read_bytes()
+
+    def __delitem__(self, key: int) -> None:
+        if key in self:
+            (self.path / self._int2str(key)).unlink()
+
+    def __contains__(self, key: int) -> bool:
+        return (self.path / self._int2str(key)).exists()
+
+    def clear(self) -> None:
+        self.path.mkdir(exist_ok=True)
+        # somehow pyright doesn't think that a Path can be PathLike
+        if isinstance(self.path, Path): # type: ignore
+            shutil.rmtree(self.path)
+        else:
+            # "There is no syntax to indicate optional or keyword arguments; such function types are rarely used as callback types"
+            # :'(
+            # Therefore, I can't use Callable[[], None]
+            GetAttr[Callable[..., None]]()(self.path, "rm")(recursive=True)
+        self.path.mkdir()
---
+++ packages/charmonium.cache/charmonium.cache-1.0.0-py3-none-any.whl/charmonium/cache/replacement_policies.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import abc
+import datetime
+from typing import TYPE_CHECKING, Any, Mapping
+
+import attr
+import bitmath
+
+
+# pyright thinks attrs has ambiguous overload
+@attr.define  # type: ignore
+class Entry:
+    value: Any
+    data_size: bitmath.Bitmath
+    recompute_time: datetime.timedelta
+    time_saved: datetime.timedelta
+    obj_store: bool
+
+
+# TODO: test that these methods are called at the right time.
+class ReplacementPolicy:  # pylint: disable=no-self-use,unused-argument
+    @abc.abstractmethod
+    def add(self, key: Any, entry: Entry) -> None:
+        ...
+    @abc.abstractmethod
+    def access(self, key: Any, entry: Entry) -> None:
+        ...
+    def invalidate(self, key: Any, entry: Entry) -> None:
+        ...
+    @abc.abstractmethod
+    def evict(self) -> tuple[Any, Entry]:
+        ...
+    @abc.abstractmethod
+    def update(self, other: ReplacementPolicy) -> None:
+        # TODO: Unlike dict update, self overrides other
+        ...
+
+class GDSize(ReplacementPolicy):
+    """GreedyDual-Size policy, described by [Cao et al]_."""
+    def __init__(self) -> None:
+        self.inflation = 0.0
+        self._data: dict[Any, tuple[float, Entry]] = {}
+    def add(self, key: Any, entry: Entry) -> None:
+        self.access(key, entry)
+    def access(self, key: Any, entry: Entry) -> None:
+        score = self.inflation + entry.recompute_time.total_seconds() / max(entry.data_size.to_Byte().value, 1)
+        self._data[key] = (score, entry)
+    def invalidate(self, key: Any, entry: Any) -> None: # pylint: disable=unused-argument
+        del self._data[key]
+    def evict(self) -> tuple[Any, Entry]:
+        if self._data:
+            self.inflation, key, entry = min([(score, key, entry) for key, (score, entry) in self._data.items()])
+            del self._data[key]
+            return key, entry
+        else:
+            raise ValueError("No data left to evict")
+    def update(self, other: ReplacementPolicy) -> None:
+        if isinstance(other, GDSize) or (type(other).__name__ == type(self).__name__ and not TYPE_CHECKING):
+            # I need the type(other).__name == type(self).__name__ because when this class is de/serialized, Python forgets that it is equal.
+            # However, I don't want the type checker to think too hard about it; it should just know isinstance(other, GDSIze), so I add not TYPE_CHECKING.
+            self._data.update(other._data) # pylint: disable=protected-access
+            self.inflation = other.inflation
+        else:
+            raise TypeError(f"Cannot update a {type(self)} from a {type(other)}")
+
+REPLACEMENT_POLICIES: Mapping[str, type[ReplacementPolicy]] = dict(
+    gdsize=GDSize,
+)
