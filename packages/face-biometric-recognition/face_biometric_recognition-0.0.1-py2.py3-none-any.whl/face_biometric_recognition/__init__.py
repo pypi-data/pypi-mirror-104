7fa77a6715fd97914e10090c28a8cb179855bfbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:57 +0000

    Add package face-biometric-recognition/0.0.1/face_biometric_recognition-0.0.1-py2.py3-none-any.whl

---
+++ packages/face-biometric-recognition/face_biometric_recognition-0.0.1-py2.py3-none-any.whl/face_biometric_recognition/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Ransom Voke Anighoro"""
+__email__ = 'voke.anighoro@gmail.com'
+__version__ = '0.0.1'
+
+from .api import (
+    load_image_file,
+    face_locations,
+    batch_face_locations,
+    face_landmarks,
+    face_encodings,
+    compare_faces,
+    face_distance
+)
