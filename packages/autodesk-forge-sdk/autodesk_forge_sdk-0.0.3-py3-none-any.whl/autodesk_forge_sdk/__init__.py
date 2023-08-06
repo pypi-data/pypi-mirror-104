825a92635c1297d5597d386d23c723e6555db649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:12 +0000

    Add package autodesk-forge-sdk/0.0.3/autodesk_forge_sdk-0.0.3-py3-none-any.whl

---
+++ packages/autodesk-forge-sdk/autodesk_forge_sdk-0.0.3-py3-none-any.whl/autodesk_forge_sdk/__init__.py
@@ -0,0 +1,3 @@
+from .auth import AuthenticationClient, Scope, TokenProviderInterface, SimpleTokenProvider, OAuthTokenProvider
+from .oss import OSSClient
+from .md import ModelDerivativeClient, urnify
