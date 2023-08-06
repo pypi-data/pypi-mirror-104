75f12eae09bd94d4e1b99464724bd72e3f2041e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:46 +0000

    Add package easydubins/1.1.0/easydubins-1.1.0.tar.gz

---
+++ packages/easydubins/easydubins-1.1.0.tar.gz/easydubins-1.1.0/setup.py
@@ -0,0 +1,33 @@
+"""
+Setup script for easydubins
+"""
+
+import os.path
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="easydubins",
+    version="1.1.0",
+    description="To generate dubin curves projection points.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Rishav",
+    author_email="xyz@gmail.com",
+    license="GPL3",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=["easydubins"],
+    include_package_data=True,
+    install_requires=["importlib_resources"],
+    entry_points={"console_scripts": ["easydubins=easydubins.__main__:main"]},
+)
