be00892e397010941881b288d90f74bc22ce7f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package mesaPlot/0.0/mesaPlot-0.0.tar.gz

---
+++ packages/mesaPlot/mesaPlot-0.0.tar.gz/mesaPlot-0.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import unittest
+
+setup(name='mesaPlot',
+      version='0.0',
+      description='Library for reading and plotting MESA data',
+      license="GPLv2+",
+      author='Robert Farmer',
+      author_email='r.j.farmer@uva.nl',
+      url='https://github.com/rjfarmer/mesaPlot',
+      packages=["mesaPlot"],
+      tests_require=["pytest","unittest2"],
+      test_suite='tests',
+      python_requires='>=3.6',
+      classifiers=[
+			"Development Status :: 5 - Production/Stable",
+			"Intended Audience :: Science/Research",
+			"License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+			"Topic :: Scientific/Engineering :: Astronomy",
+      ],
+      extras_require={
+		'dev': [
+			'unittest2'
+			]
+		}
+     )
