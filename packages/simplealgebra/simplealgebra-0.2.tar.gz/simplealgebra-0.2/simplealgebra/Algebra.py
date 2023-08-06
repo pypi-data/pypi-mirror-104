177c92a88dd517d7a03b8614c1d27472bdebfec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:04 +0000

    Add package simplealgebra/0.2/simplealgebra-0.2.tar.gz

---
+++ packages/simplealgebra/simplealgebra-0.2.tar.gz/simplealgebra-0.2/simplealgebra/Algebra.py
@@ -0,0 +1,17 @@
+import math
+
+class SimpleAlgebra():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        
+    def addition(a, b):
+        return self.a + self.b
+    
+    def substraction(a,b):
+        return self.a - self.b
+    
+    def multiplication(a,b):
+        return self.a * self.b
+
+        
