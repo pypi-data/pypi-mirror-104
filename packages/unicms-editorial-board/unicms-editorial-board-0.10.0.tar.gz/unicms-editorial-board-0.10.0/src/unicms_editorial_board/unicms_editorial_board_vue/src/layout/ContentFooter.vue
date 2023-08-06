65ef0f75f9d1f5a4baf58ae83b004e52ca5d82b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:12 +0000

    Add package unicms-editorial-board/0.10.0/unicms-editorial-board-0.10.0.tar.gz

---
+++ packages/unicms-editorial-board/unicms-editorial-board-0.10.0.tar.gz/unicms-editorial-board-0.10.0/src/unicms_editorial_board/unicms_editorial_board_vue/src/layout/ContentFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <nav>
+        <ul class="footer-menu">
+          <li>
+            <router-link :to="{path:'/'}">Dashboard</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        <b-icon icon="github"></b-icon> Contribute to:
+        <a href="https://github.com/UniversitaDellaCalabria/uniCMS" target="_blank">
+            uniCMS
+        </a>
+        /
+        <a href="https://github.com/UniversitaDellaCalabria/unicms-editorial-board" target="_blank">
+            uniCMS editorial board
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+  export default {}
+
+</script>
+<style>
+
+</style>
---
+++ packages/unicms-editorial-board/unicms-editorial-board-0.10.0.tar.gz/unicms-editorial-board-0.10.0/src/unicms_editorial_board/unicms_editorial_board_vue/src/pages/MenuItemEdit.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="content">
+        <div class="container-fluid">
+            <Breadcrumbs/>
+
+            <stacked-alerts :alerts="alerts" />
+
+            <div class="row">
+                <div class="col-12">
+                    <b-card>
+                        <div class="clearfix mb-5">
+                             <div class="pull-left">
+                                <router-link :to="{ name: 'MenuItemLogs',
+                                                    params: { menu_id: menu_id,
+                                                              menu_item_id: menu_item_id }}"
+                                    class="btn btn-sm btn-outline-secondary mx-md-1">
+                                    <b-icon icon="clock-history"
+                                        variant="secondary"></b-icon>
+                                    History
+                                </router-link>
+                            </div>
+                            <div class="pull-right">
+                                <b-button
+                                    @click="deleteModal()"
+                                    variant="danger"
+                                    class="mx-md-1 btn-sm">
+                                    <b-icon icon="trash"
+                                        variant="white"></b-icon>
+                                    Delete
+                                </b-button>
+                            </div>
+                        </div>
+
+                        <b-card-title>{{ page_title }}</b-card-title>
+
+                        <b-card-text>
+                            <django-form
+                                ref="form"
+                                :form="form"
+                                :submit="onSubmit"
+                                :form_source="form_source" />
+                        </b-card-text>
+                    </b-card>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            alerts: [],
+            menu_id: this.$route.params.menu_id,
+            menu_item_id: this.$route.params.menu_item_id,
+            form: {},
+            form_source: '/api/editorial-board/menus/'+this.$route.params.menu_id+'/items/form/',
+            files: {},
+            page_title: ''
+        }
+    },
+    methods: {
+        getItem() {
+            let source = '/api/editorial-board/menus/'+this.menu_id+'/items/'+this.menu_item_id+'/';
+            this.axios
+                .get(source)
+                .then(response => {
+                    for (const [key, value] of Object.entries(response.data)) {
+                        if(key=='webpath') {
+                            this.$set(this.form, key, value.id)
+                        }
+                        else this.$set(this.form, key, value)
+                    }
+                    this.page_title = response.data.name;
+                    this.$checkForRedisLocks(response.data.object_content_type,
+                                             this.menu_item_id)
+                    if(response.data.webpath)
+                        this.$refs.form.getOptionsFromParent('webpath',
+                            [{"text": response.data.webpath.name,
+                              "value": response.data.webpath.id}])
+                })
+        },
+        onSubmit(event) {
+            let source = '/api/editorial-board/menus/'+this.menu_id+'/items/'+this.menu_item_id+'/';
+            event.preventDefault();
+            this.axios
+                .patch(source, this.form,
+                      {headers: {"X-CSRFToken": this.$csrftoken }}
+                )
+                .then(response => {
+                    this.alerts.push(
+                        { variant: 'success',
+                          message: 'menu item edited successfully',
+                          dismissable: true }
+                    )}
+                )
+                .catch(error => {
+                    for (var key in error.response.data) {
+                        this.alerts.push(
+                            { variant: 'danger',
+                              message: key + ': ' + error.response.data[key],
+                              dismissable: true }
+                        )
+                    }
+                })
+        },
+        remove() {
+            this.axios
+                .delete('/api/editorial-board/menus/'+this.menu_id+'/items/'+this.menu_item_id+'/',
+                        {headers: {"X-CSRFToken": this.$csrftoken }}
+                       )
+                .then(response => {
+                    this.alerts.push(
+                        { variant: 'success',
+                          message: 'menu item removed successfully',
+                          dismissable: true }
+                    );
+                    this.$router.push({name: 'MenuItems',
+                                       params: {menu_id: this.menu_id,
+                                       alerts: this.alerts}})}
+                )
+                .catch(error => {
+                    this.alerts.push(
+                        { variant: 'danger',
+                          message: error.response.data.detail,
+                          dismissable: true }
+                    )
+                })
+        },
+        deleteModal() {
+            this.$bvModal.msgBoxConfirm('Do you want really delete menu item?', {
+            title: 'Please Confirm',
+                size: 'sm',
+                buttonSize: 'sm',
+                okVariant: 'danger',
+                okTitle: 'YES',
+                cancelTitle: 'NO',
+                footerClass: 'p-2',
+                hideHeaderClose: false,
+                }
+            ).then(value => {
+                if (value) this.remove();
+            })
+        }
+    },
+    mounted() {
+        this.getItem()
+    }
+}
+</script>
