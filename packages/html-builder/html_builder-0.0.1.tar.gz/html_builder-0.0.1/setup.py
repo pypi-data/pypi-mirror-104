fb6b6784af002d36d0b8584eae5acda24cdac7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package html-builder/0.0.1/html_builder-0.0.1.tar.gz

---
+++ packages/html-builder/html_builder-0.0.1.tar.gz/html_builder-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+long_description = '''
+# readme:
+
+this is a package to create html with python language
+though it will be used by myself, also you can use it.
+'''
+
+setup(
+    name='html_builder',
+    version='0.0.1',
+    author='normidar',
+    author_email='normidar7@gmail.com',
+    url='https://normidar.com',
+    description=u'this package is use for creating html by python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['HtmlCreator'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': []
+    }
+)
