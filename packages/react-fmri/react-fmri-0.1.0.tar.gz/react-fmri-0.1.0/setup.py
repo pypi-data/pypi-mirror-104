02d2e49fab1ad85ac01b79482300124bf463620d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:18 +0000

    Add package react-fmri/0.1.0/react-fmri-0.1.0.tar.gz

---
+++ packages/react-fmri/react-fmri-0.1.0.tar.gz/react-fmri-0.1.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+# Read the version from the main package.
+with open('react/__init__.py') as f:
+    for line in f:
+        if '__version__' in line:
+            _, version, _ = line.split("'")
+            break
+
+
+setup(
+    author='Ottavia Dipasquale, Matteo Frigo',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    ],
+    description='A Python package that implements REACT: Receptor-Enriched '
+                'Analysis of Functional Connectivity by Targets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['nibabel>=3.0.0', 'numpy', 'scikit-learn>=0.22', 'scipy'],
+    name='react-fmri',
+    packages=['react'],
+    python_requires='>=3',
+    scripts=['script/react', 'script/react_masks', 'script/react_normalize'],
+    url='',
+    version=version,
+    project_urls={
+        'Source': 'https://github.com/ottaviadipasquale/react-fmri',
+        'Bug Reports': 'https://github.com/ottaviadipasquale/react-fmri/issues',
+        'Documentation': 'https://github.com/ottaviadipasquale/react-fmri/'
+                         'blob/main/README.md',
+    },
+)
