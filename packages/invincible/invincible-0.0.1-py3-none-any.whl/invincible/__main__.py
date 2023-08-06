69d7010f82f90725b94769bf3de7e436f4eb4ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package invincible/0.0.1/invincible-0.0.1-py3-none-any.whl

---
+++ packages/invincible/invincible-0.0.1-py3-none-any.whl/invincible/__main__.py
@@ -0,0 +1,19 @@
+import sys
+import subprocess
+import time
+from loguru import logger
+
+
+def execute_subprocess() -> None:
+    command = sys.argv[1:]
+    while True:
+        logger.info(f"Executing `{command}`")
+        subprocess.run(command)
+        time.sleep(5)
+
+
+def main() -> None:
+    execute_subprocess()
+
+if __name__ == "__main__":
+    execute_subprocess()
