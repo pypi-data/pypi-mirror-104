6796a1bc3841b095038283f654cb7a840dd5c75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:51 +0000

    Add package rubrix/0.1.0b3/rubrix-0.1.0b3.tar.gz

---
+++ packages/rubrix/rubrix-0.1.0b3.tar.gz/rubrix-0.1.0b3/frontend/layouts/exploration.vue
@@ -0,0 +1,30 @@
+<template>
+  <app>
+    <NuxtChild />
+  </app>
+</template>
+
+<script>
+import app from '~/layouts/app.vue';
+export default {
+  name: 'exploration',
+  head() {
+    return {
+      bodyAttrs: {
+        class: [ "theme-exploration"],
+      },
+    };
+  },
+  components: {
+    app
+  }
+};
+</script>
+
+<style lang="scss">
+.theme-exploration {
+  background: $bg;
+    //   height: 100vh;
+    // overflow: hidden;
+}
+</style>
