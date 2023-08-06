ccc5c3e2c993f9d95f04857ab7151c7a88bc2733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:59 +0000

    Add package universal-parser/1.0.8/universal-parser-1.0.8.tar.gz

---
+++ packages/universal-parser/universal-parser-1.0.8.tar.gz/universal-parser-1.0.8/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="universal-parser",
+    version="1.0.8",
+    author="Vyacheslav Zakharchuk",
+    author_email="sovenok997@gmail.com",
+    description="Json/Pickle/Toml/Yaml parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/onyx-flame/universal-parser",
+    project_urls={
+        "Bug Tracker": "https://github.com/onyx-flame/universal-parser/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['scripts/format_converter'],
+    entry_points = {
+        'console_scripts': ['parser=universal_parser.main:main'],
+    },
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'PyYAML>=5.4.1',
+        'pytomlpp>=0.3.5',
+        ],
+    python_requires=">=3.6",
+)
