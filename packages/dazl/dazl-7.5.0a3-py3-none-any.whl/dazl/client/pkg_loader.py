dd6ef27ce65859a0e578cabf9f8e0c97f3dd01db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:33 +0000

    Add package dazl/7.5.0a3/dazl-7.5.0a3-py3-none-any.whl

---
+++ packages/dazl/dazl-7.5.0a3-py3-none-any.whl/dazl/client/pkg_loader.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2017-2021 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import warnings
+
+from ..ledger.pkgloader_aio_compat import PackageLoader, SyncPackageService
+
+warnings.warn(
+    "dazl.client.pkg_loader is deprecated; use dazl.protocols.pkgloader_aio instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+__all__ = ["SyncPackageService", "PackageLoader"]
