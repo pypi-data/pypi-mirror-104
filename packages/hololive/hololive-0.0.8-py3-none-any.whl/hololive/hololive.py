2d58604cb76de56c6014227abbddc083a6ab1cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:12 +0000

    Add package hololive/0.0.8/hololive-0.0.8-py3-none-any.whl

---
+++ packages/hololive/hololive-0.0.8-py3-none-any.whl/hololive/hololive.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from typing import List
+import requests
+
+class Stream:
+  title_jp: str
+  talent_jp: str
+  url: str
+  starttime: datetime
+  
+async def get_streams() -> List[Stream]:
+  streams: list[Stream] = []
+  API_schedule = requests.get("https://hololive-api.marnixah.com/").json()
+  for day in API_schedule["schedule"]:
+    date_month = day["date"].split("/")[0]
+    date_day = day["date"].split("/")[1]
+    for stream in day["schedules"]:
+      stream_obj = Stream()
+      stream_obj.url = str(stream["youtube_url"])
+      stream_obj.title_jp = str(stream["title"])
+      stream_obj.talent_jp = str(stream["member"])
+      
+      time_arr = stream["time"].split(":")
+      hour = int(time_arr[0])
+      minute = int(time_arr[1])
+
+      current_time = datetime.utcnow()
+      year = current_time.year
+
+      stream_obj.starttime = datetime(
+        year, int(date_month), int(date_day), hour, minute
+      ) - timedelta(hours=9)  # JST is 9 hours ahead of UTC
+      streams.append(stream_obj)
+  return streams
