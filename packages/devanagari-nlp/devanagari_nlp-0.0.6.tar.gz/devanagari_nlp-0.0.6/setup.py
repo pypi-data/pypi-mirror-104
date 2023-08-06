7036589f5dbd4cdffa2352a8678159dbd3b68a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:35 +0000

    Add package devanagari-nlp/0.0.6/devanagari_nlp-0.0.6.tar.gz

---
+++ packages/devanagari-nlp/devanagari_nlp-0.0.6.tar.gz/devanagari_nlp-0.0.6/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from setuptools import setup,find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# with open("requirements.txt","r") as req:
+#     requirements = req.readlines()
+
+# requirements = [x.strip() for x in requirements]
+# import pdb;pdb.set_trace()
+
+setuptools.setup(
+    name="devanagari_nlp",
+    version="0.0.6",
+    author="Shushant Pudasaini, Nitesh Ghimire , Sagar Lamichhane, Aakash Dumjan, Sujan Adhikari, Sajjan Adhikari, Janardan Karki",
+    author_email="shusrulz@gmail.com",
+    description="Package for applying NLP to devanagari datasets",
+    packages = find_packages(where="devanagari_nlp"),
+    zip_safe = False,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data = True,
+    url="https://gitlab.com/shusrulz/everest_nlp/-/archive/0.0.6/everest_nlp-0.0.6.tar.gz",
+    install_requires = ["gensim==3.7.3","sklearn","jellyfish==0.8.2","pandas"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "devanagari_nlp"},
+    python_requires=">=3.6",
+)
