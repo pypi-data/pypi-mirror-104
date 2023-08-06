17c8d57cf2decd95a1eee5ece06c0a4fe93b071a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:45 +0000

    Add package dogehouse/3.0.0a1/dogehouse-3.0.0a1-py3-none-any.whl

---
+++ packages/dogehouse/dogehouse-3.0.0a1-py3-none-any.whl/dogehouse/events.py
@@ -0,0 +1,12 @@
+READY = '_ready'
+
+NEW_TOKENS = 'new-tokens'
+
+CREATE_ROOM = 'room:create'
+ROOM_CREATED = 'room:create:reply'
+
+USER_JOINED = 'new_user_join_room'
+USER_LEFT = 'user_left_room'
+MESSAGE = 'chat:send'
+
+SEND_MESSAGE = 'send_room_chat_msg'
