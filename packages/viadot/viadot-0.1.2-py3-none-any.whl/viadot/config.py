cbc4b858d3edeb1359da08a1d0556f44f091d158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:41 +0000

    Add package viadot/0.1.2/viadot-0.1.2-py3-none-any.whl

---
+++ packages/viadot/viadot-0.1.2-py3-none-any.whl/viadot/config.py
@@ -0,0 +1,17 @@
+import json
+from os.path import expanduser, join
+
+USER_HOME = expanduser("~")
+
+
+class Config(dict):
+    @classmethod
+    def from_json(cls, path: str, key: str = None):
+        with open(path) as f:
+            config = json.load(f)
+            if key:
+                config = config[key]
+            return cls(**config)
+
+
+local_config = Config.from_json(join(USER_HOME, ".config", "credentials.json"))
