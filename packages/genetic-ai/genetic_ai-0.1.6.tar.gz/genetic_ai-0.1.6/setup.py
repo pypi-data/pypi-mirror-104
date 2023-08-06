982a67c223937b583074101adcd1286babcca5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:02 +0000

    Add package genetic-ai/0.1.6/genetic_ai-0.1.6.tar.gz

---
+++ packages/genetic-ai/genetic_ai-0.1.6.tar.gz/genetic_ai-0.1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='genetic_ai',
+    version='0.1.6',
+    packages=['genetic_ai'],
+    url='',
+    license='',
+    author='AI University',
+    author_email='nejronki@gmail.com'    
+)
