5f65527c3509130eaad58fe7a6fa474a807cd4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:26 +0000

    Add package me314/0.0.1/me314-0.0.1-py3-none-any.whl

---
+++ packages/me314/me314-0.0.1-py3-none-any.whl/me314/__init__.py
@@ -0,0 +1,4 @@
+from me314.me314 import *
+
+__version__ = "0.0.1"
+
---
+++ packages/me314/me314-0.0.1-py3-none-any.whl/me314/me314.py
@@ -0,0 +1,7 @@
+import numpy as np
+import sympy as sym
+
+
+def test_installation():
+    print("package has been successfully installed.")
+
