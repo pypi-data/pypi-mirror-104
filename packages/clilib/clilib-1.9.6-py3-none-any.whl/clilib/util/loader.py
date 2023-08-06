12be2682799ac280fecf199f8ae2ac39b2942096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:13 +0000

    Add package clilib/1.9.6/clilib-1.9.6-py3-none-any.whl

---
+++ packages/clilib/clilib-1.9.6-py3-none-any.whl/clilib/util/loader.py
@@ -0,0 +1,33 @@
+import json
+from sys import platform
+
+from clilib.util.arg_tools import arg_tools
+import importlib
+
+
+class Loader:
+    @staticmethod
+    def getActiveModules(path):
+        if platform == "win32":
+            with open(path) as f:
+                moduleSpec = json.load(f)
+            return moduleSpec
+        else:
+            with open(path) as f:
+                moduleSpec = json.load(f)
+            return moduleSpec
+
+    @staticmethod
+    def start_app(modules, modules_base):
+        args, _ = arg_tools.command_parser(modules)
+        try:
+            module = importlib.import_module(modules_base + "." + args.command[0] + ".main")
+            module.main()
+        except ImportError as ex:
+            print("Command not found: " + args.command[0] + ". Command must be one of " + ", ".join(modules))
+            print(ex)
+        except SystemExit as sys_exit:
+            exit(sys_exit.code)
+        except KeyboardInterrupt:
+            print("User keyboard interrupt!")
+            exit(1)
