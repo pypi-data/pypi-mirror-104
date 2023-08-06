8c605854302427eb9dee06b6d5defc0728a19cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:20 +0000

    Add package trytond/6.0.0/trytond-6.0.0.tar.gz

---
+++ packages/trytond/trytond-6.0.0.tar.gz/trytond-6.0.0/trytond/tests/test_res.py
@@ -0,0 +1,28 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+import unittest
+
+from trytond.pool import Pool
+
+from .test_tryton import ModuleTestCase, with_transaction
+
+
+class ResTestCase(ModuleTestCase):
+    'Test res module'
+    module = 'res'
+
+    @with_transaction()
+    def test_user_avatar(self):
+        pool = Pool()
+        User = pool.get('res.user')
+
+        user = User(login="avatar")
+        user.save()
+
+        self.assertEqual(len(user.avatars), 1)
+        self.assertIsNotNone(user.avatar)
+        self.assertRegex(user.avatar_url, r'/avatar/.*/([0-9a-fA-F]{12})')
+
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(ResTestCase)
