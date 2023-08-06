1f1192dcae6fd0e448483d5ab1693f75ce5023c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:01 +0000

    Add package Pyubiomes/0.1.1/Pyubiomes-0.1.1.tar.gz

---
+++ packages/Pyubiomes/Pyubiomes-0.1.1.tar.gz/Pyubiomes-0.1.1/setup.py
@@ -0,0 +1,5 @@
+k=open("LONGDESC.md",'r').read()
+from setuptools import setup, find_packages
+setup(name = 'Pyubiomes', version = '0.1.01', description="a (probably bad wip) python wrapper for the C library Cubiomes", author="4gboframram", url="https://github.com/4gboframram/Pyubiomes",author_email="<zachawesomeness411@gmail.com>", long_description=k, include_package_data=True,
+packages=find_packages(),
+package_data={'': ['searches.so']},)
