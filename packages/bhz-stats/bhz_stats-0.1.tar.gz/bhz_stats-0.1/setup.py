d1712740ffecf356b3cc2193c337731a237fcf98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:59 +0000

    Add package bhz-stats/0.1/bhz_stats-0.1.tar.gz

---
+++ packages/bhz-stats/bhz_stats-0.1.tar.gz/bhz_stats-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bhz_stats',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Behnaz Sadat',
+      author_email = 'ssadat2@asu.edu',
+      packages=['bhz_stats'],
+      zip_safe=False)
